e8c81c0a68ed487a8cd06e46fe2d55a6dea4182f
sjanoest <annika.oesterdiekhoff@stud.uni-due.de>
2021-12-13 15:25:19 +0100

    first draft of autogenerating test bench

--- elasticai/generator/test_generate_testbench.py
+++ elasticai/generator/test_generate_testbench.py
@@ -1,5 +1,126 @@
 import unittest
+from os.path import exists
+from elasticai.generator.generate_testbench import main
 
 
 class GenerateTestBenchTest(unittest.TestCase):
-    pass
+    def setUp(self) -> None:
+        self.generated_testbench_file = open('../testbench/sigmoid_generate_tb.vhd', 'r')
+        self.lines = self.generated_testbench_file.readlines()
+        main()
+
+    def tearDown(self) -> None:
+        self.generated_testbench_file.close()
+
+    def test_generate_file(self) -> None:
+        self.assertTrue(exists('../testbench/sigmoid_generate_tb.vhd'))
+
+    def test_generate_imports(self) -> None:
+        expected_import_lines = [
+            "library ieee;\n",
+            "use ieee.std_logic_1164.all;\n",
+            "use ieee.numeric_std.all;               -- for type conversions\n"
+        ]
+        for i in range(0, 3):
+            self.assertEqual(expected_import_lines[i], self.lines[i])
+
+    def test_generate_entity(self) -> None:
+        expected_entity_lines = [
+            "entity sigmoid_tb is\n",
+            "    port ( clk: out std_logic);\n",
+            "end entity ; -- sigmoid_tb\n"
+        ]
+        for i, j in zip(range(0, 3), range(4, 7)):
+            self.assertEqual(expected_entity_lines[i], self.lines[j])
+
+    def test_generate_component(self) -> None:
+        expected_component_lines = [
+            "architecture behav of sigmoid_tb is\n",
+            "\n",
+            "    component sigmoid is\n",
+            "        generic (\n",
+            "                DATA_WIDTH : integer := 16;\n",
+            "                FRAC_WIDTH : integer := 8\n",
+            "            );\n",
+            "        port (\n",
+            "            x : in signed(DATA_WIDTH-1 downto 0);\n",
+            "            y: out signed(DATA_WIDTH-1 downto 0)\n",
+            "        );\n",
+            "    end component;\n"
+        ]
+        for i, j in zip(range(len(expected_component_lines)), range(8, 19)):
+            self.assertEqual(expected_component_lines[i], self.lines[j])
+
+    def test_generate_inputs(self) -> None:
+        expected_inputs_lines = [
+            "    ------------------------------------------------------------\n",
+            "    -- Testbench Internal Signals\n",
+            "    ------------------------------------------------------------\n",
+            "    signal clk_period : time := 1 ns;\n",
+            "    signal test_input : signed(16-1 downto 0):=(others=>'0');\n",
+            "    signal test_output : signed(16-1 downto 0);\n"
+        ]
+        for i, j in zip(range(len(expected_inputs_lines)), range(21, 27)):
+            self.assertEqual(expected_inputs_lines[i], self.lines[j])
+
+    def test_generate_clock(self) -> None:
+        expected_clock_lines = [
+            "begin\n",
+            "\n",
+            "    clock_process : process\n",
+            "    begin\n",
+            "        clk <= '0';\n",
+            "        wait for clk_period/2;\n",
+            "        clk <= '1';\n",
+            "        wait for clk_period/2;\n",
+            "    end process; -- clock_process\n"
+        ]
+        for i, j in zip(range(len(expected_clock_lines)), range(28, 37)):
+            self.assertEqual(expected_clock_lines[i], self.lines[j])
+
+    def test_generate_utt(self) -> None:
+        expected_utt_lines = [
+            "    utt: sigmoid\n",
+            "    port map (\n",
+            "    x => test_input,\n",
+            "    y => test_output\n",
+            "    );\n"
+        ]
+        for i, j in zip(range(len(expected_utt_lines)), range(38, 43)):
+            self.assertEqual(expected_utt_lines[i], self.lines[j])
+
+    def test_generate_test_process(self) -> None:
+        expected_test_process_lines = [
+            "test_process: process is\n",
+            "    begin\n",
+            "        Report \"======Simulation start======\" severity Note;\n",
+            "        \n",
+            "        test_input <=  to_signed(-1281,16);\n",
+            "        wait for 1*clk_period;\n",
+            "        report \"The value of 'test_output' is \" & integer'image(to_integer(unsigned(test_output)));\n",
+            "        assert test_output=0 report \"The test case -1281 fail\" severity failure;\n",
+            "        \n",
+            "        test_input <=  to_signed(-1000,16);\n",
+            "        wait for 1*clk_period;\n",
+            "        report \"The value of 'test_output' is \" & integer'image(to_integer(unsigned(test_output)));\n",
+            "        assert test_output=4 report \"The test case -1000 fail\" severity failure;\n",
+            "        \n",
+            "        test_input <=  to_signed(-500,16);\n",
+            "        wait for 1*clk_period;\n",
+            "        report \"The value of 'test_output' is \" & integer'image(to_integer(unsigned(test_output)));\n",
+            "        assert test_output=28 report \"The test case -500 fail\" severity failure;\n",
+            "        \n",
+            "        \n",
+            "        -- if there is no error message, that means all test case are passed.\n",
+            "        report \"======Simulation Success======\" severity Note;\n",
+            "        report \"Please check the output message.\" severity Note;\n",
+            "        \n",
+            "        -- wait forever\n",
+            "        wait;\n",
+            "        \n",
+            "    end process; -- test_process\n",
+            "\n",
+            "end behav ; -- behav\n"
+        ]
+        for i, j in zip(range(len(expected_test_process_lines)), range(44, 75)):
+            self.assertEqual(expected_test_process_lines[i], self.lines[j])
--- elasticai/testbench/sigmoid_generate_tb.vhd
+++ elasticai/testbench/sigmoid_generate_tb.vhd
@@ -1,45 +1,32 @@
-def write_imports():
-    return """library ieee;
+library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;               -- for type conversions
-    """
-
-
-def write_entity():
-    return """entity sigmoid_tb is
+    
+entity sigmoid_tb is
     port ( clk: out std_logic);
 end entity ; -- sigmoid_tb
-    """
-
-
-def write_component(data_width, frac_width):
-    return """architecture behav of sigmoid_tb is
+    
+architecture behav of sigmoid_tb is
 
     component sigmoid is
         generic (
-                DATA_WIDTH : integer := {};
-                FRAC_WIDTH : integer := {}
+                DATA_WIDTH : integer := 16;
+                FRAC_WIDTH : integer := 8
             );
         port (
             x : in signed(DATA_WIDTH-1 downto 0);
             y: out signed(DATA_WIDTH-1 downto 0)
         );
     end component;
-    """.format(data_width, frac_width)
-
-
-def write_inputs():
-    return """    ------------------------------------------------------------
+    
+    ------------------------------------------------------------
     -- Testbench Internal Signals
     ------------------------------------------------------------
     signal clk_period : time := 1 ns;
     signal test_input : signed(16-1 downto 0):=(others=>'0');
     signal test_output : signed(16-1 downto 0);
-    """
-
-
-def write_clock():
-    return """begin
+    
+begin
 
     clock_process : process
     begin
@@ -48,37 +35,31 @@ def write_clock():
         clk <= '1';
         wait for clk_period/2;
     end process; -- clock_process
-    """
-
-
-def write_utt():
-    return """    utt: sigmoid
+    
+    utt: sigmoid
     port map (
     x => test_input,
     y => test_output
     );
-    """
-
-
-def write_test_process(testoutput_1281, testoutput_1000, testoutput_500):
-    return """test_process: process is
+    
+test_process: process is
     begin
         Report "======Simulation start======" severity Note;
         
         test_input <=  to_signed(-1281,16);
         wait for 1*clk_period;
         report "The value of 'test_output' is " & integer'image(to_integer(unsigned(test_output)));
-        assert test_output={} report "The test case -1281 fail" severity failure;
+        assert test_output=0 report "The test case -1281 fail" severity failure;
         
         test_input <=  to_signed(-1000,16);
         wait for 1*clk_period;
         report "The value of 'test_output' is " & integer'image(to_integer(unsigned(test_output)));
-        assert test_output={} report "The test case -1000 fail" severity failure;
+        assert test_output=4 report "The test case -1000 fail" severity failure;
         
         test_input <=  to_signed(-500,16);
         wait for 1*clk_period;
         report "The value of 'test_output' is " & integer'image(to_integer(unsigned(test_output)));
-        assert test_output={} report "The test case -500 fail" severity failure;
+        assert test_output=28 report "The test case -500 fail" severity failure;
         
         
         -- if there is no error message, that means all test case are passed.
@@ -91,25 +72,3 @@ def write_test_process(testoutput_1281, testoutput_1000, testoutput_500):
     end process; -- test_process
 
 end behav ; -- behav
-""".format(testoutput_1281, testoutput_1000, testoutput_500)
-
-
-def main():
-    with open('../testbench/sigmoid_generate_tb.vhd', 'w') as f:
-        f.write(write_imports())
-        f.write("\n")
-        f.write(write_entity())
-        f.write("\n")
-        f.write(write_component(data_width=16, frac_width=8))
-        f.write("\n")
-        f.write(write_inputs())
-        f.write("\n")
-        f.write(write_clock())
-        f.write("\n")
-        f.write(write_utt())
-        f.write("\n")
-        f.write(write_test_process(0, 4, 28))
-
-
-if __name__ == '__main__':
-    main()
