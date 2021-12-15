3224b5f337b55c20adca4cf686b5e2fe5c42c124
lukas <lukas.einhaus@uni-due.de>
2021-12-15 09:42:19 +0100

    minor refactoring

--- elasticai/creator/onnx.py
+++ elasticai/creator/onnx.py
@@ -1,46 +1,55 @@
-from typing import List
+from typing import List, Any, Callable
 
 import torch
+from torch.nn import Module
 
 from elasticai.creator.tags_utils import get_tags
 
 
 def _preprocess_tags(tags: dict):
-    processed_dic = {}
-    unprocessed_vals = []
+    processed = {}
+    unprocessed = []
     for key, value in tags.items():
         to_check = value
         if isinstance(value, List):
             to_check = value[0]
         if isinstance(to_check, int):
-            processed_dic[key + "_i"] = value
+            processed[key + "_i"] = value
         elif isinstance(to_check, float):
-            processed_dic[key + "_f"] = value
+            processed[key + "_f"] = value
         elif isinstance(to_check, str):
-            processed_dic[key + "_s"] = value
+            processed[key + "_s"] = value
         else:
-            unprocessed_vals.append(value)
-    return processed_dic, unprocessed_vals
+            unprocessed.append(value)
+    return processed, unprocessed
+
 
 class AutogradWrapper(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, input, callable):
+    def jvp(ctx: Any, *grad_inputs: Any) -> Any:
+        raise NotImplementedError
+
+    @staticmethod
+    def backward(ctx: Any, *grad_outputs: Any) -> Any:
+        raise NotImplementedError
+
+    @staticmethod
+    def forward(ctx: Any, input: Any, callable) -> Any:
         return input
 
     @staticmethod
-    def symbolic(g, x, callable):
-        tags = get_tags(callable)
-        kwargs,args= _preprocess_tags(tags)
-        ret = g.op("custom_ops::Wrapper",*args,operation_name_s = type(callable).__name__,**kwargs )
+    def symbolic(g, x, wrapped: Module):
+        tags = get_tags(wrapped)
+        kwargs, args = _preprocess_tags(tags)
+        ret = g.op("elasticai.creator::Wrapper", *args, operation_name_s=type(wrapped).__name__, **kwargs)
         return ret
-    
-    
 
-class ModuleWrapper(torch.nn.Module):
-    def __init__(self, module, autograd_fn=AutogradWrapper, ):
+
+class ModuleWrapper(Module):
+    def __init__(self, module, autograd_fn=AutogradWrapper):
         super().__init__()
         self.autograd_fn = autograd_fn
         self.module = module
 
-    def forward(self, input):
-        return self.autograd_fn.apply(input, self.module)
+    def forward(self, x):
+        return self.autograd_fn.apply(x, self.module)
--- elasticai/creator/tests/test_onnx_export.py
+++ elasticai/creator/tests/test_onnx_export.py
@@ -11,21 +11,22 @@ from elasticai.creator.tags_utils import tag
 class OnnxExportTest(unittest.TestCase):
     """
     TODO:
-      - get rid of the warnings: The shape inference of custom_ops::Wrapper type is missing, so it may result in wrong
+      - get rid of the warnings: The shape inference of elasticai.creator::Wrapper type is missing, so it may result in wrong
         shape inference for the exported graph. Please consider adding it in symbolic function.
     """
 
     def test_can_export_and_load(self):
         module = torch.nn.Sigmoid()
         module = ModuleWrapper(module)
-        expected = self.get_simple_string_representation(operation_name=type(module.module).__name__, domain="custom_ops")
+        expected = self.get_simple_string_representation(operation_name=type(module.module).__name__,
+                                                         domain="elasticai.creator")
 
         with BytesIO() as buffer:
             torch.onnx.export(module, torch.ones(1, ), buffer)
             buffer.seek(0)
             model = onnx.load(buffer)
-        self.assertEqual(expected, "{}".format(model))\
-    
+        self.assertEqual(expected, "{}".format(model))
+
     @staticmethod
     def get_simple_string_representation(operation_name, domain) -> str:
         template = """ir_version: 7
@@ -41,7 +42,7 @@ graph {{
       s: "{operation_name}"
       type: STRING
     }}
-    domain: "custom_ops"
+    domain: "elasticai.creator"
   }}
   name: "torch-jit-export"
   output {{
@@ -62,18 +63,19 @@ opset_import {{
   version: 9
 }}
 opset_import {{
-  domain: "custom_ops"
+  domain: "elasticai.creator"
   version: 1
 }}
 """
         template = template.format(operation_name=operation_name)
         return template
-    
+
     def test_with_tag_int(self):
         module = torch.nn.Sigmoid()
-        input_shape = [3,3]
-        module = ModuleWrapper(tag(module,input_shape=input_shape))
-        expected = self.get_string_representation_with_tag(operation_name=type(module.module).__name__, domain="custom_ops",input_shape="""      ints: 3
+        input_shape = [3, 3]
+        module = ModuleWrapper(tag(module, input_shape=input_shape))
+        expected = self.get_string_representation_with_tag(operation_name=type(module.module).__name__,
+                                                           input_shape="""      ints: 3
       ints: 3
       type: INTS""")
 
@@ -83,42 +85,23 @@ opset_import {{
             model = onnx.load(buffer)
         self.assertEqual(expected, "{}".format(model))
 
-        def test_with_tag_int(self):
-            module = torch.nn.Sigmoid()
-            input_shape = [3, 3]
-            module = ModuleWrapper(tag(module, input_shape=input_shape))
-            expected = self.get_string_representation_with_tag(operation_name=type(module.module).__name__,
-                                                               domain="custom_ops", input_shape="""      ints: 3
-             ints: 3
-             type: INTS""")
-
-            with BytesIO() as buffer:
-                torch.onnx.export(module, torch.ones(1, ), buffer)
-                buffer.seek(0)
-                model = onnx.load(buffer)
-            self.assertEqual(expected, "{}".format(model))
-
     def test_with_tag_float(self):
-        module = torch.nn.Sigmoid()
+        model = torch.nn.Sigmoid()
         input_shape = [3.5, 3.5]
-        module = ModuleWrapper(tag(module, input_shape=input_shape))
-        expected = self.get_string_representation_with_tag(operation_name=type(module.module).__name__,
-                                                           domain="custom_ops", input_shape="""      floats: 3.5
+        model = ModuleWrapper(tag(model, input_shape=input_shape))
+        expected = self.get_string_representation_with_tag(operation_name="Sigmoid",
+                                                           input_shape="""      floats: 3.5
       floats: 3.5
       type: FLOATS""")
 
-        with BytesIO() as buffer:
-            torch.onnx.export(module, torch.ones(1, ), buffer)
-            buffer.seek(0)
-            model = onnx.load(buffer)
-        self.assertEqual(expected, "{}".format(model))
+        self.check_stringified_onnx_model(expected_string=expected, model=model)
 
     def test_with_tag_strings(self):
         module = torch.nn.Sigmoid()
         input_shape = "abc"
         module = ModuleWrapper(tag(module, input_shape=input_shape))
         expected = self.get_string_representation_with_tag(operation_name=type(module.module).__name__,
-                                                           domain="custom_ops", input_shape="""      s: "abc"
+                                                           input_shape="""      s: "abc"
       type: STRING""")
 
         with BytesIO() as buffer:
@@ -126,10 +109,16 @@ opset_import {{
             buffer.seek(0)
             model = onnx.load(buffer)
         self.assertEqual(expected, "{}".format(model))
-        
-   
+
+    def check_stringified_onnx_model(self, expected_string, model):
+        with BytesIO() as buffer:
+            torch.onnx.export(model, torch.ones(1, ), buffer)
+            buffer.seek(0)
+            model = onnx.load(buffer)
+        self.assertEqual(expected_string, "{}".format(model))
+
     @staticmethod
-    def get_string_representation_with_tag(operation_name, domain,input_shape) -> str:
+    def get_string_representation_with_tag(operation_name, input_shape) -> str:
         template = """ir_version: 7
 producer_name: "pytorch"
 producer_version: "1.10"
@@ -147,7 +136,7 @@ graph {{
       s: "{operation_name}"
       type: STRING
     }}
-    domain: "custom_ops"
+    domain: "elasticai.creator"
   }}
   name: "torch-jit-export"
   output {{
@@ -168,11 +157,11 @@ opset_import {{
   version: 9
 }}
 opset_import {{
-  domain: "custom_ops"
+  domain: "elasticai.creator"
   version: 1
 }}
 """
-        template = template.format(operation_name=operation_name,input_shape=input_shape)
+        template = template.format(operation_name=operation_name, input_shape=input_shape)
         return template
 
 
