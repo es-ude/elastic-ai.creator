4c02e7138c9093134e28f4bd0590c9b7f71af732
sjanoest <annika.oesterdiekhoff@stud.uni-due.de>
2021-12-20 09:53:21 +0100

    rename files + remove macasync

--- elasticai/generator/tests/test_generate_sigmoid_testbench.py
+++ elasticai/generator/tests/test_generate_sigmoid_testbench.py
@@ -1,20 +1,20 @@
 import unittest
 from os.path import exists
-from elasticai.generator.generate_mac_async import main
+from elasticai.generator.generate_sigmoid_testbench import main
 
 
-class GenerateMacAsyncTestBenchTest(unittest.TestCase):
+class GenerateSigmoidTestBenchTest(unittest.TestCase):
     def setUp(self) -> None:
         main(path_to_testbench='../../testbench/')
-        self.generated_testbench_file = open('../../testbench/mac_async_tb.vhd', 'r')
-        self.real_testbench_file = open('mac_async_for_testing_tb.vhd', 'r')
+        self.generated_testbench_file = open('../../testbench/sigmoid_tb.vhd', 'r')
+        self.real_testbench_file = open('sigmoid_for_testing_tb.vhd', 'r')
 
     def tearDown(self) -> None:
         self.generated_testbench_file.close()
         self.real_testbench_file.close()
 
     def test_generate_file(self) -> None:
-        self.assertTrue(exists('../../testbench/mac_async_tb.vhd'))
+        self.assertTrue(exists('../../testbench/sigmoid_tb.vhd'))
 
     def test_equality_of_real_file_and_generated_file(self) -> None:
         for line1, line2 in zip(self.generated_testbench_file.readlines(), self.real_testbench_file.readlines()):
--- elasticai/generator/tests/test_generate_tanh_testbench.py
+++ elasticai/generator/tests/test_generate_tanh_testbench.py
@@ -1,20 +1,20 @@
 import unittest
 from os.path import exists
-from elasticai.generator.generate_sigmoid import main
+from elasticai.generator.generate_tanh_testbench import main
 
 
-class GenerateSigmoidTestBenchTest(unittest.TestCase):
+class GenerateTanhTestBenchTest(unittest.TestCase):
     def setUp(self) -> None:
         main(path_to_testbench='../../testbench/')
-        self.generated_testbench_file = open('../../testbench/sigmoid_tb.vhd', 'r')
-        self.real_testbench_file = open('sigmoid_for_testing_tb.vhd', 'r')
+        self.generated_testbench_file = open('../../testbench/tanh_tb.vhd', 'r')
+        self.real_testbench_file = open('tanh_for_testing_tb.vhd', 'r')
 
     def tearDown(self) -> None:
         self.generated_testbench_file.close()
         self.real_testbench_file.close()
 
     def test_generate_file(self) -> None:
-        self.assertTrue(exists('../../testbench/sigmoid_tb.vhd'))
+        self.assertTrue(exists('../../testbench/tanh_tb.vhd'))
 
     def test_equality_of_real_file_and_generated_file(self) -> None:
         for line1, line2 in zip(self.generated_testbench_file.readlines(), self.real_testbench_file.readlines()):
