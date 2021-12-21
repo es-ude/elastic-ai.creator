b0248849e2a8a753492af79ff527d729b08619fb
lukas <lukas.einhaus@uni-due.de>
2021-12-21 12:41:46 +0100

    remove todo about using hooks for constraints

--- elasticai/creator/constraints.py
+++ elasticai/creator/constraints.py
@@ -3,8 +3,6 @@ class WeightClipper:
     """
     This is an applied constraint meant to be used  after each batch. It clips the weights of the given module to be between -1,1.
     Meant to be used with binarization
-    TODO:
-      - use hooks to implement this
     """
 
     def __init__(self):
