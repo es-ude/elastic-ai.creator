24ff5f70214b1a178de3abeff2ed25b1759b4f54
Ahmad Husein <ahmedghost.sa@gmail.com>
2021-12-21 22:47:42 +0100

    add comments, removed unused functions, rename functions

--- elasticai/generator/vhd_strings.py
+++ elasticai/generator/vhd_strings.py
@@ -1,44 +1,16 @@
+from typing import Any, Dict, List, Iterable
 from os import path
-from elasticai.generator.generator_functions import *
 
 
-def write_entity(entity_name: str, data_width: int, frac_width: int, variables_dict: dict) -> str:
-    variables_list_str = ""
-    for variable in variables_dict:
-        variables_list_str = variables_list_str + """            {variable} : {io} signed(DATA_WIDTH-1 downto 0);\n""".format(
-            variable=variable, io=variables_dict[variable])
-    return """entity {entity_name} is
-    generic (
-            DATA_WIDTH: integer := {data_width};
-            FRAC_WIDTH: integer := {frac_width}
-    );
-    port (\n""".format(entity_name=entity_name, data_width=data_width, frac_width=frac_width) \
-           + variables_list_str[:-2] \
-           + """\n\t\t);
-end {entity_name};
-\n""".format(entity_name=entity_name)
-
-
-def write_component(data_width: any, frac_width: any, component_name: str, variables_dict: dict) -> str:
-    variables_list_str = ""
-    for variable in variables_dict:
-        variables_list_str = variables_list_str + "            {variable} : {io} signed(DATA_WIDTH-1 downto 0);\n".format(
-            variable=variable, io=variables_dict[variable])
-    # remove last linebreak and semicolon
-    return """    component {component_name} is
-        generic (
-                DATA_WIDTH : integer := {data_width};
-                FRAC_WIDTH : integer := {frac_width}
-            );
-        port (\n""".format(data_width=data_width, frac_width=frac_width,
-                           component_name=component_name) + variables_list_str[:-2] + """
-        );
-    end component {component_name};
-\n""".format(component_name=component_name)
-
-
-def write_process(component_name: str, process_name) -> str:
-    # process_name is a function in a generator_functions
+def get_process_string(component_name: str, process_name) -> str:
+    """
+    returns the string of a process Block
+    Args:
+        component_name (str): the name of the component
+        process_name (str): a function in generator_functions which generate look up table for sigmoid/tanh
+    Returns:
+        string as vhdl process Block
+    """
     return """begin 
     {component_name}_process:process(x)
     variable int_x: integer := 0;
@@ -50,18 +22,32 @@ def write_process(component_name: str, process_name) -> str:
     \n""".format(component_name=component_name, generate_process=process_name)
 
 
-# TODO : I may don't need it at all
-def get_path_file(folder_name: str, file_name: str) -> str:
+def get_file_path(folder_names: List[str], file_name: str) -> str:
+    """
+    returns String of a file path
+    Args:
+        folder_names (List(str)): the name of the folders
+        file_name (str): the name of the file
+    Returns:
+        string of the full path to a given filename
+    """
     base_path = path.dirname(__file__)
-    path_file = path.abspath(path.join(base_path, "..", folder_name, file_name))
-    return path_file
+    file_path = base_path
+    for folder_name in folder_names:
+        file_path = path.abspath(path.join(file_path, folder_name))
+    file_path = path.abspath(path.join(file_path, file_name))
+    return file_path
 
 
-def write_lstm_signals_definition(data_width: int, frac_width: int) -> str:
-    return generate_vhdl_codes_for_parameters_of_an_lstm_cell(data_width, frac_width)
-
-
-def write_input_gate(without_activation: str, with_activation: str) -> str:
+def get_input_gate_string(without_activation: str, with_activation: str) -> str:
+    """
+        returns the string of input gate
+    Args:
+        without_activation (str): name of a signal
+        with_activation (str): name of a signal
+    Returns:
+        string of input gate definition
+    """
     string = """-- Intermediate results
 -- Input gate without/with activation
 -- i = \sigma(W_{ii} x + b_{ii} + W_{hi} h + b_{hi})\n""" + (
@@ -71,7 +57,15 @@ def write_input_gate(without_activation: str, with_activation: str) -> str:
     return string
 
 
-def write_forget_gate(without_activation: str, with_activation: str) -> str:
+def get_forget_gate_string(without_activation: str, with_activation: str) -> str:
+    """
+        returns the string of forget gate definitions
+    Args:
+        without_activation (str): name of a signal
+        with_activation (str): name of a signal
+    Returns:
+        string of forget gate definition
+    """
     string = """-- Forget gate without/with activation
 -- f = \sigma(W_{if} x + b_{if} + W_{hf} h + b_{hf})\n""" + (
         """signal {without_activation} : signed(DATA_WIDTH-1 downto 0):=(others=>'0');
@@ -80,7 +74,15 @@ signal {with_activation} : signed(DATA_WIDTH-1 downto 0):=(others=>'0');\n\n""")
     return string
 
 
-def write_cell_gate(without_activation: str, with_activation: str) -> str:
+def get_cell_gate_string(without_activation: str, with_activation: str) -> str:
+    """
+        returns the string of cell gate definitions
+    Args:
+        without_activation (str): name of a signal
+        with_activation (str): name of a signal
+    Returns:
+        string of cell gate definition
+    """
     string = """-- Cell gate without/with activation
 -- g = \\tanh(W_{ig} x + b_{ig} + W_{hg} h + b_{hg})\n""" + (
         """signal {without_activation} : signed(DATA_WIDTH-1 downto 0):=(others=>'0');
@@ -89,7 +91,15 @@ signal {with_activation} : signed(DATA_WIDTH-1 downto 0):=(others=>'0');\n\n""".
     return string
 
 
-def write_output_gate(without_activation: str, with_activation: str) -> str:
+def get_output_gate_string(without_activation: str, with_activation: str) -> str:
+    """
+        returns the string of output gate definitions
+    Args:
+        without_activation (str): name of a signal
+        with_activation (str): name of a signal
+    Returns:
+        string of output gate definition
+    """
     string = """-- Output gate without/with activation
 -- o = \sigma(W_{io} x + b_{io} + W_{ho} h + b_{ho})\n""" + (
         """signal {without_activation} : signed(DATA_WIDTH-1 downto 0):=(others=>'0');
@@ -98,7 +108,15 @@ def write_output_gate(without_activation: str, with_activation: str) -> str:
     return string
 
 
-def write_new_cell_state(without_activation: str, with_activation: str) -> str:
+def get_new_cell_state_string(without_activation: str, with_activation: str) -> str:
+    """
+        returns the string of new cell state definitions
+    Args:
+        without_activation (str): name of a signal
+        with_activation (str): name of a signal
+    Returns:
+        string of output gate definition
+    """
     return """-- new_cell_state without/with activation
 -- c' = f * c + i * g 
 signal {without_activation} : signed(DATA_WIDTH-1 downto 0):=(others=>'0');
@@ -109,30 +127,28 @@ signal h_new : signed(DATA_WIDTH-1 downto 0):=(others=>'0');\n\n\n""".format(wit
                                                                              with_activation=with_activation)
 
 
-def write_architecture_signals_definition(variable_list: list) -> str:
-    string = ""
-    for variable in variable_list:
-        string += """    signal {variable} :signed(DATA_WIDTH-1 downto 0);\n""".format(variable=variable)
-    return string
-
-
-# TODO: it should be more general !
-def write_mac_async_architecture_behavior() -> str:
+# maybe it could be more general !
+def get_mac_async_architecture_behavior_string() -> str:
+    """
+    Returns:
+        string of the behavior for mac_async architecture
+    """
     return """    -- behavior: y=w1*x1+w2*x2+b
     product_1 <= shift_right((x1 * w1), FRAC_WIDTH)(DATA_WIDTH-1 downto 0);
     product_2 <= shift_right((x2 * w2), FRAC_WIDTH)(DATA_WIDTH-1 downto 0);
     y <= product_1 + product_2 + b;\n"""
 
 
-def write_signal_map(variables_dict: dict) -> str:
-    string = "    -- signals will be output to the cell\n"
-    for variable in variables_dict:
-        string += """    {val1} <= {val2};\n""".format(val1=variable, val2=variables_dict[variable])
-    return string + "\n"
-
-
-def write_port_map(map_name: str, component_name: str, signals) -> str:
-    # the typ of signals can be list or dict !!
+def get_port_map_string(map_name: str, component_name: str, signals) -> str:
+    """
+    returns port map string
+    Args:
+        map_name (str): the name of the map
+        component_name(str): the name of the component
+        signals(Dict/List): dictionary with the name of each signal and its definition / List of each signal
+    Returns:
+        string of port map
+    """
     string = """    {map_name}: {component_name}\n    port map (\n""".format(map_name=map_name,
                                                                              component_name=component_name)
     if type(signals) == list:
@@ -148,8 +164,12 @@ def write_port_map(map_name: str, component_name: str, signals) -> str:
     return string
 
 
-# TODO: change this
-def write_lstm_process() -> str:
+# maybe it could be more general !
+def get_lstm_process_string() -> str:
+    """
+    Returns:
+        string of lstm_process
+    """
     return """    H_OUT_PROCESS: process(o,c_new)
     begin
         h_new <= shift_right((o*c_new), FRAC_WIDTH)(DATA_WIDTH-1 downto 0);
