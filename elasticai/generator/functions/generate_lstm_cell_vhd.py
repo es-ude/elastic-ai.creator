e77c1debd737f623f68947300a0123b0c0d17830
Ahmad Husein <ahmedghost.sa@gmail.com>
2021-12-21 22:34:33 +0100

    modify to a general funtions

--- elasticai/generator/functions/generate_lstm_cell_vhd.py
+++ elasticai/generator/functions/generate_lstm_cell_vhd.py
@@ -1,5 +1,6 @@
 from elasticai.generator.vhd_strings import *
 from elasticai.generator.general_strings import *
+from elasticai.generator.generator_functions import generate_signal_definitions_for_lstm
 
 component_name = "lstm_cell"
 file_name = component_name + ".vhd"
@@ -10,7 +11,10 @@ FRAC_WIDTH = 8
 
 
 def main():
-    with open(get_path_file("source", "generated_" + file_name), "w") as writer:
+    file_path = get_file_path(folder_names=["..", "source"],
+                              file_name="generated_" + file_name)
+
+    with open(file_path, "w") as writer:
         writer.write(get_libraries_string(work_lib=True))
         writer.write(get_entity_or_component_string(
             entity_or_component="entity",
@@ -25,13 +29,19 @@ def main():
                 "h_out": "out signed(DATA_WIDTH-1 downto 0)"}))
         # architecture structure
         writer.write(get_architecture_header_string(architecture_name=architecture_name, component_name=component_name))
-        # TODO: check if it can use the general signal definition?
-        writer.write(write_lstm_signals_definition(data_width=DATA_WIDTH, frac_width=FRAC_WIDTH))
-        writer.write(write_input_gate(without_activation="i_wo_activation", with_activation="i"))
-        writer.write(write_forget_gate(without_activation="f_wo_activation", with_activation="f"))
-        writer.write(write_cell_gate(without_activation="g_wo_activation", with_activation="g"))
-        writer.write(write_output_gate(without_activation="o_wo_activation", with_activation="o"))
-        writer.write(write_new_cell_state(without_activation="c_new", with_activation="c_new_wo_activation"))
+        # generate lstm signal definitions
+        lstm_signal_definitions = generate_signal_definitions_for_lstm(data_width=DATA_WIDTH, frac_width=FRAC_WIDTH)
+        writer.write(get_signal_definitions_string(lstm_signal_definitions))
+        writer.write(get_input_gate_string(without_activation="i_wo_activation",
+                                           with_activation="i"))
+        writer.write(get_forget_gate_string(without_activation="f_wo_activation",
+                                            with_activation="f"))
+        writer.write(get_cell_gate_string(without_activation="g_wo_activation",
+                                          with_activation="g"))
+        writer.write(get_output_gate_string(without_activation="o_wo_activation",
+                                            with_activation="o"))
+        writer.write(get_new_cell_state_string(without_activation="c_new",
+                                               with_activation="c_new_wo_activation"))
         # components
         writer.write(get_entity_or_component_string(
             entity_or_component="component",
@@ -61,35 +71,71 @@ def main():
             variables_dict={"x": "in signed(DATA_WIDTH-1 downto 0)", "y": "out signed(DATA_WIDTH-1 downto 0)"}
         ))
         # architecture behavior
-        writer.write(get_begin_architecture_string())
-        # TODO: maybe use variable definition from testbench strings? if so, move them to the general strings
-        writer.write(write_signal_map({"c_out": "c_new_wo_activation", "h_out": "h_new"}))
-        writer.write(write_port_map(map_name="FORGET_GATE_MAC", component_name="mac_async",
-                                    signals={"x1": "x", "x2": "h_in", "w1": "wif", "w2": "whf", "b": "bf",
-                                             "y": "f_wo_activation"}))  #
-        writer.write(write_port_map(map_name="FORGET_GATE_SIGMOID", component_name="sigmoid",
-                                    signals=["f_wo_activation", "f"]))
-        writer.write(write_port_map(map_name="INPUT_GATE_MAC", component_name="mac_async",
-                                    signals={"x1": "x", "x2": "h_in", "w1": "wii", "w2": "whi", "b": "bi",
-                                             "y": "i_wo_activation"}))
-        writer.write(write_port_map(map_name="INPUT_GATE_SIGMOID", component_name="sigmoid",
-                                    signals=["i_wo_activation", "i"]))
-        writer.write(write_port_map(map_name="CELL_GATE_MAC", component_name="mac_async",
-                                    signals={"x1": "x", "x2": "h_in", "w1": "wig", "w2": "whg", "b": "bg",
-                                             "y": "g_wo_activation"}))
-        writer.write(write_port_map(map_name="CELL_GATE_TANH", component_name="tanh",
-                                    signals=["g_wo_activation", "g"]))
-        writer.write(write_port_map(map_name="NEW_CELL_STATE_MAC", component_name="mac_async",
-                                    signals={"x1": "f", "x2": "i", "w1": "c_in", "w2": "g", "b": "(others=>'0')",
-                                             "y": "c_new_wo_activation"}))
-        writer.write(write_port_map(map_name="NEW_CELL_STATE_TANH", component_name="tanh",
-                                    signals=["c_new_wo_activation", "c_new"]))
-        writer.write(write_port_map(map_name="MAC_ASYNC_4", component_name="mac_async",
-                                    signals={"x1": "x", "x2": "h_in", "w1": "wio", "w2": "who", "b": "bo",
-                                             "y": "o_wo_activation"}))
-        writer.write(write_port_map(map_name="SIGMOID_1", component_name="sigmoid",
-                                    signals={"x": "o_wo_activation", "y": "o"}))
-        writer.write(write_lstm_process())
+        writer.write(get_architecture_begin_string())
+        writer.write(get_variable_definitions_string({"c_out": "c_new_wo_activation",
+                                                      "h_out": "h_new"}))
+        # port mapping
+        writer.write(get_port_map_string(map_name="FORGET_GATE_MAC",
+                                         component_name="mac_async",
+                                         signals={"x1": "x",
+                                                  "x2": "h_in",
+                                                  "w1": "wif",
+                                                  "w2": "whf",
+                                                  "b": "bf",
+                                                  "y": "f_wo_activation"}))
+        writer.write(get_port_map_string(map_name="FORGET_GATE_SIGMOID",
+                                         component_name="sigmoid",
+                                         signals=["f_wo_activation",
+                                                  "f"]))
+        writer.write(get_port_map_string(map_name="INPUT_GATE_MAC",
+                                         component_name="mac_async",
+                                         signals={"x1": "x",
+                                                  "x2": "h_in",
+                                                  "w1": "wii",
+                                                  "w2": "whi",
+                                                  "b": "bi",
+                                                  "y": "i_wo_activation"}))
+        writer.write(get_port_map_string(map_name="INPUT_GATE_SIGMOID",
+                                         component_name="sigmoid",
+                                         signals=["i_wo_activation",
+                                                  "i"]))
+        writer.write(get_port_map_string(map_name="CELL_GATE_MAC",
+                                         component_name="mac_async",
+                                         signals={"x1": "x",
+                                                  "x2": "h_in",
+                                                  "w1": "wig",
+                                                  "w2": "whg",
+                                                  "b": "bg",
+                                                  "y": "g_wo_activation"}))
+        writer.write(get_port_map_string(map_name="CELL_GATE_TANH",
+                                         component_name="tanh",
+                                         signals=["g_wo_activation",
+                                                  "g"]))
+        writer.write(get_port_map_string(map_name="NEW_CELL_STATE_MAC",
+                                         component_name="mac_async",
+                                         signals={"x1": "f",
+                                                  "x2": "i",
+                                                  "w1": "c_in",
+                                                  "w2": "g",
+                                                  "b": "(others=>'0')",
+                                                  "y": "c_new_wo_activation"}))
+        writer.write(get_port_map_string(map_name="NEW_CELL_STATE_TANH",
+                                         component_name="tanh",
+                                         signals=["c_new_wo_activation",
+                                                  "c_new"]))
+        writer.write(get_port_map_string(map_name="MAC_ASYNC_4",
+                                         component_name="mac_async",
+                                         signals={"x1": "x",
+                                                  "x2": "h_in",
+                                                  "w1": "wio",
+                                                  "w2": "who",
+                                                  "b": "bo",
+                                                  "y": "o_wo_activation"}))
+        writer.write(get_port_map_string(map_name="SIGMOID_1",
+                                         component_name="sigmoid",
+                                         signals={"x": "o_wo_activation",
+                                                  "y": "o"}))
+        writer.write(get_lstm_process_string())
         writer.write(get_architecture_end_string(architecture_name=architecture_name))
 
 
