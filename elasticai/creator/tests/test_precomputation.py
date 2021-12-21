086bb85db96cb0f1892c466b0dbf6e7a9717e197
lukas <lukas.einhaus@uni-due.de>
2021-12-21 09:15:35 +0100

    clean up test list

--- elasticai/creator/tests/test_precomputation.py
+++ elasticai/creator/tests/test_precomputation.py
@@ -1,20 +1,3 @@
-"""
-Test list:
-- make sure that the create_input_data functions from elasticai.creator.derive_datasets
-  are used correctly
-- make sure precomputations are serializable and loadable
-- depthwise convolutions are handled correctly
-
-Extra:
-- compare given input_domains shape and rank to the input_shape and reuse and
-  use input_domain's elements to build a set of tensors of shape input_shape. E.g.:
-  input_shape = (2, 2)
-  input_domain = [(1, 1), (-1, 1)]
-  expected_result = [((1, 1), (1, 1)),
-                     ((1, 1), (-1, 1)),
-                     ((-1, 1), (1, 1)),
-                     ((-1, 1), (-1, 1))]
-"""
 import json
 from collections import Iterable
 
@@ -71,3 +54,4 @@ class PrecomputationTest(TensorTestCase):
         actual = json.dumps(precompute, cls=JSONEncoder)
         expected = """{"description": [], "shape": [1, 1], "x": [[[1]]], "y": [[[1]]]}"""
         self.assertEqual(expected, actual)
+
