a1b16e793dd73b9989cd49ad7ee1ad917137c528
lukas <lukas.einhaus@uni-due.de>
2021-12-24 14:13:43 +0100

    add comment and exception to fixedpointconverter

--- elasticai/creator/tests/vhdl/test_number_representations.py
+++ elasticai/creator/tests/vhdl/test_number_representations.py
@@ -1,24 +1,24 @@
 from unittest import TestCase
-from elasticai.creator.vhdl.number_representations import FixedPointBuilder
+from elasticai.creator.vhdl.number_representations import FixedPointConverter
 
 
 class FixedPointTest(TestCase):
     def test_get_zero(self):
-        f = FixedPointBuilder(bits_used_for_fraction=0)
-        self.assertEqual(0, f.build(0))
+        f = FixedPointConverter(bits_used_for_fraction=0)
+        self.assertEqual(0, f.from_float(0))
 
     def test_get_one_with_2bits_for_fraction(self):
-        f = FixedPointBuilder(bits_used_for_fraction=2)
-        self.assertEqual(1 << 2, f.build(1))
+        f = FixedPointConverter(bits_used_for_fraction=2)
+        self.assertEqual(1 << 2, f.from_float(1))
 
     def test_get_one_with_3bits_for_fraction(self):
-        f = FixedPointBuilder(bits_used_for_fraction=3)
-        self.assertEqual(1 << 3, f.build(1))
+        f = FixedPointConverter(bits_used_for_fraction=3)
+        self.assertEqual(1 << 3, f.from_float(1))
 
     def test_raise_error_if_not_convertible(self):
-        f = FixedPointBuilder(bits_used_for_fraction=0)
+        f = FixedPointConverter(bits_used_for_fraction=0)
         try:
-            f.build(0.5)
+            f.from_float(0.5)
             self.fail()
         except ValueError as e:
             self.assertEqual(
--- elasticai/creator/vhdl/number_representations.py
+++ elasticai/creator/vhdl/number_representations.py
@@ -1,4 +1,12 @@
-class FixedPointBuilder:
+class FixedPointConverter:
+    """
+    Create a fixed point representation as an int data type.
+
+    We might want to have this create its own type `FixedPointNumber` in
+    the future. That way we could make sure that the conversion is idempotent
+    for numbers that are fixed point already.
+    """
+
     def __init__(self, bits_used_for_fraction: int):
         self.bits_used_for_fraction = bits_used_for_fraction
 
@@ -6,7 +14,7 @@ class FixedPointBuilder:
     def one(self) -> int:
         return 1 << self.bits_used_for_fraction
 
-    def build(self, x: float) -> int:
+    def from_float(self, x: float) -> int:
         x_tmp = float(x)
         x_tmp = x_tmp * self.one
         if not x_tmp.is_integer():
