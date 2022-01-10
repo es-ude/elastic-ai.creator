080297d2f8c5dd7d866e4cf92093a4619fecec8e
Lukas Einhaus <lukas.einhaus@uni-due.de>
2022-01-10 17:34:53 +0100

    minor refactoring

---
+++ elasticai/creator/tests/vhdl/test_naive_lut_based_conv.py
@@ -0,0 +1,31 @@
+from elasticai.creator.vhdl.generator.component import NaiveLUTBasedConv
+from unittest import TestCase
+
+
+class TestNaiveLUTBasedConv(TestCase):
+    def test_basic_instantiation(self):
+        generate_conv = NaiveLUTBasedConv(
+            implements="implemented_entity",
+            name="my_architecture",
+            inputs=[],
+            outputs=[]
+        )
+        code = list(generate_conv())
+        expected = [
+            "architecture my_architecture of implemented_entity is",
+            "begin",
+            "end my_architecture;"
+        ]
+        self.assertEqual(expected, code)
+
+    def test_one_bit_output_version(self):
+        generate_conv = NaiveLUTBasedConv(
+            implements="",
+            name="",
+            inputs=["110"],
+            outputs=["0"]
+        )
+        expected = ['\toutput <= "0" when input = "110";']
+        code = list(generate_conv())
+        code = code[2:]
+        self.assertEqual(expected, code)
