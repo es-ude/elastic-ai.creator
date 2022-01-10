3ce3c29c4c81bebdccec909befd3980809966734
lukas <lukas.einhaus@uni-due.de>
2022-01-10 11:29:33 +0100

    add numeric to logic encoder

--- elasticai/creator/vhdl/generator/component.py
+++ elasticai/creator/vhdl/generator/component.py
@@ -1,12 +1,12 @@
 from abc import ABC, abstractmethod
 from functools import partial
 from itertools import chain
+from typing import Any, Callable
 
 import torch.nn
 
 from elasticai.creator.vhdl.generator.general_strings import (
     get_libraries_string,
-    get_entity_or_component_string,
     get_architecture_header_string,
     get_architecture_end_string,
 )
@@ -19,17 +19,17 @@ from elasticai.creator.vhdl.language import (
     Entity,
     InterfaceVariable,
     DataType,
+    Code,
 )
 
 
 class Component(ABC):
-    def __init__(self, data_width, frac_width, x, component_name=None):
+    def __init__(self, input_domain, component_name=None):
         self.component_name = self._get_lower_case_class_name_or_component_name(
             component_name=component_name
         )
-        self.data_width = data_width
-        self.frac_width = frac_width
-        self.x = x
+
+        self.input_domain = input_domain
 
     @classmethod
     def _get_lower_case_class_name_or_component_name(cls, component_name):
@@ -50,6 +50,13 @@ class Component(ABC):
         pass
 
 
+class FixedPointComponent(Component, ABC):
+    def __init__(self, data_width, frac_width, input_domain, component_name=None):
+        super().__init__(input_domain, component_name)
+        self.data_width = data_width
+        self.frac_width = frac_width
+
+
 class DataWidthVariable(InterfaceVariable):
     def __init__(self, value: int):
         super().__init__(
@@ -64,7 +71,7 @@ class FracWidthVariable(InterfaceVariable):
         )
 
 
-class Sigmoid(Component):
+class Sigmoid(FixedPointComponent):
     def build(self) -> str:
         entity = Entity(self.component_name)
         entity.generic_list = [
@@ -82,14 +89,14 @@ class Sigmoid(Component):
         code += get_process_string(
             component_name=self.component_name,
             lookup_table_generator_function=sigmoid_process(
-                x_list=torch.as_tensor(self.x), function=torch.nn.Sigmoid()
+                x_list=torch.as_tensor(self.input_domain), function=torch.nn.Sigmoid()
             ),
         )
         code += get_architecture_end_string(architecture_name=self.architecture_name)
         return code
 
 
-class Tanh(Component):
+class Tanh(FixedPointComponent):
     def build(self) -> str:
         code = ""
         entity = Entity(self.component_name)
@@ -112,7 +119,7 @@ class Tanh(Component):
             partial(
                 get_process_string,
                 component_name=self.component_name,
-                lookup_table_generator_function=tanh_process(self.x),
+                lookup_table_generator_function=tanh_process(self.input_domain),
             ),
             partial(
                 get_architecture_end_string, architecture_name=self.architecture_name
@@ -121,3 +128,11 @@ class Tanh(Component):
         for function in string_builders:
             code += function()
         return code
+
+
+class NaiveLUTConv(Component):
+    def __init__(self, input_domain: Any, software_conv: Callable[[Any], Any]):
+        super().__init__(input_domain=input_domain)
+
+    def _build_body(self) -> Code:
+        inputs, outputs = self.io_table
--- elasticai/creator/vhdl/number_representations.py
+++ elasticai/creator/vhdl/number_representations.py
@@ -1,4 +1,7 @@
-from typing import Union, overload
+from abc import ABC, abstractmethod
+from functools import partial
+from numbers import Integral, Rational
+from typing import Union, Iterable, Any, overload
 
 
 class FloatToSignedFixedPointConverter:
@@ -45,11 +48,48 @@ class FloatToBinaryFixedPointStringConverter:
         return two_complements_representation(signed_fixed_point, self.total_bit_width)
 
 
+def _int_to_bin_str(number: int, bits: int) -> str:
+    return "{{0:0{number_of_bits}b}}".format(number_of_bits=bits).format(number)
+
+
 def two_complements_representation(x, number_of_bits):
     if x < 0:
         unsigned_int_version = (1 << number_of_bits) + x
     else:
         unsigned_int_version = x
-    return "{{0:0{number_of_bits}b}}".format(number_of_bits=number_of_bits).format(
-        unsigned_int_version
-    )
+    return _int_to_bin_str(x, number_of_bits)
+
+
+class ToLogicEncoder:
+    """
+    Throughout our implementations we have to deal with two different levels of representations for numbers:
+    During training we typically need to apply mathematical operations and we do not care too much about how our numbers are encoded.
+    E.g. in a scenario where we want to use two bit on hardware to represent our numbers, in our machine learning framework we
+    might decide it is beneficial to use the numbers -3 and 4 for some reason. However, especially in the context of precomputed
+    results, the hardware implementation does not need to know the numeric values, but instead just needs to be able to keep a
+    consistent and correct mapping. The NumericToLogicEncoder takes care of performing the translations from numeric representation
+    to the bit vector used in the hardware implementation. We encode bit vectors just as unsigned integers.
+    """
+
+    def __init__(self):
+        self.numerics = set()
+        self.mapping = dict()
+
+    def add_numeric(self, numeric_representation: int) -> None:
+        self.numerics.add(numeric_representation)
+        self._update_mapping()
+
+    def _update_mapping(self) -> None:
+        length = len(self.numerics)
+        sorted_numerics = list(self.numerics)
+        sorted_numerics.sort()
+        mapping = dict(((value, index) for index, value in enumerate(sorted_numerics)))
+        self.mapping.update(mapping)
+
+    def __getitem__(self, item: int) -> int:
+        return self.mapping[item]
+
+
+class BitVector:
+    def __init__(self, number: int):
+        self._number = int
