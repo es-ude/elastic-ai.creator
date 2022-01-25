eeaa54ad218a9fd094f0976e083d66ffdff0e1ec
lukas <lukas.einhaus@uni-due.de>
2022-01-25 12:05:09 +0100

    add unit tests and docs for metaprogramming module
    This should allow others to continue development

---
+++ elasticai/creator/tests/test_metaprogramming.py
@@ -0,0 +1,36 @@
+import unittest
+from abc import abstractmethod
+from typing import Protocol, runtime_checkable
+from unittest import TestCase
+
+from elasticai.creator.metaprogramming import add_method, add_instance_attribute
+
+
+class TestWrappingClasses(TestCase):
+    @unittest.SkipTest
+    def test_method(self):
+        def my_method(self) -> list[int]:
+            return [1]
+
+        @add_method(method_name="my_method", method=my_method)
+        class A:
+            ...
+
+        @runtime_checkable
+        class P(Protocol):
+            @abstractmethod
+            def my_method(self) -> list[int]:
+                ...
+
+        # below assignment should be correct for static type analysis
+        a: P = A()
+
+        self.assertTrue(isinstance(a, P))
+
+    def test_attribute(self):
+        @add_instance_attribute(attribute_name="my_attribute", attribute=4)
+        class A:
+            ...
+
+        self.assertTrue(not hasattr(A, "my_attribute"))
+        self.assertEqual(4, A().my_attribute)
