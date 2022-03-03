fe9ae877ef9e08b39576276f2d5db2f0e514563c
Ahmad Husein <ahmedghost.sa@gmail.com>
2022-03-03 14:52:54 +0100

    separate generating the file from building mac_async

---
+++ elasticai/creator/vhdl/generator/mac_async.py
@@ -0,0 +1,117 @@
+from itertools import chain
+from typing import Iterable
+
+from elasticai.creator.vhdl.generator.vhd_strings import get_mac_async_architecture_behavior_string
+from elasticai.creator.vhdl.language import (
+    ContextClause,
+    LibraryClause,
+    UseClause,
+    Entity,
+    InterfaceVariable,
+    DataType,
+    Mode,
+    Architecture,
+    InterfaceSignal
+)
+
+
+class MacAsync:
+    def __init__(self, component_name, data_width, frac_width):
+        self.component_name = component_name
+        self.architecture_name = "{component_name}_rtl".format(component_name=self.component_name)
+        self.data_width = data_width
+        self.frac_width = frac_width
+
+    def __call__(self) -> Iterable[str]:
+        library = ContextClause(
+            library_clause=LibraryClause(logical_name_list=["ieee"]),
+            use_clause=UseClause(
+                selected_names=[
+                    "ieee.std_logic_1164.all",
+                    "ieee.numeric_std.all",
+                ]
+            ),
+        )
+        entity = Entity(self.component_name)
+        entity.generic_list.append(
+            InterfaceVariable(
+                identifier="DATA_WIDTH", identifier_type=DataType.INTEGER, value=self.data_width
+            )
+        )
+        entity.generic_list.append(
+            InterfaceVariable(
+                identifier="FRAC_WIDTH", identifier_type=DataType.INTEGER, value=self.frac_width
+            )
+        )
+        entity.port_list.append(
+            InterfaceVariable(
+                identifier="x1",
+                mode=Mode.IN,
+                range="DATA_WIDTH-1 downto 0",
+                identifier_type=DataType.SIGNED,
+            )
+        )
+        entity.port_list.append(
+            InterfaceVariable(
+                identifier="x2",
+                mode=Mode.IN,
+                range="DATA_WIDTH-1 downto 0",
+                identifier_type=DataType.SIGNED,
+            )
+        )
+        entity.port_list.append(
+            InterfaceVariable(
+                identifier="w1",
+                mode=Mode.IN,
+                range="DATA_WIDTH-1 downto 0",
+                identifier_type=DataType.SIGNED,
+            )
+        )
+        entity.port_list.append(
+            InterfaceVariable(
+                identifier="w2",
+                mode=Mode.IN,
+                range="DATA_WIDTH-1 downto 0",
+                identifier_type=DataType.SIGNED,
+            )
+        )
+        entity.port_list.append(
+            InterfaceVariable(
+                identifier="b",
+                mode=Mode.IN,
+                range="DATA_WIDTH-1 downto 0",
+                identifier_type=DataType.SIGNED,
+            )
+        )
+        entity.port_list.append(
+            InterfaceVariable(
+                identifier="y",
+                mode=Mode.OUT,
+                range="DATA_WIDTH-1 downto 0",
+                identifier_type=DataType.SIGNED,
+            )
+        )
+
+        architecture = Architecture(
+            identifier=self.architecture_name,
+            design_unit=self.component_name,
+        )
+        architecture.architecture_statement_part = (
+            get_mac_async_architecture_behavior_string
+        )
+        architecture.architecture_declaration_list.append(
+            InterfaceSignal(
+                identifier="product_1",
+                range="DATA_WIDTH-1 downto 0",
+                identifier_type=DataType.SIGNED,
+            )
+        )
+        architecture.architecture_declaration_list.append(
+            InterfaceSignal(
+                identifier="product_2",
+                range="DATA_WIDTH-1 downto 0",
+                identifier_type=DataType.SIGNED,
+            )
+        )
+        code = chain(chain(library(), entity()), architecture())
+        return code
