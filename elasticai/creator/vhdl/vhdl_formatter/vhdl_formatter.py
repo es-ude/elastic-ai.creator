57e4df1faa9e21c645d8a8a103341ecd4c1c5f48
Ahmad Husein <ahmedghost.sa@gmail.com>
2022-03-15 18:30:28 +0100

    add vhdl formatter

---
+++ elasticai/creator/vhdl/vhdl_formatter/vhdl_formatter.py
@@ -0,0 +1,28 @@
+import os
+import subprocess
+
+from elasticai.creator.vhdl.generator.generator_functions import get_file_path_string
+
+"""
+    we are using vsg (VHDL Style Guide) for indenting the generated vhdl files
+    config.json defines the indentation size, since the default indentation size is 2 spaces
+    
+    source : 
+        https://github.com/jeremiah-c-leary/vhdl-style-guide/tree/master
+"""
+
+
+def format_vhdl(file_path):
+    # get the path of config
+    config = get_file_path_string(["..", "vhdl_formatter"], file_name="config.json")
+    # check if the vhdl file exist !
+    if os.path.isfile(file_path):
+        subprocess.Popen(
+            "vsg -f {file_path} --style indent_only --configuration {config} --fix".format(
+                file_path=file_path,
+                config=config
+            ),
+            shell=True,
+        )
+    else:
+        print("no such a file")
