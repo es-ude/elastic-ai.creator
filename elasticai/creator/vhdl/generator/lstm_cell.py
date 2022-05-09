ff38021e5b17f42062a46a8ff2d881e70cbcb9ae
Julian Hoever <julianhoever@posteo.de>
2022-05-09 20:52:38 +0200

    Use resource_utils

--- elasticai/creator/vhdl/generator/lstm_cell.py
+++ elasticai/creator/vhdl/generator/lstm_cell.py
@@ -1,6 +1,6 @@
-import importlib.resources
 from itertools import filterfalse
 
+from elasticai.creator.resource_utils import read_text
 from elasticai.creator.vhdl.generator.generator_functions import generate_signal_definitions_for_lstm
 from elasticai.creator.vhdl.language import Code
 
@@ -12,7 +12,7 @@ class LstmCell:
         self.frac_width = frac_width
 
     def __call__(self) -> Code:
-        template = importlib.resources.read_text("elasticai.creator.vhdl.generator.templates", "lstm_cell.tpl.vhd")
+        template = read_text("elasticai.creator.vhdl.generator.templates", "lstm_cell.tpl.vhd")
         signal_defs = generate_signal_definitions_for_lstm(data_width=self.data_width, frac_width=self.frac_width)
         parameters = signal_defs | {'data_width': self.data_width, 'frac_width': self.frac_width}
         code = template.format(**parameters)
