af2c705f45e6877139b0a3ca5ab110159fc4f0f1
Lukas Einhaus <lukas.einhaus@uni-due.de>
2022-05-20 17:24:30 +0200

    fix: add missing tags_utils again

---
+++ elasticai/creator/tags_utils.py
@@ -0,0 +1,60 @@
+from typing import Any, Iterable, Protocol, TypeVar, runtime_checkable
+
+from torch import Tensor
+
+T = TypeVar("T")
+
+
+@runtime_checkable
+class Tagged(Protocol):
+    def elasticai_tags(self) -> dict:
+        pass
+
+
+@runtime_checkable
+class ModuleProto(Protocol):
+    @property
+    def training(self) -> bool:
+        pass
+
+    def extra_repr(self) -> str:
+        pass
+
+    def named_children(self) -> Iterable[tuple[str, "ModuleProto"]]:
+        pass
+
+    def __call__(self, x: Tensor, *args, **kwargs) -> Tensor:
+        pass
+
+
+@runtime_checkable
+class TaggedModule(ModuleProto, Tagged, Protocol):
+    pass
+
+
+def tag(module: ModuleProto, **new_tags: Any) -> TaggedModule:
+    """Add tags to any object wrapping it in a TagWrapper if necessary
+
+    new_tags will override possibly existing tags
+    """
+    old_tags = get_tags(module)
+    tags = old_tags | new_tags
+    module._elasticai_tags = tags
+
+    def get_tags_method(self=module) -> dict:
+        return self._elasticai_tags
+
+    module.elasticai_tags = get_tags_method
+    return module
+
+
+def get_tags(module: Any) -> dict:
+    if isinstance(module, Tagged):
+        return module.elasticai_tags()
+    return {}
+
+
+def has_tag(module: Any, tag: str) -> bool:
+    if isinstance(module, Tagged):
+        return tag in module.elasticai_tags()
+    return False
