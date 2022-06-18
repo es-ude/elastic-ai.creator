4fcf0d16cdcac082c19dd654210b5d37991f9139
Julian Hoever <julianhoever@posteo.de>
2022-06-19 00:44:40 +0200

    refactor(vhdl): remove no longer needed fixed point converter classes

--- elasticai/creator/vhdl/number_representations.py
+++ elasticai/creator/vhdl/number_representations.py
@@ -1,7 +1,7 @@
 import math
 from collections.abc import Sequence
 from itertools import chain
-from typing import Any, Iterable, Iterator, Union
+from typing import Any, Iterable, Iterator
 
 
 class FixedPoint:
@@ -205,98 +205,12 @@ def int_values_to_fixed_point(
     return list(map(lambda x: FixedPoint.from_int(x, total_bits, frac_bits), values))
 
 
-class FloatToSignedFixedPointConverter:
-    """
-    Create a fixed point representation as an unsigned int data type using two complements.
-
-    We might want to have this create its own type `FixedPointNumber` in
-    the future. That way we could make sure that the conversion is idempotent
-    for numbers that are fixed point already.
-    """
-
-    def __init__(self, bits_used_for_fraction: int, strict=True):
-        self.bits_used_for_fraction = bits_used_for_fraction
-        self._strict = strict
-
-    @property
-    def one(self) -> int:
-        return 1 << self.bits_used_for_fraction
-
-    def __call__(self, x: float) -> int:
-        x_tmp = float(x)
-        x_tmp = x_tmp * self.one
-        if self._strict and not x_tmp.is_integer():
-            raise ValueError(
-                f"{x} not convertible to fixed point number using {self.bits_used_for_fraction} bits for fractional part"
-            )
-        return int(x_tmp)
-
-    def to_string(self, x: float) -> str:
-        return str(self.__call__(x))
-
-
-class FloatToBinaryFixedPointStringConverter:
-    def __init__(
-        self,
-        total_bit_width: int,
-        as_signed_fixed_point: FloatToSignedFixedPointConverter,
-    ):
-        self.total_bit_width = total_bit_width
-        self.as_signed_fixed_point = as_signed_fixed_point
-
-    def __call__(self, x: Union[float, int]) -> str:
-        signed_fixed_point = self.as_signed_fixed_point(x)
-        return two_complements_representation(signed_fixed_point, self.total_bit_width)
-
-
-class FloatToHexFixedPointStringConverter:
-    def __init__(
-        self,
-        total_bit_width: int,
-        as_signed_fixed_point: FloatToSignedFixedPointConverter,
-    ):
-        self.total_bit_width = total_bit_width
-        self.as_signed_fixed_point = as_signed_fixed_point
-
-    def __call__(self, x: Union[float, int]) -> str:
-        signed_fixed_point = self.as_signed_fixed_point(x)
-        return hex_representation(signed_fixed_point, self.total_bit_width)
-
-
 def _int_to_bin_str(number: int, bits: int) -> str:
     if number < 0:
         raise ValueError("Negative values are not supported.")
     if bits <= 0 or (number > 0 and math.log2(number) > bits):
         raise ValueError(f"The number {number} cannot be represented with {bits} bits.")
     return "{{0:0{number_of_bits}b}}".format(number_of_bits=bits).format(number)
-
-
-def _int_to_hex_str(number: int, bits: int) -> str:
-    if number < 0:
-        raise ValueError("Negative values are not supported.")
-    if bits <= 0 or (number > 0 and math.log2(number) > bits):
-        raise ValueError(f"The number {number} cannot be represented with {bits} bits.")
-    return 'x"{{:0{number_of_bits}x}}"'.format(
-        number_of_bits=math.ceil(bits / 4)
-    ).format(number)
-
-
-def _get_unsigned_int_version(x, number_of_bits):
-    if x < 0:
-        unsigned_int_version = (1 << number_of_bits) + x
-    else:
-        unsigned_int_version = x
-    return unsigned_int_version
-
-
-def hex_representation(x: int, num_bits: int) -> str:
-    unsigned_int_version = _get_unsigned_int_version(x, num_bits)
-    return _int_to_hex_str(unsigned_int_version, num_bits)
-
-
-def two_complements_representation(x, num_bits):
-    unsigned_int_version = _get_unsigned_int_version(x, num_bits)
-    return _int_to_bin_str(unsigned_int_version, num_bits)
 
 
 class ToLogicEncoder:
