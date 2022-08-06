7885cdb30a413070816442c0e6daf2c0400b2743
Julian Hoever <julianhoever@posteo.de>
2022-08-06 11:01:02 +0200

    test(vhdl): fixed tests of the build function mapping

--- elasticai/creator/tests/vhdl/translator/test_build_function_mapping.py
+++ elasticai/creator/tests/vhdl/translator/test_build_function_mapping.py
@@ -2,6 +2,7 @@ import unittest
 from dataclasses import dataclass
 from typing import Any
 
+import elasticai.creator.qat.layers as qtorch
 from elasticai.creator.vhdl.language import Code
 from elasticai.creator.vhdl.translator.abstract.translatable import Translatable
 from elasticai.creator.vhdl.translator.build_function_mapping import (
@@ -39,37 +40,26 @@ def mock_build_function3(layer: Any) -> Translatable:
     return MockTranslatable([MockVHDLComponent()])
 
 
-@dataclass
-class MockLayer1:
-    x = 1
-
-
-@dataclass
-class MockLayer2:
-    x = 2
-
-
-@dataclass
-class MockLayer3:
-    x = 3
-
-
 class BuildFunctionMappingTest(unittest.TestCase):
     def setUp(self) -> None:
+        self.linear_layer = qtorch.QLinear(1, 1, qtorch.Binarize())
+        self.conv1d_layer = qtorch.QConv1d(1, 1, (1,), qtorch.Binarize())
+        self.conv2d_layer = qtorch.QConv2d(1, 1, (1, 1), qtorch.Binarize())
+
         self.mapping_dict = {
-            "test_build_function_mapping.MockLayer1": mock_build_function1,
-            "test_build_function_mapping.MockLayer2": mock_build_function2,
+            "torch.nn.utils.parametrize.ParametrizedQLinear": mock_build_function1,
+            "torch.nn.utils.parametrize.ParametrizedQConv1d": mock_build_function2,
         }
+
         self.mapping = BuildFunctionMapping(mapping=self.mapping_dict)
 
     def test_get_from_layer_existing_key(self) -> None:
-        self.assertEqual(mock_build_function1, self.mapping.get_from_layer(MockLayer1))
         self.assertEqual(
-            mock_build_function1, self.mapping.get_from_layer(MockLayer1())
+            mock_build_function1, self.mapping.get_from_layer(self.linear_layer)
         )
 
     def test_get_from_layer_not_existing_key(self) -> None:
-        self.assertEqual(None, self.mapping.get_from_layer(MockLayer3))
+        self.assertEqual(None, self.mapping.get_from_layer(self.conv2d_layer))
 
     def test_to_dict(self) -> None:
         self.assertEqual(self.mapping_dict, self.mapping.to_dict())
@@ -77,13 +67,13 @@ class BuildFunctionMappingTest(unittest.TestCase):
     def test_join_with_dict_new_and_existing_keys(self) -> None:
         new_mapping = self.mapping.join_with_dict(
             {
-                "test_build_function_mapping.MockLayer3": mock_build_function3,
-                "test_build_function_mapping.MockLayer2": mock_build_function1,
+                "torch.nn.utils.parametrize.ParametrizedQConv2d": mock_build_function3,
+                "torch.nn.utils.parametrize.ParametrizedQConv1d": mock_build_function1,
             }
         )
         target_mapping_dict = {
-            "test_build_function_mapping.MockLayer1": mock_build_function1,
-            "test_build_function_mapping.MockLayer2": mock_build_function1,
-            "test_build_function_mapping.MockLayer3": mock_build_function3,
+            "torch.nn.utils.parametrize.ParametrizedQLinear": mock_build_function1,
+            "torch.nn.utils.parametrize.ParametrizedQConv1d": mock_build_function1,
+            "torch.nn.utils.parametrize.ParametrizedQConv2d": mock_build_function3,
         }
         self.assertEqual(target_mapping_dict, new_mapping.to_dict())
