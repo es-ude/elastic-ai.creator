5f1e1db8da7ce533cb592d56ca97e25ca563a60e
Julian Hoever <julianhoever@posteo.de>
2022-08-17 23:51:54 +0200

    feat(examples): add linear layer to the translation example

--- elasticai/creator/examples/translate_lstm_linear_model.py
+++ elasticai/creator/examples/translate_lstm_linear_model.py
@@ -1,11 +1,13 @@
 import argparse
-from functools import partial
 from pathlib import Path
 
-import torch.nn
+import torch
 
 from elasticai.creator.vhdl.number_representations import FixedPoint
-from elasticai.creator.vhdl.translator.abstract.layers import LSTMTranslationArgs
+from elasticai.creator.vhdl.translator.abstract.layers import (
+    Linear1dTranslationArgs,
+    LSTMTranslationArgs,
+)
 from elasticai.creator.vhdl.translator.pytorch import translator
 from elasticai.creator.vhdl.translator.pytorch.build_function_mappings import (
     DEFAULT_BUILD_FUNCTION_MAPPING,
@@ -21,17 +23,18 @@ def read_commandline_args() -> argparse.Namespace:
 class LSTMModel(torch.nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.lstm_1 = torch.nn.LSTM(input_size=10, hidden_size=100)
-        self.lstm_2 = torch.nn.LSTM(input_size=100, hidden_size=10)
+        self.lstm = torch.nn.LSTM(input_size=1, hidden_size=10)
+        self.linear = torch.nn.Linear(in_features=10, out_features=1)
 
     def forward(self, x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
-        return self.lstm_2(self.lstm_1(x)[0])
+        return self.linear(self.lstm(x)[0])
 
 
 def main() -> None:
     args = read_commandline_args()
 
     model = LSTMModel()
+    fixed_point_factory = FixedPoint.get_factory(total_bits=16, frac_bits=8)
 
     translated = translator.translate_model(
         model=model, build_function_mapping=DEFAULT_BUILD_FUNCTION_MAPPING
@@ -40,10 +43,13 @@ def main() -> None:
         translatable_layers=translated,
         translation_args=dict(
             LSTMTranslatable=LSTMTranslationArgs(
-                fixed_point_factory=partial(FixedPoint, total_bits=16, frac_bits=8),
+                fixed_point_factory=fixed_point_factory,
                 sigmoid_resolution=(-2.5, 2.5, 256),
                 tanh_resolution=(-1, 1, 256),
-            )
+            ),
+            Linear1dTranslatable=Linear1dTranslationArgs(
+                fixed_point_factory=fixed_point_factory,
+            ),
         ),
     )
     translator.save_code(code_repr=code_repr, path=args.build_dir)
