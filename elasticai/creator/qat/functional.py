9bbccddfc6ce6c2b928166cdfaf1112b294dba17
Lukas Einhaus <lukas.einhaus@uni-due.de>
2022-08-29 15:44:24 +0200

    fix(qat): reimplement binarize
    new version avoids creating new
    tensors in favor of implicit
    broadcasts and is more readable

--- elasticai/creator/qat/functional.py
+++ elasticai/creator/qat/functional.py
@@ -4,30 +4,20 @@
 from typing import Any
 
 import torch
-from torch import jit
 
 
-@jit.script
-def _heaviside(x):
-    return torch.heaviside(x, torch.tensor([1.0]))
-
-
-# noinspection PyPep8Naming,PyAbstractClass
-class heaviside_with_ste(torch.autograd.Function):
+class binarize(torch.autograd.Function):
     @staticmethod
     def forward(ctx: Any, *args: Any, **kwargs: Any) -> Any:
         if len(args) == 0:
             raise TypeError
         x = args[0]
-        masked_zero_for_out_of_range = _heaviside(x + 1) - _heaviside(x - 1)
-        ctx.save_for_backward(masked_zero_for_out_of_range)
-        y = 2 * _heaviside(x) - 1
+        out_of_range = torch.logical_or(torch.gt(x, 1.0), torch.lt(x, -1.0))
+        ctx.save_for_backward(out_of_range)
+        y = torch.where(x >= 0, 1., -1.)
         return y
 
     @staticmethod
     def backward(ctx: Any, *grad_outputs: Any) -> Any:
-        (masked_zero_for_out_of_range_inputs,) = ctx.saved_tensors
-        return grad_outputs[0] * masked_zero_for_out_of_range_inputs
-
-
-binarize = heaviside_with_ste
+        (out_of_range,) = ctx.saved_tensors
+        return grad_outputs[0]*torch.where(out_of_range, 0., 1.)
