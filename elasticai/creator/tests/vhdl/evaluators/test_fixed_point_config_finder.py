beb9da0ec8c3fbc6bb4ff65a97e7424e4da6dd0d
Julian Hoever <julianhoever@posteo.de>
2022-09-28 10:58:42 +0200

    feat(vhdl): working further on the fixed point configuration finder

--- elasticai/creator/tests/vhdl/evaluators/test_fixed_point_config_finder.py
+++ elasticai/creator/tests/vhdl/evaluators/test_fixed_point_config_finder.py
@@ -1,46 +1,65 @@
 import unittest
-from typing import Any, Callable
 
-from elasticai.creator.mlframework.typing import Module
+import torch
+from torch.nn import Linear, Module
+from torch.nn.parameter import Parameter
+
+# from elasticai.creator.mlframework.typing import Module
 from elasticai.creator.vhdl.evaluators.fixed_point_config_finder import (
     DataLoader,
     FixedPointConfigFinder,
 )
 
+# class MyModel(Module):
+#     def __init__(self, fn: Callable[[float], float]) -> None:
+#         self.fn = fn
+#
+#     def __call__(self, x: float, *args: Any, **kwargs: Any) -> float:
+#         return self.fn(x)
+
 
 class MyModel(Module):
-    def __init__(self, fn: Callable[[float], float]) -> None:
-        self.fn = fn
+    def __init__(self, weight: float, bias: float) -> None:
+        super().__init__()
+        self.linear = Linear(in_features=1, out_features=1)
 
-    def __call__(self, x: float, *args: Any, **kwargs: Any) -> float:
-        return self.fn(x)
+        self.linear.weight = Parameter(torch.ones(1, 1) * weight)
+        self.linear.bias = Parameter(torch.ones(1) * bias)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.linear(x)
 
 
-def create_data_loader(input_sample: list[float], label: list[float]) -> DataLoader:
-    input_sample_batch = [input_sample]
-    label_batch = [label]
-    labelled_inputs = (input_sample_batch, label_batch)
-    data_loader = [labelled_inputs]
-    return data_loader
+def create_data_loader(samples: list[float], labels: list[float]) -> DataLoader:
+    return [(samples, labels)]
 
 
 class FixedPointConfigurationFinderTest(unittest.TestCase):
-    def test_integers_up_to_three_yield_3total_0frac_bits(self) -> None:
-        input_sample = [1.0]
-        label = input_sample
-        evaluator = FixedPointConfigFinder(
-            MyModel(lambda x: x), create_data_loader(input_sample, label)
-        )
+    def test_fixed_point_representation_with_0frac_bits(self) -> None:
+        model = MyModel(weight=2, bias=3)
+        data = create_data_loader(samples=[1, 2, 3], labels=[5, 7, 9])
+        evaluator = FixedPointConfigFinder(model=model, data=data, total_bits=16)
         result = evaluator.run()
-        expected = dict(total_bits=2, frac_bits=0)
+        expected = dict(total_bits=16, frac_bits=0)
         self.assertEqual(expected, result)
 
-    def test_halved_numbers_summed_to_1_5_yield_2total_1frac_bit(self) -> None:
-        input_sample = [0.5]
-        label = input_sample
-        evaluator = FixedPointConfigFinder(
-            MyModel(lambda x: x), create_data_loader(input_sample, label)
-        )
-        result = evaluator.run()
-        expected = dict(total_bits=2, frac_bits=1)
-        self.assertEqual(expected, result)
+
+# class FixedPointConfigurationFinderTest(unittest.TestCase):
+#     def test_integers_with_0mse_yield_2total_0frac_bits(self) -> None:
+#         data_loader = create_data_loader(samples=[0.0, 0.0], labels=[1.0, 1.0])
+#         evaluator = FixedPointConfigFinder(
+#             model=MyModel(lambda x: 1.0), data=data_loader, total_bits=16
+#         )
+#         result = evaluator.run()
+#         expected = dict(total_bits=2, frac_bits=0)
+#         self.assertEqual(expected, result)
+#
+#     def test_halved_numbers_yield_3total_1frac_bit(self) -> None:
+#         data_loader = create_data_loader(samples=[0.0, 1.0], labels=[0.5, 1.5])
+#         evaluator = FixedPointConfigFinder(
+#             model=MyModel(lambda x: x + 0.5), data=data_loader, total_bits=16
+#         )
+#         result = evaluator.run()
+#         expected = dict(total_bits=3, frac_bits=1)
+#         self.assertEqual(expected, result)
+#
--- elasticai/creator/vhdl/evaluators/fixed_point_config_finder.py
+++ elasticai/creator/vhdl/evaluators/fixed_point_config_finder.py
@@ -1,15 +1,35 @@
-from typing import Any, Iterable
+from copy import deepcopy
+from typing import Any, Callable, Iterable
 
 from elasticai.creator.mlframework.typing import Module
 from elasticai.creator.vhdl.evaluators.evaluator import Evaluator
+from elasticai.creator.vhdl.number_representations import FixedPoint
 
 DataLoader = Iterable[tuple[Any, Any]]
 
 
 class FixedPointConfigFinder(Evaluator):
-    def __init__(self, model: Module, data: DataLoader) -> None:
+    def __init__(self, model: Module, data: DataLoader, total_bits: int) -> None:
         self.model = model
         self.data = data
+        self.total_bits = total_bits
+
+    def _to_fixed_point(self, value: float, frac_bits: int) -> int:
+        to_fp = FixedPoint.get_factory(self.total_bits, frac_bits)
+        return to_fp(value).to_signed_int()
+
+    def _to_float(self, value: int, frac_bits: int) -> float:
+        fp = FixedPoint.from_int(value, self.total_bits, frac_bits, signed_int=True)
+        return float(fp)
+
+    def _apply_to_params(self, model: Module, callable: Callable) -> None:
+        if model.parameters() is not None:
+            state_dict = model.state_dict()
+            print(state_dict)
 
     def run(self) -> dict[str, int]:
+        for frac_bits in range(self.total_bits):
+            model = deepcopy(self.model)
+            self._apply_to_params(model, self._to_fixed_point)
+
         return dict(total_bits=2, frac_bits=0)
