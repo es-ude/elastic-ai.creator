9b8ecae971bc1dedabf17e79272008a3cbfb5123
Julian Hoever <julianhoever@posteo.de>
2022-10-20 20:51:19 +0200

    feat(examples): commit current state of the fixed point linear example

--- elasticai/creator/examples/fixed_point_linear_layer.py
+++ elasticai/creator/examples/fixed_point_linear_layer.py
@@ -1,9 +1,16 @@
+from typing import Any
+
 import matplotlib.pyplot as plt
 import torch
+from torch.nn.utils.parametrize import register_parametrization
 from torch.utils.data import DataLoader, Dataset, TensorDataset
 
-from elasticai.creator.vhdl.custom_layers.linear import FixedPointLinear
-from elasticai.creator.vhdl.number_representations import FixedPoint
+from elasticai.creator.vhdl.custom_layers.linear import FixedPointLinear, _LinearBase
+from elasticai.creator.vhdl.number_representations import (
+    FixedPoint,
+    FixedPointFactory,
+    fixed_point_params_from_factory,
+)
 
 
 def get_dataset() -> tuple[torch.Tensor, torch.Tensor]:
@@ -55,25 +62,6 @@ def augment_data(
     return augmented_samples, augmented_labels
 
 
-def create_full_resolution_model() -> torch.nn.Module:
-    return torch.nn.Sequential(
-        torch.nn.Linear(in_features=3, out_features=2),
-        torch.nn.ReLU(),
-        torch.nn.Linear(in_features=2, out_features=1),
-        torch.nn.Sigmoid(),
-    )
-
-
-def create_fixed_point_model() -> torch.nn.Module:
-    fp_factory = FixedPoint.get_factory(total_bits=16, frac_bits=8)
-    return torch.nn.Sequential(
-        FixedPointLinear(in_features=3, out_features=2, fixed_point_factory=fp_factory),
-        torch.nn.ReLU(),
-        FixedPointLinear(in_features=2, out_features=1, fixed_point_factory=fp_factory),
-        torch.nn.Sigmoid(),
-    )
-
-
 def binary_accuracy(
     predictions: torch.Tensor, labels: torch.Tensor, threshold: float
 ) -> float:
@@ -109,9 +97,11 @@ def train(
 
         for inputs, labels in dl_train:
             optimizer.zero_grad()
-            outputs: torch.Tensor = model(inputs)
+            outputs = model(inputs)
+
             loss = loss_fn(outputs, labels)
             loss.backward()
+
             optimizer.step()
             running_train_loss += loss.item()
 
@@ -125,6 +115,7 @@ def train(
         for inputs, labels in dl_val:
             outputs = model(inputs)
             loss = loss_fn(outputs, labels)
+
             running_val_loss += loss.item()
             running_accuracy += binary_accuracy(outputs, labels, 0.5)
 
@@ -141,6 +132,114 @@ def train(
     return losses_train, losses_val, accuracy
 
 
+def create_full_resolution_model() -> torch.nn.Module:
+    return torch.nn.Sequential(
+        _LinearBase(in_features=3, out_features=2),
+        torch.nn.ReLU(),
+        _LinearBase(in_features=2, out_features=1),
+        torch.nn.Sigmoid(),
+    )
+
+
+class FixedPointQuantFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(
+        ctx: Any, x: torch.Tensor, fixed_point_factory: FixedPointFactory
+    ) -> torch.Tensor:
+        total_bits, frac_bits = fixed_point_params_from_factory(fixed_point_factory)
+        largest_fp_int = 2 ** (total_bits - 1) - 1
+        return (x * (1 << frac_bits)).floor().clamp(-largest_fp_int, largest_fp_int)
+
+    @staticmethod
+    def backward(ctx: Any, grad_outputs: Any) -> Any:
+        return grad_outputs, None
+
+
+class FixedPointDequantFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(
+        ctx: Any, x: torch.Tensor, fixed_point_factory: FixedPointFactory
+    ) -> torch.Tensor:
+        total_bits, frac_bits = fixed_point_params_from_factory(fixed_point_factory)
+        min_value = 2 ** (total_bits - frac_bits - 1) * (-1)
+        max_value = (2 ** (total_bits - 1) - 1) / (1 << frac_bits)
+        return (x / (1 << frac_bits)).clamp(min_value, max_value)
+
+    @staticmethod
+    def backward(ctx: Any, grad_outputs: Any) -> Any:
+        return grad_outputs, None
+
+
+class FixedPointQuant(torch.nn.Module):
+    def __init__(self, fixed_point_factory: FixedPointFactory) -> None:
+        super().__init__()
+        self.fixed_point_factory = fixed_point_factory
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return FixedPointQuantFunction.apply(x, self.fixed_point_factory)
+
+
+class FixedPointDequant(torch.nn.Module):
+    def __init__(self, fixed_point_factory: FixedPointFactory) -> None:
+        super().__init__()
+        self.fixed_point_factory = fixed_point_factory
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return FixedPointDequantFunction.apply(x, self.fixed_point_factory)
+
+
+class QuantModule(torch.nn.Module):
+    def __init__(
+        self, module: torch.nn.Module, quant: torch.nn.Module, dequant: torch.nn.Module
+    ) -> None:
+        super().__init__()
+        self.module = module
+        self.quant = quant
+        self.dequant = dequant
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.dequant(self.module(self.quant(x)))
+
+
+class FixedPointModel(torch.nn.Module):
+    def __init__(self, total_bits: int, frac_bits: int) -> None:
+        super().__init__()
+        factory = FixedPoint.get_factory(total_bits, frac_bits)
+
+        quant = FixedPointQuant(factory)
+        dequant = FixedPointDequant(factory)
+
+        self._linear1 = QuantModule(
+            module=FixedPointLinear(
+                in_features=3, out_features=2, fixed_point_factory=factory
+            ),
+            quant=quant,
+            dequant=dequant,
+        )
+        register_parametrization(self._linear1.module, "weight", quant)
+        register_parametrization(self._linear1.module, "bias", quant)
+
+        self._linear2 = QuantModule(
+            module=FixedPointLinear(
+                in_features=2, out_features=1, fixed_point_factory=factory
+            ),
+            quant=quant,
+            dequant=dequant,
+        )
+        register_parametrization(self._linear2.module, "weight", quant)
+        register_parametrization(self._linear2.module, "bias", quant)
+
+        self._relu = torch.nn.ReLU()
+        self._sigmoid = torch.nn.Sigmoid()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self._linear1(x)
+        x = self._relu(x)
+        x = self._linear2(x)
+        x = self._sigmoid(x)
+        return x
+
+
 def main() -> None:
     x, y = get_dataset()
     x_train, y_train = augment_data(x, y, expand_factor=240, noise_var=0.05, seed=24)
@@ -148,7 +247,8 @@ def main() -> None:
     ds_train = TensorDataset(x_train, y_train)
     ds_test = TensorDataset(x_test, y_test)
 
-    model = create_full_resolution_model()
+    # model = create_full_resolution_model()
+    model = FixedPointModel(total_bits=32, frac_bits=24)
 
     losses_train, losses_val, accuracy_val = train(
         model=model,
@@ -156,7 +256,7 @@ def main() -> None:
         val_ds=ds_test,
         batch_size=16,
         learning_rate=1e-3,
-        num_epochs=200,
+        num_epochs=100,
     )
 
     plt.plot(losses_train, label="train_loss")
