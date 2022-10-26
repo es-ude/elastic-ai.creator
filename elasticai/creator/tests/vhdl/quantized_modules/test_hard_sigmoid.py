feat(vhdl): fix wrong calculation of fixed point values and add quantized forward functions

elasticai/creator/tests/vhdl/quantized_modules/test_hard_sigmoid.py
@@ -53,10 +53,14 @@ class HardSigmoidBaseTest(unittest.TestCase):
 
         self.assertEquals(expected, actual)
 
+    def test_hard_sigmoid_base_quantized_forward(self) -> None:
+        with self.assertRaises(NotImplementedError):
+            _HardSigmoidBase().quantized_forward(torch.ones(10))
+
 
 class FixedPointHardSigmoidTest(unittest.TestCase):
     def test_fixed_point_hard_sigmoid_calculates_output_correctly(self) -> None:
-        fp_args = 16, 8
+        fp_args = 8, 3
         hard_sigmoid = FixedPointHardSigmoid(
             fixed_point_factory=FixedPoint.get_factory(*fp_args)
         )
@@ -68,5 +72,19 @@ class FixedPointHardSigmoidTest(unittest.TestCase):
         )
         expected = to_list(F.hardsigmoid(quantized_xs))
         actual = to_list(hard_sigmoid(xs))
+
+        self.assertEquals(expected, actual)
+
+    def test_fixed_point_hard_sigmoid_quantized_forward(self) -> None:
+        fp_args = 8, 3
+        hard_sigmoid = FixedPointHardSigmoid(
+            fixed_point_factory=FixedPoint.get_factory(*fp_args)
+        )
+
+        xs = [-3, -1.5, 0, 1.5, 3]
+        expected = [0, 3, 4, 5, 8]
+
+        quantized_xs = torch.tensor(to_fixed_point(xs, *fp_args), dtype=torch.float32)
+        actual = to_list(hard_sigmoid.quantized_forward(quantized_xs))
 
         self.assertEquals(expected, actual)

elasticai/creator/vhdl/quantized_modules/_autograd_functions/fixed_point_quantization.py
@@ -15,7 +15,9 @@ class FixedPointQuantFunction(torch.autograd.Function):
     ) -> torch.Tensor:
         total_bits, frac_bits = fixed_point_params_from_factory(fixed_point_factory)
         largest_fp_int = 2 ** (total_bits - 1) - 1
-        return (x * (1 << frac_bits)).floor().clamp(-largest_fp_int, largest_fp_int)
+        return (
+            (x * (1 << frac_bits)).int().float().clamp(-largest_fp_int, largest_fp_int)
+        )
 
     @staticmethod
     def backward(ctx: Any, *grad_outputs: Any) -> Any:

elasticai/creator/vhdl/quantized_modules/hard_sigmoid.py
@@ -1,6 +1,5 @@
-from math import floor
-
 import torch
+import torch.nn.functional as F
 
 from elasticai.creator.vhdl.number_representations import (
     FixedPointFactory,
@@ -26,7 +25,7 @@ class _HardSigmoidBase(torch.nn.Hardsigmoid):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         q_x = self.input_dequant(self.input_quant(x))
-        return super().forward(q_x)
+        return F.hardsigmoid(q_x)
 
     def quantized_forward(self, x: torch.Tensor) -> torch.Tensor:
         raise NotImplementedError("The quantized_forward function is not implemented.")
@@ -49,7 +48,7 @@ class FixedPointHardSigmoid(_HardSigmoidBase):
         _, frac_bits = fixed_point_params_from_factory(self.fixed_point_factory)
 
         def fp(value: float) -> int:
-            return floor(value * (1 << frac_bits))
+            return int(value * (1 << frac_bits))
 
         def fp_hard_sigmoid(a: int) -> int:
             if a <= fp(-3):
@@ -57,6 +56,6 @@ class FixedPointHardSigmoid(_HardSigmoidBase):
             elif a >= fp(3):
                 return fp(1)
             else:
-                return floor(a * fp(1 / 6) / fp(1)) + fp(1 / 2)
+                return int(a * fp(1 / 6) / fp(1)) + fp(1 / 2)
 
         return x.apply_(fp_hard_sigmoid)

elasticai/creator/vhdl/quantized_modules/linear.py
@@ -80,7 +80,7 @@ class FixedPointLinear(_LinearBase):
     def quantized_forward(self, x: torch.Tensor) -> torch.Tensor:
         def fp_matmul(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
             _, frac_bits = fixed_point_params_from_factory(self.fixed_point_factory)
-            return (torch.matmul(a, b) / (1 << frac_bits)).floor()
+            return (torch.matmul(a, b) / (1 << frac_bits)).int().float()
 
         q_weight = self.param_quant(self.weight)
 