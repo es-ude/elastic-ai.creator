ad49bc68ef5f38e6047d569d8e90513f50698a27
Julian Hoever <julianhoever@posteo.de>
2022-11-06 16:18:05 +0100

    test(vhdl): add tests to test the quant and dequant parameters of the LinearBase class

--- elasticai/creator/tests/vhdl/quantized_modules/test_linear.py
+++ elasticai/creator/tests/vhdl/quantized_modules/test_linear.py
@@ -82,6 +82,56 @@ class LinearBaseTest(unittest.TestCase):
 
         self.assertEqual(actual, target)
 
+    def test_linear_with_input_quant_and_input_dequant(self) -> None:
+        linear = _LinearBase(
+            in_features=3,
+            out_features=1,
+            input_quant=lambda x: x + 5,
+            input_dequant=lambda x: x - 2,
+        )
+        linear.weight = Parameter(torch.ones_like(linear.weight))
+        linear.bias = Parameter(torch.ones_like(linear.bias))
+
+        input_tensor = torch.as_tensor([1, 2, 3], dtype=torch.float32)
+        actual = to_list(linear(input_tensor))
+        target = [16]
+
+        self.assertEqual(actual, target)
+
+    def test_linear_with_param_quant_and_param_dequant(self) -> None:
+        linear = _LinearBase(
+            in_features=3,
+            out_features=1,
+            param_quant=lambda x: x + 5,
+            param_dequant=lambda x: x - 2,
+        )
+        linear.weight = Parameter(torch.ones_like(linear.weight))
+        linear.bias = Parameter(torch.ones_like(linear.bias))
+
+        input_tensor = torch.as_tensor([1, 2, 3], dtype=torch.float32)
+        actual = to_list(linear(input_tensor))
+        target = [28]
+
+        self.assertEqual(actual, target)
+
+    def test_linear_with_input_param_quant_dequant(self) -> None:
+        linear = _LinearBase(
+            in_features=3,
+            out_features=1,
+            input_quant=lambda x: x + 3,
+            input_dequant=lambda x: x - 2,
+            param_quant=lambda x: x + 5,
+            param_dequant=lambda x: x - 2,
+        )
+        linear.weight = Parameter(torch.ones_like(linear.weight))
+        linear.bias = Parameter(torch.ones_like(linear.bias))
+
+        input_tensor = torch.as_tensor([1, 2, 3], dtype=torch.float32)
+        actual = to_list(linear(input_tensor))
+        target = [40]
+
+        self.assertEqual(actual, target)
+
     def test_linear_quantized_forward_raises_error(self) -> None:
         with self.assertRaises(NotImplementedError):
             _LinearBase(1, 2).quantized_forward(torch.ones(1))
