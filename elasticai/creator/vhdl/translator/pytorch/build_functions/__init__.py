ffcbb1d57408ad03e91bd1228bc6d3289f1d0c66
Chao Qian <chao.qian@uni-due.de>
2022-11-11 21:52:20 +0100

    feat(vhdl): add fp_linear build function, and test passed

--- elasticai/creator/vhdl/translator/pytorch/build_functions/__init__.py
+++ elasticai/creator/vhdl/translator/pytorch/build_functions/__init__.py
@@ -1,3 +1,6 @@
+from elasticai.creator.vhdl.translator.pytorch.build_functions.fp_linear_1d_build_function import (
+    build_fp_linear_1d,
+)
 from elasticai.creator.vhdl.translator.pytorch.build_functions.linear_1d_build_function import (
     build_linear_1d,
 )
---
+++ elasticai/creator/vhdl/translator/pytorch/build_functions/fp_linear_1d_build_function.py
@@ -0,0 +1,11 @@
+import torch
+
+from elasticai.creator.vhdl.quantized_modules.linear import FixedPointLinear
+from elasticai.creator.vhdl.translator.abstract.layers import FPLinear1dModule
+
+
+def build_fp_linear_1d(linear: FixedPointLinear) -> FPLinear1dModule:
+    def to_list(tensor: torch.Tensor) -> list:
+        return tensor.detach().numpy().tolist()
+
+    return FPLinear1dModule(weight=to_list(linear.weight), bias=to_list(linear.bias))
