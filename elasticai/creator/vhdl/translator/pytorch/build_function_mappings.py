5a4bcd6fb6de9cff6c639866db1dd50918f3039b
Julian Hoever <julianhoever@posteo.de>
2022-11-17 11:31:14 +0100

    fix(vhdl): remove duplicated key

--- elasticai/creator/vhdl/translator/pytorch/build_function_mappings.py
+++ elasticai/creator/vhdl/translator/pytorch/build_function_mappings.py
@@ -13,7 +13,6 @@ DEFAULT_BUILD_FUNCTION_MAPPING = BuildFunctionMapping(
         "torch.nn.modules.rnn.LSTM": build_lstm,
         "elasticai.creator.vhdl.quantized_modules.linear.FixedPointLinear": build_fp_linear_1d,
         "elasticai.creator.vhdl.quantized_modules.hard_sigmoid.FixedPointHardSigmoid": build_fp_hard_sigmoid,
-        "elasticai.creator.vhdl.quantized_modules.hard_sigmoid.FixedPointHardSigmoid": build_fp_hard_sigmoid,
         "elasticai.creator.vhdl.quantized_modules.relu.FixedPointReLU": build_fp_relu,
     }
 )
