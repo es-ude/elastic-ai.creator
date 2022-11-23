f458fb6c216385a119774a3f98788941e13ed5c9
Julian Hoever <julianhoever@posteo.de>
2022-11-23 19:59:48 +0100

    feat(vhdl): implement and test lstm cell base class and start implementing fp lstm cell

--- elasticai/creator/tests/vhdl/quantized_modules/test_lstm.py
+++ elasticai/creator/tests/vhdl/quantized_modules/test_lstm.py
@@ -1,36 +1,75 @@
 import unittest
 
 import torch
+from torch.nn.parameter import Parameter
 
-from elasticai.creator.vhdl.quantized_modules.hard_sigmoid import _HardSigmoidBase
-from elasticai.creator.vhdl.quantized_modules.hard_tanh import _HardTanhBase
-from elasticai.creator.vhdl.quantized_modules.linear import _LinearBase
+from elasticai.creator.tests.vhdl.quantized_modules.utils import to_list
 from elasticai.creator.vhdl.quantized_modules.lstm import _LSTMCellBase
 
 
-def linear_factory(in_features: int, out_features: int, bias: bool) -> _LinearBase:
-    return _LinearBase(in_features=in_features, out_features=out_features, bias=bias)
+def to_tensor(x: list) -> torch.Tensor:
+    return torch.tensor(x, dtype=torch.float32)
 
 
 class LSTMCellBaseTest(unittest.TestCase):
-    def test_lstm_cell_base_shape_equals_reference_lstm_cell(self) -> None:
-        lstm_cell = _LSTMCellBase(
-            input_size=3,
-            hidden_size=2,
-            bias=True,
-            linear_factory=linear_factory,
-            sigmoid_factory=lambda: _HardSigmoidBase(),
-            tanh_factory=lambda: _HardTanhBase(),
+    @staticmethod
+    def create_lstm_cell_base_and_reference(
+        input_size: int, hidden_size: int, bias: bool
+    ) -> tuple[_LSTMCellBase, torch.nn.LSTMCell]:
+        cell = _LSTMCellBase(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            bias=bias,
+            linear_factory=torch.nn.Linear,
+            sigmoid_factory=torch.nn.Sigmoid,
+            tanh_factory=torch.nn.Tanh,
         )
-        reference_lstm_cell = torch.nn.LSTMCell(input_size=3, hidden_size=2, bias=True)
+        reference_cell = torch.nn.LSTMCell(
+            input_size=input_size, hidden_size=hidden_size, bias=bias
+        )
 
-        inputs = torch.tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype=torch.float32)
-        actual_h, actual_c = lstm_cell(inputs)
-        expected_h, expected_c = reference_lstm_cell(inputs)
+        def ones_like(x: torch.Tensor) -> torch.Tensor:
+            return Parameter(torch.ones_like(x))
 
-        self.assertEqual(tuple(expected_h.shape), tuple(actual_h.shape))
-        self.assertEqual(tuple(expected_c.shape), tuple(actual_c.shape))
+        cell.linear_ih.weight = ones_like(cell.linear_ih.weight)
+        cell.linear_hh.weight = ones_like(cell.linear_hh.weight)
+        reference_cell.weight_ih = ones_like(reference_cell.weight_ih)
+        reference_cell.weight_hh = ones_like(reference_cell.weight_hh)
+
+        if bias:
+            cell.linear_ih.bias = ones_like(cell.linear_ih.bias)
+            cell.linear_hh.bias = ones_like(cell.linear_hh.bias)
+            reference_cell.bias_ih = ones_like(reference_cell.bias_ih)
+            reference_cell.bias_hh = ones_like(reference_cell.bias_hh)
+
+        return cell, reference_cell
+
+    def test_lstm_cell_base_without_bias_equals_torch_lstm_cell(self) -> None:
+        cell, reference_cell = self.create_lstm_cell_base_and_reference(
+            input_size=3, hidden_size=2, bias=False
+        )
+        inputs = to_tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        actual_h, actual_c = cell(inputs)
+        expected_h, expected_c = reference_cell(inputs)
+
+        self.assertEqual(to_list(expected_h), to_list(actual_h))
+        self.assertEqual(to_list(expected_c), to_list(actual_c))
+
+    def test_lstm_cell_base_equals_torch_lstm_cell(self) -> None:
+        cell, reference_cell = self.create_lstm_cell_base_and_reference(
+            input_size=3, hidden_size=2, bias=True
+        )
+        inputs = to_tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        actual_h, actual_c = cell(inputs)
+        expected_h, expected_c = reference_cell(inputs)
+
+        self.assertEqual(to_list(expected_h), to_list(actual_h))
+        self.assertEqual(to_list(expected_c), to_list(actual_c))
 
     def test_lstm_cell_base_quantized_forward_raises_error(self) -> None:
+        cell, _ = self.create_lstm_cell_base_and_reference(
+            input_size=3, hidden_size=2, bias=True
+        )
         with self.assertRaises(NotImplementedError):
-            ...
+            inputs = torch.tensor([[1, 2, 3]], dtype=torch.float32)
+            _ = cell.quantized_forward(inputs)
--- elasticai/creator/vhdl/quantized_modules/lstm.py
+++ elasticai/creator/vhdl/quantized_modules/lstm.py
@@ -1,7 +1,20 @@
 from collections.abc import Callable
-from typing import Optional
+from functools import partial
+from typing import Any, Optional
 
 import torch
+
+from elasticai.creator.vhdl.number_representations import FixedPointFactory
+from elasticai.creator.vhdl.quantized_modules import (
+    FixedPointHardSigmoid,
+    FixedPointLinear,
+)
+from elasticai.creator.vhdl.quantized_modules.autograd_functions import (
+    FixedPointDequantFunction,
+    FixedPointQuantFunction,
+)
+from elasticai.creator.vhdl.quantized_modules.hard_tanh import FixedPointHardTanh
+from elasticai.creator.vhdl.quantized_modules.typing import OperationType, QuantType
 
 
 class _LSTMCellBase(torch.nn.Module):
@@ -10,23 +23,31 @@ class _LSTMCellBase(torch.nn.Module):
         input_size: int,
         hidden_size: int,
         bias: bool,
-        linear_factory: Callable[[int, int, bool], torch.nn.Linear],
-        sigmoid_factory: Callable[[], torch.nn.Hardsigmoid],
-        tanh_factory: Callable[[], torch.nn.Hardtanh],
+        linear_factory: Callable,
+        sigmoid_factory: Callable,
+        tanh_factory: Callable,
+        mul_op: OperationType = torch.mul,
+        add_op: OperationType = torch.add,
+        input_quant: QuantType = lambda x: x,
+        input_dequant: QuantType = lambda x: x,
     ) -> None:
         super().__init__()
 
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.bias = bias
+        self.input_quant = input_quant
+        self.input_dequant = input_dequant
+        self.matmul_op = mul_op
+        self.add_op = add_op
 
+        self.linear_ih = linear_factory(input_size, hidden_size * 4, bias)
+        self.linear_hh = linear_factory(hidden_size, hidden_size * 4, bias)
         self.sigmoid = sigmoid_factory()
         self.tanh = tanh_factory()
 
-        self.linear_f = linear_factory(input_size + hidden_size, hidden_size, bias)
-        self.linear_i = linear_factory(input_size + hidden_size, hidden_size, bias)
-        self.linear_g = linear_factory(input_size + hidden_size, hidden_size, bias)
-        self.linear_o = linear_factory(input_size + hidden_size, hidden_size, bias)
+    def _fake_quant(self, *inputs: torch.Tensor) -> tuple[torch.Tensor, ...]:
+        return tuple(self.input_dequant(self.input_quant(x)) for x in inputs)
 
     def forward(
         self, x: torch.Tensor, state: Optional[torch.Tensor] = None
@@ -37,14 +58,21 @@ class _LSTMCellBase(torch.nn.Module):
         else:
             h_prev, c_prev = state
 
-        inputs = torch.cat((x, h_prev), dim=1)
+        x, h_prev, c_prev = self._fake_quant(x, h_prev, c_prev)
 
-        f = self.sigmoid(self.linear_f(inputs))
-        i = self.sigmoid(self.linear_i(inputs))
-        g = self.tanh(self.linear_g(inputs))
-        o = self.sigmoid(self.linear_o(inputs))
+        pred_ii, pred_if, pred_ig, pred_io = torch.split(
+            self.linear_ih(x), self.hidden_size, dim=1
+        )
+        pred_hi, pred_hf, pred_hg, pred_ho = torch.split(
+            self.linear_hh(h_prev), self.hidden_size, dim=1
+        )
 
-        c = f * c_prev + i * g
+        i = self.sigmoid(pred_ii + pred_hi)
+        f = self.sigmoid(pred_if + pred_hf)
+        g = self.tanh(pred_ig + pred_hg)
+        o = self.sigmoid(pred_io + pred_ho)
+
+        c = self.add_op(self.matmul_op(f, c_prev), self.matmul_op(i, g))
         h = o * self.tanh(c)
 
         return h, c
@@ -53,3 +81,37 @@ class _LSTMCellBase(torch.nn.Module):
         self, x: torch.Tensor, state: Optional[torch.Tensor] = None
     ) -> tuple[torch.Tensor, torch.Tensor]:
         raise NotImplementedError("The quantized_forward function is not implemented.")
+
+
+class FixedPointLSTMCell(_LSTMCellBase):
+    def __init__(
+        self,
+        fixed_point_factory: FixedPointFactory,
+        input_size: int,
+        hidden_size: int,
+        bias: bool = True,
+    ) -> None:
+        self.fixed_point_factory = fixed_point_factory
+        self.quant = lambda x: FixedPointQuantFunction.apply(x, fixed_point_factory)
+        self.dequant = lambda x: FixedPointDequantFunction.apply(x, fixed_point_factory)
+        super().__init__(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            bias=bias,
+            linear_factory=partial(
+                FixedPointLinear, fixed_point_factory=fixed_point_factory
+            ),
+            sigmoid_factory=partial(
+                FixedPointHardSigmoid, fixed_point_factory=fixed_point_factory
+            ),
+            tanh_factory=partial(
+                FixedPointHardTanh, fixed_point_factory=fixed_point_factory
+            ),
+            input_quant=self.input_quant,
+            input_dequant=self.input_dequant,
+        )
+
+    def quantized_forward(
+        self, x: torch.Tensor, state: Optional[torch.Tensor] = None
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        ...
