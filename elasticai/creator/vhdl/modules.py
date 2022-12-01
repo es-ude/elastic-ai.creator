33e66d99b5b8c0801c93e520463ea92c6392e2b8
Lukas Einhaus <lukas.einhaus@uni-due.de>
2022-12-01 18:08:56 +0100

    feat(vhdl): generate portmap output_address

--- elasticai/creator/vhdl/modules.py
+++ elasticai/creator/vhdl/modules.py
@@ -1,5 +1,7 @@
 from typing import Iterable
 
+from elasticai.creator.resource_utils import read_text
+from elasticai.creator.vhdl.templates.utils import expand_template
 from elasticai.creator.vhdl.vhdl_files import (
     VHDLModule,
     VHDLBaseModule,
@@ -8,12 +10,16 @@ from elasticai.creator.vhdl.vhdl_files import (
 
 
 class Module:
+    def __init__(self):
+        self.elasticai_tags = {}
+
     def to_vhdl(self) -> Iterable[VHDLModule]:
+        code = read_text("elasticai.creator.vhdl.templates", "network.tpl.vhd")
+        code = expand_template(code, **self.elasticai_tags)
+
         yield from [
             VHDLBaseModule(
                 name="network",
-                files=(
-                    VHDLBaseFile(name="network.vhd", code=["vhdl code", "more code"]),
-                ),
+                files=(VHDLBaseFile(name="network.vhd", code=code),),
             )
         ]
