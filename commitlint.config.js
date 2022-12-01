d345351c96c0ce6d0a5bcf52ae9bca8eacdafd6b
Lukas Einhaus <lukas.einhaus@uni-due.de>
2022-12-01 16:23:57 +0100

    ci(templates):add commitlint constraints

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'ci', 'wip'],
+        'scope-enum': ['vhdl', 'training', 'transform']
+    }
+}
