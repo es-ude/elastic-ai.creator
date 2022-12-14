e518975728140ab29692bea341ea015cfcfb59df
Lukas Einhaus <lukas.einhaus@uni-due.de>
2022-12-14 14:57:06 +0100

    ci: adjust commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,6 @@ module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
         'type-enum': [2, "always", ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'ci', 'wip']],
-        'scope-enum':  [2, "always", ['all', 'vhdl', 'training', 'transform']]
+        'scope-enum':  [2, "always", ['all', 'vhdl', 'nn', 'transform', 'translation', 'templates', 'gh-workflow']]
     }
 }
