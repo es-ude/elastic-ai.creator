d7aaf28042881c272f851e5402135d15a149ec42
Lukas Einhaus <lukas.einhaus@uni-due.de>
2022-12-14 17:36:05 +0100

    chore: add style again to pyproject and commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'type-enum': [2, "always", ['feat', 'fix', 'docs', 'refactor', 'revert', 'chore', 'wip', 'perf']],
+        'type-enum': [2, "always", ['feat', 'fix', 'docs', 'style', 'refactor', 'revert', 'chore', 'wip', 'perf']],
         'scope-enum': [2, "always", ['template', 'translation', 'nn', 'transformation', 'unit', 'integration']]
     }
 }
