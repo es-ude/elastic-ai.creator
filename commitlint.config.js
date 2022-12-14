87eaa632c644d70c5ac693b2f5f6aac6a3625acc
Lukas Einhaus <lukas.einhaus@uni-due.de>
2022-12-14 15:08:13 +0100

    ci: temporarily disable commitlint constraints

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,3 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [2, "always", ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'ci', 'wip']],
-        'scope-enum':  [2, "always", ['all', 'vhdl', 'nn', 'transform', 'translation', 'templates', 'gh-workflow']]
-    }
 }
