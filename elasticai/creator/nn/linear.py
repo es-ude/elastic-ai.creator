a9615581159ba4b962fac8458d9b76de0a61d98f
Julian Hoever <julianhoever@posteo.de>
2023-01-21 13:57:54 +0100

    feat(nn): integrate arithmetics for the linear layer

--- elasticai/creator/nn/linear.py
+++ elasticai/creator/nn/linear.py
@@ -1,14 +1,12 @@
 import torch
 
+from elasticai.creator.nn.arithmetics import Arithmetics, FixedPointArithmetics
 from elasticai.creator.nn.autograd_functions.fixed_point_quantization import (
     FixedPointDequantFunction,
     FixedPointQuantFunction,
 )
-from elasticai.creator.nn.quant_typings import OperationType, QuantType
-from elasticai.creator.vhdl.number_representations import (
-    FixedPointFactory,
-    fixed_point_params_from_factory,
-)
+from elasticai.creator.nn.quantization import FakeQuant, QuantType
+from elasticai.creator.vhdl.number_representations import FixedPointFactory
 
 
 class _LinearBase(torch.nn.Linear):
@@ -16,36 +14,27 @@ class _LinearBase(torch.nn.Linear):
         self,
         in_features: int,
         out_features: int,
+        arithmetics: Arithmetics,
         bias: bool = True,
-        matmul_op: OperationType = torch.matmul,
-        add_op: OperationType = torch.add,
         input_quant: QuantType = lambda x: x,
-        input_dequant: QuantType = lambda x: x,
         param_quant: QuantType = lambda x: x,
-        param_dequant: QuantType = lambda x: x,
         device=None,
         dtype=None,
     ) -> None:
         super().__init__(in_features, out_features, bias, device, dtype)
-        self.matmul_op = matmul_op
-        self.add_op = add_op
+        self.ops = arithmetics
         self.input_quant = input_quant
-        self.input_dequant = input_dequant
         self.param_quant = param_quant
-        self.param_dequant = param_dequant
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        dq_input = self.input_dequant(self.input_quant(x))
-        dq_weight = self.param_dequant(self.param_quant(self.weight))
+        x = self.input_quant(x)
+        weight = self.param_quant(self.weight)
 
         if self.bias is not None:
-            dq_bias = self.param_dequant(self.param_quant(self.bias))
-            return self.add_op(self.matmul_op(dq_input, dq_weight.T), dq_bias)
+            bias = self.param_quant(self.bias)
+            return self.ops.add(self.ops.matmul(x, weight.T), bias)
 
-        return self.matmul_op(dq_input, dq_weight.T)
-
-    def quantized_forward(self, x: torch.Tensor) -> torch.Tensor:
-        raise NotImplementedError("The quantized_forward function is not implemented.")
+        return self.ops.matmul(x, weight.T)
 
 
 class FixedPointLinear(_LinearBase):
@@ -57,37 +46,20 @@ class FixedPointLinear(_LinearBase):
         bias: bool = True,
         device=None,
     ) -> None:
-        self.fixed_point_factory = fixed_point_factory
-        self.quant = lambda x: FixedPointQuantFunction.apply(x, fixed_point_factory)
-        self.dequant = lambda x: FixedPointDequantFunction.apply(x, fixed_point_factory)
+        self.arithmetics = FixedPointArithmetics(
+            fixed_point_factory=fixed_point_factory
+        )
+        self.quant = FakeQuant(
+            quant=lambda x: FixedPointQuantFunction.apply(x, fixed_point_factory),
+            dequant=lambda x: FixedPointDequantFunction.apply(x, fixed_point_factory),
+            arithmetics=self.arithmetics,
+        )
         super().__init__(
             in_features=in_features,
             out_features=out_features,
             bias=bias,
+            arithmetics=self.arithmetics,
             input_quant=self.quant,
-            input_dequant=self.dequant,
             param_quant=self.quant,
-            param_dequant=self.dequant,
             device=device,
         )
-
-    def quantized_forward(self, x: torch.Tensor) -> torch.Tensor:
-        total_bits, frac_bits = fixed_point_params_from_factory(
-            self.fixed_point_factory
-        )
-
-        def fp_matmul(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
-            return (torch.matmul(a, b) / (1 << frac_bits)).int().float()
-
-        def clamp_overflowing_values(a: torch.Tensor) -> torch.Tensor:
-            largest_fp_int = 2 ** (total_bits - 1) - 1
-            return a.clamp(-largest_fp_int, largest_fp_int)
-
-        q_weight = self.param_quant(self.weight)
-        output = fp_matmul(x, q_weight.T)
-
-        if self.bias is not None:
-            q_bias = self.param_quant(self.bias)
-            output = self.add_op(output, q_bias)
-
-        return clamp_overflowing_values(output)
--- elasticai/creator/tests/unit/nn/test_linear.py
+++ elasticai/creator/tests/unit/nn/test_linear.py
@@ -1,183 +1,136 @@
-import unittest
-
 import torch
 from torch.nn.parameter import Parameter
 
+from elasticai.creator.nn.arithmetics import FloatArithmetics
 from elasticai.creator.nn.linear import FixedPointLinear, _LinearBase
-from elasticai.creator.tests.unit.nn.utils import to_list
-from elasticai.creator.vhdl.number_representations import FixedPoint
+from elasticai.creator.nn.quantization import QuantType
+from elasticai.creator.tests.unit.tensor_test_case import TensorTestCase
+from elasticai.creator.vhdl.number_representations import FixedPoint, FixedPointFactory
 
 
-class LinearBaseTest(unittest.TestCase):
-    def test_default_linear(self) -> None:
-        linear = _LinearBase(in_features=3, out_features=1)
-        linear.weight = Parameter(torch.ones_like(linear.weight))
-        linear.bias = Parameter(torch.ones_like(linear.bias) * 4)
+def tensor(data: list) -> torch.Tensor:
+    return torch.as_tensor(data, dtype=torch.float32)
 
-        input_tensor = torch.as_tensor([1, 2, 3], dtype=torch.float32)
-        actual = to_list(linear(input_tensor))
-        target = [10]
 
-        self.assertEqual(actual, target)
-
-    def test_linear_with_customized_add_op(self) -> None:
-        linear = _LinearBase(in_features=3, out_features=1, add_op=torch.sub)
-        linear.weight = Parameter(torch.ones_like(linear.weight))
-        linear.bias = Parameter(torch.ones_like(linear.bias) * 4)
-
-        input_tensor = torch.as_tensor([1, 2, 3], dtype=torch.float32)
-        actual = to_list(linear(input_tensor))
-        target = [2]
-
-        self.assertEqual(actual, target)
-
-    def test_linear_with_customized_matmul_op(self) -> None:
-        def custom_matmul(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
-            return torch.matmul(a, b) * (-1)
-
-        linear = _LinearBase(in_features=3, out_features=1, matmul_op=custom_matmul)
-        linear.weight = Parameter(torch.ones_like(linear.weight))
-        linear.bias = Parameter(torch.ones_like(linear.bias) * 4)
-
-        input_tensor = torch.as_tensor([1, 2, 3], dtype=torch.float32)
-        actual = to_list(linear(input_tensor))
-        target = [-2]
-
-        self.assertEqual(actual, target)
-
-    def test_linear_with_customized_add_op_and_matmul_op(self) -> None:
-        def custom_matmul(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
-            return torch.matmul(a, b) * (-1)
-
-        linear = _LinearBase(
-            in_features=3, out_features=1, matmul_op=custom_matmul, add_op=torch.sub
-        )
-        linear.weight = Parameter(torch.ones_like(linear.weight))
-        linear.bias = Parameter(torch.ones_like(linear.bias) * 4)
-
-        input_tensor = torch.as_tensor([1, 2, 3], dtype=torch.float32)
-        actual = to_list(linear(input_tensor))
-        target = [-10]
-
-        self.assertEqual(actual, target)
-
-    def test_linear_with_customized_matmul_without_bias(self) -> None:
-        def custom_matmul(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
-            return torch.matmul(a, b) * (-1)
-
-        linear = _LinearBase(
-            in_features=3, out_features=1, matmul_op=custom_matmul, bias=False
-        )
-        linear.weight = Parameter(torch.ones_like(linear.weight))
-
-        input_tensor = torch.as_tensor([1, 2, 3], dtype=torch.float32)
-        actual = to_list(linear(input_tensor))
-        target = [-6]
-
-        self.assertEqual(actual, target)
-
-    def test_linear_with_input_quant_and_input_dequant(self) -> None:
-        linear = _LinearBase(
-            in_features=3,
-            out_features=1,
-            input_quant=lambda x: x + 5,
-            input_dequant=lambda x: x - 2,
-        )
-        linear.weight = Parameter(torch.ones_like(linear.weight))
+def linear_base_with_fixed_params(
+    in_features: int,
+    out_features: int,
+    bias: bool,
+    input_quant: QuantType = lambda x: x,
+    param_quant: QuantType = lambda x: x,
+) -> _LinearBase:
+    arithmetics = FloatArithmetics()
+    linear = _LinearBase(
+        in_features=in_features,
+        out_features=out_features,
+        bias=bias,
+        arithmetics=arithmetics,
+        input_quant=input_quant,
+        param_quant=param_quant,
+    )
+    linear.weight = Parameter(torch.ones_like(linear.weight))
+    if bias:
         linear.bias = Parameter(torch.ones_like(linear.bias))
+    return linear
 
-        input_tensor = torch.as_tensor([1, 2, 3], dtype=torch.float32)
-        actual = to_list(linear(input_tensor))
-        target = [16]
 
-        self.assertEqual(actual, target)
-
-    def test_linear_with_param_quant_and_param_dequant(self) -> None:
-        linear = _LinearBase(
-            in_features=3,
-            out_features=1,
-            param_quant=lambda x: x + 5,
-            param_dequant=lambda x: x - 2,
-        )
-        linear.weight = Parameter(torch.ones_like(linear.weight))
+def fp_linear_with_fixed_params(
+    in_features: int,
+    out_features: int,
+    bias: bool,
+    fixed_point_factory: FixedPointFactory,
+) -> FixedPointLinear:
+    linear = FixedPointLinear(
+        in_features=in_features,
+        out_features=out_features,
+        bias=bias,
+        fixed_point_factory=fixed_point_factory,
+    )
+    linear.weight = Parameter(torch.ones_like(linear.weight))
+    if bias:
         linear.bias = Parameter(torch.ones_like(linear.bias))
+    return linear
 
-        input_tensor = torch.as_tensor([1, 2, 3], dtype=torch.float32)
-        actual = to_list(linear(input_tensor))
-        target = [28]
 
-        self.assertEqual(actual, target)
+class LinearBaseTest(TensorTestCase):
+    def test_with_bias(self) -> None:
+        linear = linear_base_with_fixed_params(in_features=3, out_features=1, bias=True)
 
-    def test_linear_with_input_param_quant_dequant(self) -> None:
-        linear = _LinearBase(
-            in_features=3,
-            out_features=1,
-            input_quant=lambda x: x + 3,
-            input_dequant=lambda x: x - 2,
-            param_quant=lambda x: x + 5,
-            param_dequant=lambda x: x - 2,
+        actual = linear(tensor([1, 2, 3]))
+        expected = [7.0]
+
+        self.assertTensorEqual(expected, actual)
+
+    def test_without_bias(self) -> None:
+        linear = linear_base_with_fixed_params(
+            in_features=3, out_features=1, bias=False
         )
-        linear.weight = Parameter(torch.ones_like(linear.weight))
-        linear.bias = Parameter(torch.ones_like(linear.bias))
 
-        input_tensor = torch.as_tensor([1, 2, 3], dtype=torch.float32)
-        actual = to_list(linear(input_tensor))
-        target = [40]
+        actual = linear(tensor([1, 2, 3]))
+        expected = [6.0]
 
-        self.assertEqual(actual, target)
+        self.assertTensorEqual(expected, actual)
 
-    def test_linear_quantized_forward_raises_error(self) -> None:
-        with self.assertRaises(NotImplementedError):
-            _LinearBase(1, 2).quantized_forward(torch.ones(1))
+    def test_input_quant(self) -> None:
+        linear = linear_base_with_fixed_params(
+            in_features=3, out_features=1, bias=True, input_quant=lambda x: x + 3
+        )
 
+        actual = linear(tensor([1, 2, 3]))
+        expected = [16.0]
 
-class FixedPointLinearTest(unittest.TestCase):
-    def test_fixed_point_linear_in_bounds(self) -> None:
-        fp_factory = FixedPoint.get_factory(total_bits=16, frac_bits=8)
-        linear = FixedPointLinear(
+        self.assertTensorEqual(expected, actual)
+
+    def test_param_quant(self) -> None:
+        linear = linear_base_with_fixed_params(
+            in_features=3, out_features=1, bias=True, param_quant=lambda x: x + 3
+        )
+
+        actual = linear(tensor([1, 2, 3]))
+        expected = [28.0]
+
+        self.assertTensorEqual(expected, actual)
+
+    def test_input_param_quant(self) -> None:
+        linear = linear_base_with_fixed_params(
             in_features=3,
             out_features=1,
+            bias=True,
+            input_quant=lambda x: x + 1,
+            param_quant=lambda x: x + 3,
+        )
+
+        actual = linear(tensor([1, 2, 3]))
+        expected = [40.0]
+
+        self.assertTensorEqual(expected, actual)
+
+
+class FixedPointLinearTest(TensorTestCase):
+    def test_with_inputs_in_bounds(self) -> None:
+        fp_factory = FixedPoint.get_factory(total_bits=8, frac_bits=2)
+        linear = fp_linear_with_fixed_params(
+            in_features=3,
+            out_features=1,
+            bias=True,
             fixed_point_factory=fp_factory,
         )
-        linear.weight = Parameter(torch.ones_like(linear.weight))
-        linear.bias = Parameter(torch.ones_like(linear.bias))
 
-        input_tensor = torch.as_tensor([-7, 2, 3], dtype=torch.float32)
-        actual = to_list(linear(input_tensor))
-        target = [-1]
+        actual = linear(tensor([-3.25, 1.5, 0.25]))
+        expected = [-0.5]
 
-        self.assertEqual(actual, target)
+        self.assertTensorEqual(expected, actual)
 
-    def test_fixed_point_linear_out_of_bounds(self) -> None:
-        fp_factory = FixedPoint.get_factory(total_bits=3, frac_bits=0)
-        linear = FixedPointLinear(
+    def test_with_inputs_out_of_bounds(self) -> None:
+        fp_factory = FixedPoint.get_factory(total_bits=2, frac_bits=0)
+        linear = fp_linear_with_fixed_params(
             in_features=3,
             out_features=1,
+            bias=True,
             fixed_point_factory=fp_factory,
         )
-        linear.weight = Parameter(torch.ones_like(linear.weight))
-        linear.bias = Parameter(torch.ones_like(linear.bias))
 
-        input_tensor = torch.as_tensor([1, 2, 3], dtype=torch.float32)
-        actual = to_list(linear(input_tensor))
-        target = [7]
+        actual = linear(tensor([1, 2, 3]))
+        expected = [1.0]
 
-        self.assertEqual(actual, target)
-
-    def test_fixed_point_linear_quantized_forward(self) -> None:
-        fp_factory = FixedPoint.get_factory(total_bits=8, frac_bits=4)
-        linear = FixedPointLinear(
-            in_features=3,
-            out_features=1,
-            fixed_point_factory=fp_factory,
-        )
-        linear.weight = Parameter(torch.ones_like(linear.weight))
-        linear.bias = Parameter(torch.ones_like(linear.bias))
-
-        input_tensor = torch.tensor(
-            [fp_factory(x).to_signed_int() for x in [-1, 0.5, 2]], dtype=torch.float32
-        )
-        expected = [40]
-        actual = to_list(linear.quantized_forward(input_tensor))
-
-        self.assertEqual(expected, actual)
+        self.assertTensorEqual(expected, actual)
