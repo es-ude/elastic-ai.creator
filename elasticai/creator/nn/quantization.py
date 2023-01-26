596dbd8cdf3cde67eedea2779a35ff682c9ac9f7
Julian Hoever <julianhoever@posteo.de>
2023-01-26 15:32:41 +0100

    fix(unit): remove unused OperationType type and FakeQuant class

--- elasticai/creator/nn/quantization.py
+++ elasticai/creator/nn/quantization.py
@@ -1,19 +1,5 @@
 from collections.abc import Callable
-from dataclasses import dataclass
 
 import torch
 
-from elasticai.creator.nn.arithmetics import Arithmetics
-
 QuantType = Callable[[torch.Tensor], torch.Tensor]
-OperationType = Callable[[torch.Tensor, torch.Tensor], torch.Tensor]
-
-
-@dataclass
-class FakeQuant:
-    quant: QuantType
-    dequant: QuantType
-    arithmetics: Arithmetics
-
-    def __call__(self, x: torch.Tensor) -> torch.Tensor:
-        return self.dequant(self.quant(self.arithmetics.clamp(x)))
