54e38d57f27db2d8d0baff5fee3c35a91e26ecd9
Lukas Einhaus <lukas.einhaus@uni-due.de>
2023-03-16 10:11:31 -0400

    feat: binary_arithmetics

---
+++ tests/base_modules/test_binary_arithmetics_with_layers.py
@@ -0,0 +1,22 @@
+import torch
+
+from elasticai.creator.nn.binary_arithmetics import BinaryArithmetics
+from elasticai.creator.nn.linear import Linear
+
+
+class TestLinearLayer:
+    def test_binary_linear_layer(self) -> None:
+        x = [-0.4, 0.1]
+        weight = [[0.1, -0.8], [-1.2, 4.0]]
+        expected = [-1.0, 1.0]
+        linear = Linear(
+            arithmetics=BinaryArithmetics(),
+            in_features=len(x),
+            out_features=len(weight),
+            bias=False,
+        )
+        with torch.no_grad():
+            linear.weight = torch.nn.Parameter(torch.tensor(weight))
+
+        actual = linear(torch.tensor(x)).tolist()
+        assert actual == expected
