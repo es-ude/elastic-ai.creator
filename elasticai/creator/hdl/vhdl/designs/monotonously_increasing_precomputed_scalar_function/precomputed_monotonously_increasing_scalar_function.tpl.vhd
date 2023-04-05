6b59da53a896db7676119de2f74129bcc47287ed
Lukas Einhaus <lukas.einhaus@uni-due.de>
2023-04-05 13:15:21 +0200

    feat: make precomputed scalar functions use unified interface
    First shot at making sure that each hw block uses the same
    port interface. This change introduces a version of
    bufferless hw blocks that route signals through to the next
    block.

--- elasticai/creator/hdl/vhdl/designs/monotonously_increasing_precomputed_scalar_function/precomputed_monotonously_increasing_scalar_function.tpl.vhd
+++ elasticai/creator/hdl/vhdl/designs/monotonously_increasing_precomputed_scalar_function/precomputed_monotonously_increasing_scalar_function.tpl.vhd
@@ -4,14 +4,27 @@ use ieee.numeric_std.all;               -- for type conversions
 
 entity $name is
     port (
-        x : in signed($data_width downto 0);
-        y : out signed($data_width downto 0)
+        enable : in std_logic;
+        clock  : in std_logic;
+        x_addr : out std_logic_vector($x_address_width-1 downto 0);
+        y_addr : in std_logic_vector($y_address_width-1 downto 0);
+
+        x   : in std_logic_vector($data_width-1 downto 0);
+        y  : out std_logic_vector($data_width-1 downto 0);
+
+        done   : out std_logic
     );
 
 end sigmoid;
 
 architecture rtl of $name is
+    signal signed_x, signed_y : signed($data_width-1 downto 0) := (others=>'0');
+
 begin
+    signed_x <- signed(x);
+    y <- std_logic_vector(signed_y);
+    x_addr <- y_addr;
+    done <- enable;
     $name_process : process(x)
     begin
     $process_content
