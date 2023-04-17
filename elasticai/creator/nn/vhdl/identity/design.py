54327fa3e45ca3617d642134ca8d842e7d2afc4c
Julian Hoever <julianhoever@posteo.de>
2023-04-17 10:03:05 +0200

    feat: implement translatable identity module

---
+++ elasticai/creator/nn/vhdl/identity/design.py
@@ -0,0 +1,42 @@
+from elasticai.creator.hdl.code_generation.abstract_base_template import (
+    module_to_package,
+)
+from elasticai.creator.hdl.code_generation.code_generation import (
+    calculate_address_width,
+)
+from elasticai.creator.hdl.design_base.design import Design, Port
+from elasticai.creator.hdl.design_base.ports import create_port_for_buffered_design
+from elasticai.creator.hdl.translatable import Path
+from elasticai.creator.hdl.vhdl.code_generation.template import Template
+
+
+class Identity(Design):
+    def __init__(self, name: str, num_input_features: int, num_input_bits: int) -> None:
+        self._num_input_features = num_input_features
+        self._num_input_bits = num_input_bits
+        self._address_width = calculate_address_width(num_input_features)
+        super().__init__(name)
+
+    @property
+    def port(self) -> Port:
+        return create_port_for_buffered_design(
+            x_width=self._num_input_bits,
+            y_width=self._num_input_bits,
+            x_count=self._num_input_features,
+            y_count=self._num_input_features,
+        )
+
+    def save_to(self, destination: Path) -> None:
+        template = Template(
+            base_name="identity", package=module_to_package(self.__module__)
+        )
+        template.update_parameters(
+            name=self.name,
+            x_address_width=str(self._address_width),
+            y_address_width=str(self._address_width),
+            x_width=str(self._num_input_bits),
+            y_width=str(self._num_input_bits),
+        )
+        destination.create_subpath(self.name).as_file(".vhd").write_text(
+            template.lines()
+        )
---
+++ elasticai/creator/nn/vhdl/identity/layer.py
@@ -0,0 +1,22 @@
+import torch
+
+from elasticai.creator.hdl.design_base.design import Design
+from elasticai.creator.hdl.translatable import Saveable
+from elasticai.creator.nn.vhdl.identity.design import Identity as IdentityDesign
+
+
+class FPIdentity(torch.nn.Identity):
+    def __init__(self, num_input_features: int, total_bits: int) -> None:
+        self._num_input_features = num_input_features
+        self._num_input_bits = total_bits
+        super().__init__()
+
+    def translate(self) -> Saveable:
+        return self.translate_to_vhdl(self.__class__.__name__.lower())
+
+    def translate_to_vhdl(self, name: str) -> Design:
+        return IdentityDesign(
+            name=name,
+            num_input_features=self._num_input_features,
+            num_input_bits=self._num_input_bits,
+        )
