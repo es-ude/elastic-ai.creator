28a75c337734b6bed887b1a3f9fc0369d92d330b
Julian Hoever <julianhoever@posteo.de>
2023-04-21 13:37:09 +0200

    refactor: use identity instead of linear layer to simplify test

--- tests/vhdl/designs/test_sequential.py
+++ tests/vhdl/designs/test_sequential.py
@@ -1,4 +1,4 @@
-import unittest
+from typing import cast
 
 from elasticai.creator.hdl.code_generation.code_generation import (
     calculate_address_width,
@@ -10,67 +10,52 @@ from elasticai.creator.hdl.vhdl.code_generation.code_generation import (
     signal_definition,
 )
 from elasticai.creator.hdl.vhdl.code_generation.template import Template
-from elasticai.creator.in_memory_path import InMemoryPath
-from elasticai.creator.nn.vhdl.fp_linear_1d import FPLinear1d
+from elasticai.creator.in_memory_path import InMemoryFile, InMemoryPath
+from elasticai.creator.nn.vhdl.identity.layer import FPIdentity
 from elasticai.creator.nn.vhdl.sequential import Sequential
 
+"""
+Tests:
+  - [x] replace fplinear1d with minimal layer that implements identity
+  - [x] remove hardsigmoid cases from tests
+  - [ ] check that sequential layer generates a unique name for each instantiated subdesign (entity instance)
+  - [ ] check that each of the above names corresponds to an existing generated subdesign
+  - [ ] test each section (connections, instantiations, etc.) in isolation
+  - [ ] add a second layer with buffer
+"""
 
-class SequentialTestCase(unittest.TestCase):
-    """
-    Tests:
-      - [ ] replace fplinear1d with minimal layer that implements identity
-      - [x] remove hardsigmoid cases from tests
-      - [ ] check that sequential layer generates a unique name for each instantiated subdesign (entity instance)
-      - [ ] check that each of the above names corresponds to an existing generated subdesign
-      - [ ] test each section (connections, instantiations, etc.) in isolation
-      - [ ] add a second layer with buffer
-    """
 
-    def test_empty_sequential(self):
-        module = Sequential(tuple())
-        template = SequentialTemplate(
-            connections=sorted(
-                ["y <= x;", "x_address <= y_address;", "done <= enable;"]
-            ),
-            instantiations=[],
-            signal_definitions=[],
-            x_width="1",
-            y_width="1",
-            x_address_width="1",
-            y_address_width="1",
-            name="sequential",
-        )
-        expected = template.lines()
-        destination = InMemoryPath("sequential", parent=None)
-        design = module.translate("sequential")
-        design.save_to(destination)
-        self.assertEqual(destination["sequential"].text, expected)
+def test_empty_sequential() -> None:
+    module = Sequential(tuple())
+    template = SequentialTemplate(
+        connections=sorted(["y <= x;", "x_address <= y_address;", "done <= enable;"]),
+        instantiations=[],
+        signal_definitions=[],
+        x_width="1",
+        y_width="1",
+        x_address_width="1",
+        y_address_width="1",
+        name="sequential",
+    )
+    expected = template.lines()
+    destination = InMemoryPath("sequential", parent=None)
+    design = module.translate("sequential")
+    design.save_to(destination)
+    assert expected == _extract_code(destination, "sequential")
 
-    def test_with_single_linear_layer(self):
-        bit_width = 16
-        in_features = 6
-        out_features = 3
-        template = _prepare_sequential_template_with_linear(
-            bit_width, in_features, out_features
-        )
-        expected = template.lines()
-        module = Sequential(
-            (
-                (
-                    FPLinear1d(
-                        in_features=6,
-                        out_features=3,
-                        total_bits=16,
-                        frac_bits=8,
-                        bias=False,
-                    ),
-                )
-            )
-        )
-        design = module.translate("sequential")
-        destination = InMemoryPath("sequential", parent=None)
-        design.save_to(destination)
-        self.assertEqual(expected, destination["sequential"].text)
+
+def test_with_single_layer() -> None:
+    num_input_features = 6
+    total_bits = 16
+    template = _prepare_sequential_template_with_identity(
+        num_input_features, total_bits
+    )
+    expected = template.lines()
+    module = Sequential((FPIdentity(num_input_features, total_bits),))
+    design = module.translate("sequential")
+    destination = InMemoryPath("sequential", parent=None)
+    design.save_to(destination)
+    assert expected == _extract_code(destination, "sequential")
 
 
 class SequentialTemplate:
@@ -84,7 +69,7 @@ class SequentialTemplate:
         x_address_width: str,
         y_address_width: str,
         name: str,
-    ):
+    ) -> None:
         self._template = Template(
             "network",
             package="elasticai.creator.hdl.vhdl.designs",
@@ -104,10 +89,10 @@ class SequentialTemplate:
         return self._template.lines()
 
 
-def _prepare_sequential_template_with_linear(
-    bit_width, in_features, out_features
+def _prepare_sequential_template_with_identity(
+    num_input_features: int, total_bits: int
 ) -> SequentialTemplate:
-    entity = "fplinear1d_0"
+    entity = "fpidentity_0"
     instance = f"i_{entity}"
     connections = create_connections(
         {
@@ -136,13 +121,13 @@ def _prepare_sequential_template_with_linear(
         [
             signal_definition(name=f"{instance}_{signal.name}", width=signal.width)
             for signal in (
-                std_signals.x(bit_width),
-                std_signals.y(bit_width),
+                std_signals.x(total_bits),
+                std_signals.y(total_bits),
                 std_signals.clock(),
                 std_signals.enable(),
                 std_signals.done(),
-                std_signals.x_address(calculate_address_width(in_features)),
-                std_signals.y_address(calculate_address_width(out_features)),
+                std_signals.x_address(calculate_address_width(num_input_features)),
+                std_signals.y_address(calculate_address_width(num_input_features)),
             )
         ]
     )
@@ -151,14 +136,14 @@ def _prepare_sequential_template_with_linear(
         connections=connections,
         instantiations=instantiations,
         signal_definitions=signal_definitions,
-        x_width=f"{bit_width}",
-        y_width=f"{bit_width}",
-        x_address_width=str(calculate_address_width(in_features)),
-        y_address_width=str(calculate_address_width(out_features)),
+        x_width=f"{total_bits}",
+        y_width=f"{total_bits}",
+        x_address_width=str(calculate_address_width(num_input_features)),
+        y_address_width=str(calculate_address_width(num_input_features)),
         name="sequential",
     )
     return template
 
 
-if __name__ == "__main__":
-    unittest.main()
+def _extract_code(destination: InMemoryPath, name: str) -> list[str]:
+    return cast(InMemoryFile, destination[name]).text
