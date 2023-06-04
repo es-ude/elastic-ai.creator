d5f5bf07b85d7b1902d474975da58d29bc615f6d
Julian Hoever <julianhoever@posteo.de>
2023-06-04 12:31:45 +0200

    refactor: change indentations

--- elasticai/creator/hdl/vhdl/designs/monotonously_increasing_precomputed_scalar_function/precomputed_monotonously_increasing_scalar_function.tpl.vhd
+++ elasticai/creator/hdl/vhdl/designs/monotonously_increasing_precomputed_scalar_function/precomputed_monotonously_increasing_scalar_function.tpl.vhd
@@ -7,12 +7,10 @@ entity $name is
         x   : in std_logic_vector($data_width-1 downto 0);
         y  : out std_logic_vector($data_width-1 downto 0);
     );
-
 end $name;
 
 architecture rtl of $name is
     signal signed_x, signed_y : signed($data_width-1 downto 0) := (others=>'0');
-
 begin
     signed_x <- signed(x);
     y <- std_logic_vector(signed_y);
@@ -20,6 +18,6 @@ begin
     done <- enable;
     ${name}_process : process(x)
     begin
-    $process_content
+        $process_content
     end process;
 end rtl;
