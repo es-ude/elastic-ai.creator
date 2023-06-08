9b86f9d440cc991d624a6f3492a3caf7419bdbf3
Julian Hoever <julianhoever@posteo.de>
2023-06-08 21:13:31 +0200

    feat: reimplement hard tanh activation function

---
+++ elasticai/creator/base_modules/hard_tanh.py
@@ -0,0 +1,6 @@
+import torch
+
+
+class HardTanh(torch.nn.Hardtanh):
+    def __init__(self, min_val: float = -1, max_val: float = 1) -> None:
+        super().__init__(min_val=min_val, max_val=max_val, inplace=False)
---
+++ elasticai/creator/nn/vhdl/hard_tanh/design.py
@@ -0,0 +1,38 @@
+from elasticai.creator.hdl.code_generation.template import (
+    InProjectTemplate,
+    module_to_package,
+)
+from elasticai.creator.hdl.design_base.design import Design, Port
+from elasticai.creator.hdl.design_base.ports import create_port_for_base_design
+from elasticai.creator.hdl.savable import Path
+
+
+class FPHardTanh(Design):
+    def __init__(
+        self, name: str, total_bits: int, frac_bits: int, min_val: int, max_val: int
+    ) -> None:
+        super().__init__(name)
+        self._total_bits = total_bits
+        self._frac_bits = frac_bits
+        self._min_val = min_val
+        self._max_val = max_val
+
+    @property
+    def port(self) -> Port:
+        return create_port_for_base_design(
+            x_width=self._total_bits, y_width=self._total_bits
+        )
+
+    def save_to(self, destination: Path) -> None:
+        template = InProjectTemplate(
+            package=module_to_package(self.__module__),
+            file_name="fp_hard_tanh.tpl.vhd",
+            parameters=dict(
+                layer_name=self.name,
+                data_width=str(self._total_bits),
+                frac_width=str(self._frac_bits),
+                min_val=str(self._min_val),
+                max_val=str(self._max_val),
+            ),
+        )
+        destination.create_subpath(self.name).as_file(".vhd").write(template)
