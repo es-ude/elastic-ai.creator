79b7a1eea0cb71f5a838cfebf02970927410f594
Julian Hoever <julianhoever@posteo.de>
2023-06-08 14:19:12 +0200

    feat: make sure that inplace parameter is fixed defined

--- elasticai/creator/base_modules/hard_sigmoid.py
+++ elasticai/creator/base_modules/hard_sigmoid.py
@@ -2,5 +2,5 @@ import torch
 
 
 class HardSigmoid(torch.nn.Hardsigmoid):
-    def __init__(self, inplace: bool = False) -> None:
-        super().__init__(inplace=inplace)
+    def __init__(self) -> None:
+        super().__init__(inplace=False)
