9ba64ae3d253db76a6368c5e561ce28bcec2aab5
Julian Hoever <julianhoever@posteo.de>
2023-06-09 09:32:45 +0200

    feat: add tests for the FPMonotonouslyIncreasingModule

---
+++ tests/vhdl/designs/test_fp_monotonously_increasing_module.py
@@ -0,0 +1,96 @@
+from typing import cast
+
+import torch
+
+from elasticai.creator.hdl.vhdl.designs.monotonously_increasing_precomputed_scalar_function.fp_monotonously_increasing_module import (
+    FPMonotonouslyIncreasingModule,
+)
+from elasticai.creator.in_memory_path import InMemoryFile, InMemoryPath
+
+
+def test_vhdl_code_matches_expected_for_tanh_as_base_module() -> None:
+    expected = """library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;               -- for type conversions
+
+entity tanh is
+    port (
+        x   : in std_logic_vector(8-1 downto 0);
+        y  : out std_logic_vector(8-1 downto 0);
+    );
+end tanh;
+
+architecture rtl of tanh is
+    signal signed_x, signed_y : signed(8-1 downto 0) := (others=>'0');
+begin
+    signed_x <- signed(x);
+    y <- std_logic_vector(signed_y);
+    x_addr <- y_addr;
+    done <- enable;
+    tanh_process : process(x)
+    begin
+        if signed_x <= 20 then signed_y <= to_signed(3, 8);
+        if signed_x <= 10 then signed_y <= to_signed(3, 8);
+        if signed_x <= 0 then signed_y <= to_signed(0, 8);
+        if signed_x <= -10 then signed_y <= to_signed(-3, 8);
+        else signed_y <= to_signed(-3, 8);
+        end if;
+    end process;
+end rtl;
+""".splitlines()
+    tanh = FPMonotonouslyIncreasingModule(
+        base_module=torch.nn.Tanh(),
+        total_bits=8,
+        frac_bits=2,
+        num_steps=5,
+        sampling_intervall=(-5, 5),
+    )
+    build_path = InMemoryPath("build", parent=None)
+    design = tanh.translate("tanh")
+    design.save_to(build_path)
+    actual = cast(InMemoryFile, build_path["tanh"]).text
+    assert actual == expected
+
+
+def test_vhdl_code_matches_expected_for_sigmoid_as_base_module() -> None:
+    expected = """library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;               -- for type conversions
+
+entity sigmoid is
+    port (
+        x   : in std_logic_vector(8-1 downto 0);
+        y  : out std_logic_vector(8-1 downto 0);
+    );
+end sigmoid;
+
+architecture rtl of sigmoid is
+    signal signed_x, signed_y : signed(8-1 downto 0) := (others=>'0');
+begin
+    signed_x <- signed(x);
+    y <- std_logic_vector(signed_y);
+    x_addr <- y_addr;
+    done <- enable;
+    sigmoid_process : process(x)
+    begin
+        if signed_x <= 20 then signed_y <= to_signed(3, 8);
+        if signed_x <= 10 then signed_y <= to_signed(3, 8);
+        if signed_x <= 0 then signed_y <= to_signed(2, 8);
+        if signed_x <= -10 then signed_y <= to_signed(0, 8);
+        else signed_y <= to_signed(0, 8);
+        end if;
+    end process;
+end rtl;
+""".splitlines()
+    sigmoid = FPMonotonouslyIncreasingModule(
+        base_module=torch.nn.Sigmoid(),
+        total_bits=8,
+        frac_bits=2,
+        num_steps=5,
+        sampling_intervall=(-5, 5),
+    )
+    build_path = InMemoryPath("build", parent=None)
+    design = sigmoid.translate("sigmoid")
+    design.save_to(build_path)
+    actual = cast(InMemoryFile, build_path["sigmoid"]).text
+    assert actual == expected
