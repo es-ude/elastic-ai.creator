707310b3202ec1b48f847a228455f8cd77436219
Julian Hoever <julianhoever@posteo.de>
2023-06-09 19:14:03 +0200

    refactor: rename template and remove some newlines

--- elasticai/creator/nn/vhdl/identity/bufferless_identity.tpl.vhd
+++ elasticai/creator/nn/vhdl/identity/bufferless_identity.tpl.vhd
@@ -6,10 +6,8 @@ entity $name is
     port (
         enable : in std_logic;
         clock  : in std_logic;
-
         x   : in std_logic_vector($x_width-1 downto 0);
         y  : out std_logic_vector($y_width-1 downto 0);
-
     );
 end $name;
 
