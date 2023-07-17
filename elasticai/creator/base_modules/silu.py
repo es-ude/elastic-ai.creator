bb2b7a81bf6365a54590575f39a447b6cd769cd9
Silas Brandenburg <silas.brandenburg@stud.uni-due.de>
2023-07-17 14:00:51 +0200

    refactor: changed names of learnable parameters in the swish function

--- elasticai/creator/base_modules/silu.py
+++ elasticai/creator/base_modules/silu.py
@@ -7,10 +7,10 @@ class SiLU(torch.nn.SiLU):
     def __init__(self, arithmetics: Arithmetics) -> None:
         super().__init__(inplace=False)
         self._arithmetics = arithmetics
-        self.beta = torch.nn.Parameter(torch.ones(1, requires_grad=True))
-        self.asymetric_offset = torch.nn.Parameter(torch.zeros(1, requires_grad=True))
+        self.scale = torch.nn.Parameter(torch.ones(1, requires_grad=True))
+        self.beta = torch.nn.Parameter(torch.zeros(1, requires_grad=True))
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
-        a = super().forward(self.beta * input - self.asymetric_offset)
+        a = super().forward(self.scale * input - self.beta)
         x = self._arithmetics.quantize(a)
         return x
