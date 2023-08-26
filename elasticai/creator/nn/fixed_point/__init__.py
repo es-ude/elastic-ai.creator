93917d8654794b7f5baa005e989f2984d6c846e3
Julian Hoever <julianhoever@posteo.de>
2023-08-26 16:14:33 +0200

    feat: add layers to __init__ file in fixed_point package to improve usability

---
+++ elasticai/creator/nn/fixed_point/__init__.py
@@ -0,0 +1,6 @@
+from .conv1d import BatchNormedConv1d, Conv1d
+from .hard_sigmoid import HardSigmoid
+from .hard_tanh import HardTanh
+from .linear import BatchNormedLinear, Linear
+from .precomputed import Sigmoid, SiLUWithTrainableScaleBeta, Tanh
+from .relu import ReLU
