715d6c7540748fb53e93f78782f653ee45e6bdb4
Lukas Einhaus <lukas.einhaus@uni-due.de>
2023-09-01 10:21:03 +0200

    refactor: simplify number conversion implementations

--- elasticai/creator/nn/fixed_point/mac/number_conversion.py
+++ elasticai/creator/nn/fixed_point/mac/number_conversion.py
@@ -1,23 +1,22 @@
-def _toggle_bits_for_negative_number(number: int, total_bits: int) -> int:
-    return -(number + int("1" * total_bits, 2))
-
-
-def _toggle_bits_for_positive_number(number: int, total_bits: int) -> int:
-    return -_toggle_bits_for_negative_number(-number, total_bits)
-
-
 def _toggle_bits(number: int, total_bits: int) -> int:
-    if number < 0:
-        return _toggle_bits_for_negative_number(number, total_bits)
-    return _toggle_bits_for_positive_number(number, total_bits)
+    def invert(value: int) -> int:
+        return value ^ int("1" * total_bits, 2)
+
+    def discard_leading_bits(value: int) -> int:
+        return value & int("1" * total_bits, 2)
+
+    return discard_leading_bits(invert(abs(number)))
+
+
+def _twos_complement(number, total_bits):
+    return _toggle_bits(number, total_bits) + 1
 
 
 def bits_to_integer(pattern: str) -> int:
     is_negative = pattern[0] == "1"
     number = int(pattern, 2)
     if is_negative:
-        number = _toggle_bits(number, len(pattern)) + 1
-        number = -number
+        number = -_twos_complement(number, len(pattern))
     return number
 
 
@@ -36,24 +35,36 @@ def bits_to_natural(pattern: str) -> int:
 
 def integer_to_bits(number: int, total_bits: int) -> str:
     if number < 0:
-        number = -_toggle_bits(number, total_bits) + 1
+        number = _twos_complement(number, total_bits)
     return f"{number:0{total_bits}b}"
 
 
+def _max_twos_complement_pattern(total_bits):
+    return "0" + "1" * (total_bits - 1)
+
+
+def _min_twos_complement_pattern(total_bits):
+    return "1" + "0" * (total_bits - 1)
+
+
 def max_rational(total_bits: int, frac_bits: int) -> float:
-    return bits_to_rational("0" + "1" * (total_bits - 1), frac_bits=frac_bits)
+    return bits_to_rational(
+        _max_twos_complement_pattern(total_bits), frac_bits=frac_bits
+    )
 
 
 def min_rational(total_bits: int, frac_bits: int) -> float:
-    return bits_to_rational("1" + "0" * (total_bits - 1), frac_bits=frac_bits)
+    return bits_to_rational(
+        _min_twos_complement_pattern(total_bits), frac_bits=frac_bits
+    )
 
 
 def min_integer(total_bits: int) -> int:
-    return bits_to_integer("1" + "0" * (total_bits - 1))
+    return bits_to_integer(_min_twos_complement_pattern(total_bits))
 
 
 def max_integer(total_bits: int) -> int:
-    return bits_to_integer("0" + "1" * (total_bits - 1))
+    return bits_to_integer(_max_twos_complement_pattern(total_bits))
 
 
 def min_natural(total_bits: int) -> int:
