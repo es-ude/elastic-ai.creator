e4f562789a71067d045bdf2591cccae2a308bf58
Jasmin Scharf <j.scharf@live.de>
2023-09-28 14:43:17 +0200

    feat(commitlint): add commitlint with easy installation

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,1 @@
-module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [2, "always", ['feat', 'fix', 'docs', 'style', 'refactor', 'revert', 'chore', 'wip', 'perf']]
-    }
-}
+module.exports = {extends: ['@commitlint/config-conventional']}
