67117a443032cbafd3bcf8abcab7177a801fd659
Leo Buron <leo.buron@uni-due.de>
2023-10-04 15:25:47 +0200

    feat: added skeleton, middleware, top module and Vivado constraints fopr env5 LSTM example

---
+++ elasticai/creator/vhdl/system_integrations/env5_constraints/env5_constraints.py
@@ -0,0 +1,8 @@
+from elasticai.creator.file_generation.savable import Path
+from elasticai.creator.file_generation.template import InProjectTemplate, module_to_package
+
+class ENV5Constraints:
+    def save_to(self, destination: Path):
+        template = InProjectTemplate(package=module_to_package(self.__module__), file_name='env5_config.xdc', parameters={})
+        file = destination.as_file('.xdc')
+        file.write(template)
