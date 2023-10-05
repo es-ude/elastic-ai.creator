f879b4de526bc7c7cd93371b5874c1eac2f465f5
Lukas Einhaus <lukas.einhaus@uni-due.de>
2023-10-05 15:03:56 +0200

    fix: fix lstm test bench file name

--- elasticai/creator/nn/fixed_point/lstm/design/testbench.py
+++ elasticai/creator/nn/fixed_point/lstm/design/testbench.py
@@ -9,15 +9,13 @@ from elasticai.creator.vhdl.design.design import Design
 class LSTMTestBench:
     def __init__(self, name: str, uut: Design):
         self._uut = uut
-        self._base_name = name
+        self._name = name
 
     def save_to(self, destination: Path):
         test_bench = InProjectTemplate(
             package=module_to_package(self.__module__),
-            file_name=f"{self._base_name}_tb.tpl.vhd",
+            file_name=f"lstm_network_tb.tpl.vhd",
             parameters={"uut_name": self._uut.name},
         )
-        destination.create_subpath(f"{self._base_name}_tb").as_file(".vhd").write(
-            test_bench
-        )
+        destination.create_subpath(f"{self._name}_tb").as_file(".vhd").write(test_bench)
         self._uut.save_to(destination)
