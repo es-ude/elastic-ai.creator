baad73b8e97f5c7b65e52a1c9755eb2086de02aa
Lukas Einhaus <lukas.einhaus@uni-due.de>
2023-10-07 14:41:14 +0200

    fix: fix fxp mac test

--- elasticai/creator/nn/fixed_point/mac/_hw_test.py
+++ elasticai/creator/nn/fixed_point/mac/_hw_test.py
@@ -49,23 +49,21 @@ def test_mac_hw_for_integers(tmp_path, fxp_params, x1, x2):
     assert y == actual
 
 
-@pytest.mark.skip
 @pytest.mark.parametrize(
     "x1, x2, expected",
     [
         # 00001 * 00010 + 00100 * 00001 -> 00000 00010 + 00000 00100 -> 00000 00110 -> 000(00 001)10 -> 00001
-        ((0.25, 1.0), (0.5, 0.25), 0.5),
-        # 11111 * 00010 + 11100 * 00010 -> 11111 11111 * 00000 00010 + 11111 11100 * 00010
+        ((0.25, 1.0), (0.5, 0.25), 0.25),
+        # 11111 * 00010 + 11100 * 00010 -> 11111 11111 * 00000 00010 + 11111 11100 * 00000 00010
         #  ->  11111 11110
         #    + 11111 11000
-        #   -> 11111 10110 -> 111(00 101)10 -> rounding up: 111(00 110)10 ->
-        ((-0.25, -1.0), (0.5, 0.5), -0.75),
+        #   -> 11111 10110 -> 111(11 101)10 -> rounding up: 111(11 110)10 -> 11110
+        ((-0.25, -1.0), (0.5, 0.5), -0.5),
     ],
 )
 def test_sw_mac_rounds_towards_zero(x1, x2, expected):
     fxp_params = FXPParams(total_bits=5, frac_bits=2)
-    x1 = (0.25, 1.0)
-    x2 = (0.5, 0.25)
-    mac = MacLayer(fxp_params=fxp_params)
+
+    mac = MacLayer(fxp_params=fxp_params, vector_width=len(x1))
     y = mac(torch.tensor(x1), torch.tensor(x2)).item()
-    assert 0.5 == y
+    assert expected == y
