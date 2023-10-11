8ad3272c80a350348df1ce7a562df6f51928a4ee
Julian Hoever <julianhoever@posteo.de>
2023-10-11 13:19:42 +0200

    feat(firmware): new firmware that does not save testbenches

--- elasticai/creator/vhdl/system_integrations/firmware_env5.py
+++ elasticai/creator/vhdl/system_integrations/firmware_env5.py
@@ -1,45 +1,28 @@
-import warnings
-
 from elasticai.creator.file_generation.savable import Path
+from elasticai.creator.vhdl.design.design import Design
 from elasticai.creator.vhdl.design_creator import DesignCreator
 from elasticai.creator.vhdl.system_integrations.env5_constraints.env5_constraints import (
     ENV5Constraints,
 )
 from elasticai.creator.vhdl.system_integrations.middleware.middleware import Middleware
-from elasticai.creator.vhdl.system_integrations.skeleton.skeleton import LSTMSkeleton
+from elasticai.creator.vhdl.system_integrations.skeleton.skeleton import Skeleton
 from elasticai.creator.vhdl.system_integrations.top.env5_reconfig_top import (
     ENV5ReconfigTop,
 )
 
-warnings.warn(
-    message=(
-        "Will be removed. Use FirmwareENV5 from the"
-        " elasticai.creator.vhdl.system_integrations.firmware_env5 package"
-        " instead."
-    ),
-    category=DeprecationWarning,
-)
-
 
 class FirmwareENv5:
-    def __init__(self, model: DesignCreator):
-        self._network = model.create_design("network")
-        self._testbench = model.create_testbench("network_tb", self._network)
+    def __init__(self, network: Design) -> None:
+        self._network = network
 
-    def save_to(self, destination: Path):
+    def save_to(self, destination: Path) -> None:
         def save_srcs(destination: Path):
-            lstm_skeleton = LSTMSkeleton(self._network.name)
-            lstm_skeleton.save_to(destination.create_subpath("skeleton"))
-
-            self._network.save_to(destination.create_subpath(self._network.name))
+            skeleton = Skeleton(self._network.name, self._network.port)
+            skeleton.save_to(destination.create_subpath("skeleton"))
 
             middleware = Middleware()
             middleware.save_to(destination.create_subpath("middleware"))
-            self._testbench.save_to(
-                destination.create_subpath("test_benches").create_subpath(
-                    self._testbench.name
-                )
-            )
+
             env5_reconfig_top = ENV5ReconfigTop()
             env5_reconfig_top.save_to(destination.create_subpath("env5_reconfig_top"))
 
