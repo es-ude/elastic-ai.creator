b4ffacb1847685851def6beb9f53044fe5dbd75f
Julian Hoever <julianhoever@posteo.de>
2023-10-11 12:00:46 +0200

    feat(skeleton): add general skeleton class

--- elasticai/creator/vhdl/system_integrations/skeleton/skeleton.py
+++ elasticai/creator/vhdl/system_integrations/skeleton/skeleton.py
@@ -3,6 +3,28 @@ from elasticai.creator.file_generation.template import (
     InProjectTemplate,
     module_to_package,
 )
+from elasticai.creator.vhdl.design.ports import Port
+
+
+class Skeleton:
+    def __init__(self, network_name: str, port: Port):
+        self.name = "skeleton"
+        self._network_name = network_name
+        self._port = port
+
+    def save_to(self, destination: Path):
+        template = InProjectTemplate(
+            package=module_to_package(self.__module__),
+            file_name="network_skeleton.tpl.vhd",
+            parameters=dict(
+                name=self.name,
+                network_name=self._network_name,
+                data_width=str(self._port["x"].width),
+                x_addr_width=str(self._port["x_address"].width),
+            ),
+        )
+        file = destination.as_file(".vhd")
+        file.write(template)
 
 
 class LSTMSkeleton:
