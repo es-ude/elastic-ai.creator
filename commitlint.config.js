6a3b5d90479d62bde430a24cf67aab196b2ca5e8
Jasmin Scharf <j.scharf@live.de>
2023-10-22 20:47:05 +0200

    fix(commitlint): add gitignore

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,6 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [2, "always", ['feat', 'fix', 'docs', 'style', 'refactor', 'revert', 'chore', 'wip', 'perf']]
+    }
+}
