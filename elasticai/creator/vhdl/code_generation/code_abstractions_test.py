c94dc3ba59698e87eac2efe702a43dfc925401bd
Leo Buron <leo.buron@uni-due.de>
2023-10-31 15:12:36 +0100

    feat: convert negative numbers to bit patterns using two's complement

--- elasticai/creator/vhdl/code_generation/code_abstractions_test.py
+++ elasticai/creator/vhdl/code_generation/code_abstractions_test.py
@@ -13,10 +13,10 @@ def test_to_vhdl_binary_string_raises_error_if_value_not_representable() -> None
     [
         (0, 1, '"0"'),
         (1, 1, '"1"'),
-        (6, 3, '"110"'),
-        (-6, 3, '"-110"'),
+        (6, 4, '"0110"'),
+        (-6, 4, '"1010"'),
         (7, 5, '"00111"'),
-        (-7, 5, '"-00111"'),
+        (-7, 5, '"11001"'),
     ],
 )
 def test_to_vhdl_binary_string(number: int, number_of_bits: int, expected: str) -> None:
