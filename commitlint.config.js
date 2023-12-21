3e1d509e23d8aa5302e708bb309514294b9d7984
Julian Hoever <julianhoever@posteo.de>
2023-12-21 13:28:01 +0100

    chore: only throw a warning if commit message exceeds char limit
    Signed-off-by: Julian Hoever <julianhoever@posteo.de>

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'type-enum': [2, "always", ['feat', 'fix', 'docs', 'style', 'refactor', 'revert', 'chore', 'wip', 'perf']]
+        'type-enum': [2, "always", ['feat', 'fix', 'docs', 'style', 'refactor', 'revert', 'chore', 'wip', 'perf']],
+        'header-max-length': [1, 'always', 100]
     }
 }
