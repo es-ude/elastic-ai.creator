00fcfeaaf2bd880dd3cc1138fb988af3c75a4e70
SuperChange001 <chaoqian1119@gmail.com>
2024-05-28 10:16:47 +0200

    Revert "Merging main to develop"

--- elasticai/creator/vhdl/shared_designs/precomputed_scalar_function/precomputed_scalar_function.tpl.vhd
+++ elasticai/creator/vhdl/shared_designs/precomputed_scalar_function/precomputed_scalar_function.tpl.vhd
@@ -2,23 +2,22 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;               -- for type conversions
 
-entity ${name} is
+entity $name is
     port (
         enable : in std_logic;
         clock  : in std_logic;
-        x      : in std_logic_vector(${input_data_width}-1 downto 0);
-        y      : out std_logic_vector(${output_data_width}-1 downto 0)
+        x      : in std_logic_vector($data_width-1 downto 0);
+        y      : out std_logic_vector($data_width-1 downto 0)
     );
-end ${name};
+end $name;
 
-architecture rtl of ${name} is
-    signal signed_x : signed(${input_data_width}-1 downto 0) := (others=>'0');
-    signal signed_y : signed(${output_data_width}-1 downto 0) := (others=>'0');
+architecture rtl of $name is
+    signal signed_x, signed_y : signed($data_width-1 downto 0) := (others=>'0');
 begin
     signed_x <= signed(x);
     y <= std_logic_vector(signed_y);
     ${name}_process : process(x)
     begin
-        ${process_content}
+        $process_content
     end process;
 end rtl;
