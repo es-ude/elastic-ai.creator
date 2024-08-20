bd73847da77e3cd5512ea07a3558519dcbe5e922
Edwina1030 <edwina1030@hotmail.com>
2024-08-20 13:10:23 +0200

    fix: changed the test framework used by create_makefile_test

--- elasticai/creator/nn/integer/vhdl_test_automation/create_makefile_test.py
+++ elasticai/creator/nn/integer/vhdl_test_automation/create_makefile_test.py
@@ -1,38 +1,35 @@
 import os
 import tempfile
-import unittest
 
 from elasticai.creator.nn.integer.vhdl_test_automation.create_makefile import (
     create_makefile,
 )
 
 
-class TestCreateMakefile(unittest.TestCase):
-    def test_makefile_creation(self):
-        with tempfile.TemporaryDirectory() as temp_dir:
-            stop_time = "5000ns"
-            create_makefile(temp_dir, stop_time=stop_time)
+def test_makefile_creation():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        stop_time = "5000ns"
+        create_makefile(temp_dir, stop_time=stop_time)
 
-            makefile_path = os.path.join(temp_dir, "makefile")
-            self.assertTrue(os.path.exists(makefile_path))
+        makefile_path = os.path.join(temp_dir, "makefile")
+        assert os.path.exists(makefile_path)
 
-            with open(makefile_path, "r") as file:
-                content = file.read()
-                self.assertIn(f"STOP_TIME = {stop_time}", content)
-                self.assertIn("FILES = ./source/*/*.vhd", content)
-                self.assertIn(
-                    "GHDL_FLAGS  = --ieee=synopsys --warn-no-vital-generic", content
-                )
-                self.assertIn("WAVEFORM_VIEWER = gtkwave", content)
+        with open(makefile_path, "r") as file:
+            content = file.read()
+            assert f"STOP_TIME = {stop_time}" in content
+            assert "FILES = ./source/*/*.vhd" in content
+            assert "GHDL_FLAGS  = --ieee=synopsys --warn-no-vital-generic" in content
+            assert "WAVEFORM_VIEWER = gtkwave" in content
 
-    def test_default_stop_time(self):
-        with tempfile.TemporaryDirectory() as temp_dir:
-            create_makefile(temp_dir)
 
-            makefile_path = os.path.join(temp_dir, "makefile")
-            self.assertTrue(os.path.exists(makefile_path))
+def test_default_stop_time():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        create_makefile(temp_dir)
 
-            with open(makefile_path, "r") as file:
-                content = file.read()
-                self.assertIn("STOP_TIME = 4000ns", content)
-                self.assertIn("FILES = ./source/*/*.vhd", content)
+        makefile_path = os.path.join(temp_dir, "makefile")
+        assert os.path.exists(makefile_path)
+
+        with open(makefile_path, "r") as file:
+            content = file.read()
+            assert "STOP_TIME = 4000ns" in content
+            assert "FILES = ./source/*/*.vhd" in content
