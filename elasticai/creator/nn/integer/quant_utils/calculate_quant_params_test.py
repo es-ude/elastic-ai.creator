5dce0692f24f8b355202eb4935b035fcc22ec3ae
Edwina1030 <edwina1030@hotmail.com>
2024-08-20 12:19:22 +0200

    fix: changed the test framework used by calculate_quant_params_test

--- elasticai/creator/nn/integer/quant_utils/calculate_quant_params_test.py
+++ elasticai/creator/nn/integer/quant_utils/calculate_quant_params_test.py
@@ -1,5 +1,3 @@
-import unittest
-
 import torch
 
 from elasticai.creator.nn.integer.quant_utils.calculate_quant_params import (
@@ -8,43 +6,39 @@ from elasticai.creator.nn.integer.quant_utils.calculate_quant_params import (
 )
 
 
-class TestCalculateQuantParams(unittest.TestCase):
-    def test_calculate_asymmetric_quant_params_signed(self):
-        min_float = torch.tensor([-3.2], dtype=torch.float32)
-        max_float = torch.tensor([2.5], dtype=torch.float32)
-        min_quant = torch.tensor([-128], dtype=torch.int32)
-        max_quant = torch.tensor([127], dtype=torch.int32)
-        eps = torch.tensor(torch.finfo(torch.float32).eps)
+def test_calculate_asymmetric_quant_params_signed():
+    min_float = torch.tensor([-3.2], dtype=torch.float32)
+    max_float = torch.tensor([2.5], dtype=torch.float32)
+    min_quant = torch.tensor([-128], dtype=torch.int32)
+    max_quant = torch.tensor([127], dtype=torch.int32)
+    eps = torch.tensor(torch.finfo(torch.float32).eps)
 
-        scale_factor, zero_point, min_float, max_float = (
-            calculate_asymmetric_quant_params(
-                min_float, max_float, min_quant, max_quant, eps
-            )
-        )
+    scale_factor, zero_point, min_float, max_float = calculate_asymmetric_quant_params(
+        min_float, max_float, min_quant, max_quant, eps
+    )
 
-        self.assertAlmostEqual(
-            scale_factor, torch.tensor([0.0223529413], dtype=torch.float32), places=10
-        )
-        self.assertEqual(zero_point, torch.tensor([15], dtype=torch.int32))
-        self.assertEqual(min_float, torch.tensor([-3.2], dtype=torch.float32))
-        self.assertEqual(max_float, torch.tensor([2.5], dtype=torch.float32))
+    assert torch.isclose(
+        scale_factor, torch.tensor([0.0223529413], dtype=torch.float32), atol=1e-10
+    )
+    assert torch.equal(zero_point, torch.tensor([15], dtype=torch.int32))
+    assert torch.equal(min_float, torch.tensor([-3.2], dtype=torch.float32))
+    assert torch.equal(max_float, torch.tensor([2.5], dtype=torch.float32))
 
-    def test_calculate_symmetric_quant_params_signed(self):
-        min_quant = torch.tensor([-127], dtype=torch.int32)
-        max_quant = torch.tensor([127], dtype=torch.int32)
-        min_float = torch.tensor([-1.002], dtype=torch.float32)
-        max_float = torch.tensor([1.0], dtype=torch.float32)
-        eps = torch.tensor(torch.finfo(torch.float32).eps)
 
-        scale_factor, zero_point, min_float, max_float = (
-            calculate_symmetric_quant_params(
-                min_quant, max_quant, min_float, max_float, eps
-            )
-        )
+def test_calculate_symmetric_quant_params_signed():
+    min_quant = torch.tensor([-127], dtype=torch.int32)
+    max_quant = torch.tensor([127], dtype=torch.int32)
+    min_float = torch.tensor([-1.002], dtype=torch.float32)
+    max_float = torch.tensor([1.0], dtype=torch.float32)
+    eps = torch.tensor(torch.finfo(torch.float32).eps)
 
-        self.assertAlmostEqual(
-            scale_factor, torch.tensor([0.00788976377], dtype=torch.float32), places=6
-        )
-        self.assertEqual(zero_point, torch.tensor([0], dtype=torch.int32))
-        self.assertEqual(min_float, torch.tensor([-1.002], dtype=torch.float32))
-        self.assertEqual(max_float, torch.tensor([1.002], dtype=torch.float32))
+    scale_factor, zero_point, min_float, max_float = calculate_symmetric_quant_params(
+        min_quant, max_quant, min_float, max_float, eps
+    )
+
+    assert torch.isclose(
+        scale_factor, torch.tensor([0.00788976377], dtype=torch.float32), atol=1e-10
+    )
+    assert torch.equal(zero_point, torch.tensor([0], dtype=torch.int32))
+    assert torch.equal(min_float, torch.tensor([-1.002], dtype=torch.float32))
+    assert torch.equal(max_float, torch.tensor([1.002], dtype=torch.float32))
