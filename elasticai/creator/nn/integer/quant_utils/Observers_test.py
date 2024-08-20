e8eab4b2200afbb8e61e7eef41794d67bc9cfd09
Edwina1030 <edwina1030@hotmail.com>
2024-08-20 12:24:27 +0200

    fix: changed the test framework used by Observers_test

--- elasticai/creator/nn/integer/quant_utils/Observers_test.py
+++ elasticai/creator/nn/integer/quant_utils/Observers_test.py
@@ -1,5 +1,3 @@
-import unittest
-
 import torch
 
 from elasticai.creator.nn.integer.quant_utils.Observers import (
@@ -9,41 +7,44 @@ from elasticai.creator.nn.integer.quant_utils.Observers import (
 )
 
 
-class TestObservers(unittest.TestCase):
-    def _setUp1_for_GlobalMinMaxObserver(self):
-        tensor1 = torch.tensor([-1.0, 2.0, 3.0, 4.0], dtype=torch.float32)
-        tensor2 = torch.tensor([-5.0, 6.0, 7.0, 8.0], dtype=torch.float32)
-        tensor3 = torch.tensor([-9.0, 10.0, 11.0, 12.0], dtype=torch.float32)
-        return tensor1, tensor2, tensor3
+def setUp1_for_GlobalMinMaxObserver():
+    tensor1 = torch.tensor([-1.0, 2.0, 3.0, 4.0], dtype=torch.float32)
+    tensor2 = torch.tensor([-5.0, 6.0, 7.0, 8.0], dtype=torch.float32)
+    tensor3 = torch.tensor([-9.0, 10.0, 11.0, 12.0], dtype=torch.float32)
+    return tensor1, tensor2, tensor3
 
-    def _setUp2_for_GlobalMinMaxObserver(self):
-        tensor1 = torch.tensor([-9.0, 10.0, 11.0, 12.0], dtype=torch.float32)
-        tensor2 = torch.tensor([-5.0, 6.0, 7.0, 8.0], dtype=torch.float32)
-        tensor3 = torch.tensor([-1.0, 2.0, 3.0, 4.0], dtype=torch.float32)
-        return tensor1, tensor2, tensor3
 
-    def test_GlobalMinMaxObserver_setUp1(self):
-        tensor1, tensor2, tensor3 = self._setUp1_for_GlobalMinMaxObserver()
-        observer = GlobalMinMaxObserver()
-        observer(tensor1)
-        self.assertEqual(observer.min_float, torch.tensor([-1.0], dtype=torch.float32))
-        self.assertEqual(observer.max_float, torch.tensor([4.0], dtype=torch.float32))
-        observer(tensor2)
-        self.assertEqual(observer.min_float, torch.tensor([-5.0], dtype=torch.float32))
-        self.assertEqual(observer.max_float, torch.tensor([8.0], dtype=torch.float32))
-        observer(tensor3)
-        self.assertEqual(observer.min_float, torch.tensor([-9.0], dtype=torch.float32))
-        self.assertEqual(observer.max_float, torch.tensor([12.0], dtype=torch.float32))
+def _setUp2_for_GlobalMinMaxObserver():
+    tensor1 = torch.tensor([-9.0, 10.0, 11.0, 12.0], dtype=torch.float32)
+    tensor2 = torch.tensor([-5.0, 6.0, 7.0, 8.0], dtype=torch.float32)
+    tensor3 = torch.tensor([-1.0, 2.0, 3.0, 4.0], dtype=torch.float32)
+    return tensor1, tensor2, tensor3
 
-    def test_GlobalMinMaxObserver_setUp2(self):
-        tensor1, tensor2, tensor3 = self._setUp2_for_GlobalMinMaxObserver()
-        observer = GlobalMinMaxObserver()
-        observer(tensor1)
-        self.assertEqual(observer.min_float, torch.tensor([-9.0], dtype=torch.float32))
-        self.assertEqual(observer.max_float, torch.tensor([12.0], dtype=torch.float32))
-        observer(tensor2)
-        self.assertEqual(observer.min_float, torch.tensor([-9.0], dtype=torch.float32))
-        self.assertEqual(observer.max_float, torch.tensor([12.0], dtype=torch.float32))
-        observer(tensor3)
-        self.assertEqual(observer.min_float, torch.tensor([-9.0], dtype=torch.float32))
-        self.assertEqual(observer.max_float, torch.tensor([12.0], dtype=torch.float32))
+
+def test_GlobalMinMaxObserver_setUp1():
+    tensor1, tensor2, tensor3 = setUp1_for_GlobalMinMaxObserver()
+    observer = GlobalMinMaxObserver()
+
+    observer(tensor1)
+    assert torch.equal(observer.min_float, torch.tensor([-1.0], dtype=torch.float32))
+    assert torch.equal(observer.max_float, torch.tensor([4.0], dtype=torch.float32))
+    observer(tensor2)
+    assert torch.equal(observer.min_float, torch.tensor([-5.0], dtype=torch.float32))
+    assert torch.equal(observer.max_float, torch.tensor([8.0], dtype=torch.float32))
+    observer(tensor3)
+    assert torch.equal(observer.min_float, torch.tensor([-9.0], dtype=torch.float32))
+    assert torch.equal(observer.max_float, torch.tensor([12.0], dtype=torch.float32))
+
+
+def test_GlobalMinMaxObserver_setUp2():
+    tensor1, tensor2, tensor3 = _setUp2_for_GlobalMinMaxObserver()
+    observer = GlobalMinMaxObserver()
+    observer(tensor1)
+    assert torch.equal(observer.min_float, torch.tensor([-9.0], dtype=torch.float32))
+    assert torch.equal(observer.max_float, torch.tensor([12.0], dtype=torch.float32))
+    observer(tensor2)
+    assert torch.equal(observer.min_float, torch.tensor([-9.0], dtype=torch.float32))
+    assert torch.equal(observer.max_float, torch.tensor([12.0], dtype=torch.float32))
+    observer(tensor3)
+    assert torch.equal(observer.min_float, torch.tensor([-9.0], dtype=torch.float32))
+    assert torch.equal(observer.max_float, torch.tensor([12.0], dtype=torch.float32))
