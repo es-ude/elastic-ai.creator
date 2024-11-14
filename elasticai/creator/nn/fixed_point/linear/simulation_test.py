6b1256f0af5bb7e857d14560d800d6455b95003a
leo <leo.buron@uni-due.de>
2024-11-14 08:14:32 +0100

    docs(nn): removed unnecessary comments

--- elasticai/creator/nn/fixed_point/linear/simulation_test.py
+++ elasticai/creator/nn/fixed_point/linear/simulation_test.py
@@ -1,12 +1,3 @@
-"""
-1. create testbench
-2. save testbench to build folder
-3. hand sequence of vhdl files in correct order to simulation tool
-4. hand input data for testcase to simulation tool
-5. let simulation tool compile files (if necessary)
-6. let simulation tool run the simulation
-7. parse/deserialize simulation output to required data
-"""
 import csv
 import pathlib
 from typing import Any
