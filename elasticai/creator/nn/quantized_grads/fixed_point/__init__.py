fc987572b0c2e42e285fbe4434f14e4f25ec1e4b
Leo Buron <leo.buron@uni-due.de>
2024-11-28 14:22:55 +0100

    refactor(nn-qgrad): added imports for higher module
    Signed-off-by: Leo Buron <leo.buron@uni-due.de>

--- elasticai/creator/nn/quantized_grads/fixed_point/__init__.py
+++ elasticai/creator/nn/quantized_grads/fixed_point/__init__.py
@@ -1,2 +1,3 @@
 from ._quantize_to_fixed_point import quantize
 from ._two_complement_fixed_point_config import FixedPointConfigV2
+from .layer import BatchNorm2d, Conv2d, Dropout, Linear, MaxPool2d, ReLU
