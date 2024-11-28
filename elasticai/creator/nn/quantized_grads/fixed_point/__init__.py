8cece4ea5b56f4f286e8b6ccb414d876e5b5599b
Leo Buron <leo.buron@uni-due.de>
2024-11-28 14:29:45 +0100

    feat(nn-qgrad): added optimizer for fixed point SGD. This optimizer needs refactoring in the near future

--- elasticai/creator/nn/quantized_grads/fixed_point/__init__.py
+++ elasticai/creator/nn/quantized_grads/fixed_point/__init__.py
@@ -1,3 +1,4 @@
 from ._quantize_to_fixed_point import quantize
 from ._two_complement_fixed_point_config import FixedPointConfigV2
 from .layer import BatchNorm2d, Conv2d, Dropout, Linear, MaxPool2d, ReLU
+from .optimizer import QuantizedFxpSGD
---
+++ elasticai/creator/nn/quantized_grads/fixed_point/optimizer/__init__.py
@@ -0,0 +1,1 @@
+from .quantized_SGD import QuantizedFxpSGD
