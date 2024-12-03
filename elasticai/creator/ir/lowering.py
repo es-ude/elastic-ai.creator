e2bd9cd5911fcee5c6e2b4c0f3642db5684b5151
Lukas Einhaus <lukas.einhaus@uni-due.de>
2024-12-03 12:56:45 +0100

    refactor(lowering): avoid keeping two registries

--- elasticai/creator/ir/lowering.py
+++ elasticai/creator/ir/lowering.py
@@ -1,4 +1,5 @@
 from collections.abc import Callable, Iterable
+from functools import wraps
 from typing import Generic, Protocol, TypeVar, overload
 
 from .function_registry import FunctionRegistry as _Registry
@@ -18,8 +19,7 @@ class LoweringPass(Generic[Tin, Tout]):
         def key_lookup_fn(x: Tin) -> str:
             return x.type
 
-        self._fns: _Registry[Tin, Tout] = _Registry(key_lookup_fn)
-        self._iterfns: _Registry[Tin, Iterable[Tout]] = _Registry(key_lookup_fn)
+        self._fns: _Registry[Tin, Iterable[Tout]] = _Registry(key_lookup_fn)
 
     @overload
     def register(self, fn: Callable[[Tin], Tout], /) -> Callable[[Tin], Tout]: ...
@@ -29,20 +29,31 @@ class LoweringPass(Generic[Tin, Tout]):
         self, name: str, /
     ) -> Callable[[Callable[[Tin], Tout]], Callable[[Tin], Tout]]: ...
 
-    def _check_for_redefinition(self, arg):
-        if arg in self._fns or arg in self._iterfns:
-            raise ValueError(f"function for {arg} already defined in lowering pass")
-
     def register(self, arg, /):
         self._check_for_redefinition(arg)
-        return self._fns.register(arg)
+
+        if isinstance(arg, str):
+
+            def _reg(fn):
+                wrapper = self._create_iterfn_wrapper(fn)
+                self._fns.register(arg)(wrapper)
+                return fn
+
+            return _reg
+
+        self._fns.register(self._create_iterfn_wrapper(arg))
+        return arg
+
+    def _create_iterfn_wrapper(self, fn):
+        @wraps(fn)
+        def wrapper(arg):
+            return (fn(arg),)
+
+        return wrapper
 
     def __call__(self, args: Iterable[Tin]) -> Iterable[Tout]:
         for arg in args:
-            if self._fns.can_dispatch(arg):
-                yield self._fns.call(arg)
-            else:
-                yield from self._iterfns.call(arg)
+            yield from self._fns.call(arg)
 
     @overload
     def register_iterable(
@@ -58,4 +69,8 @@ class LoweringPass(Generic[Tin, Tout]):
 
     def register_iterable(self, arg, /):
         self._check_for_redefinition(arg)
-        return self._iterfns.register(arg)
+        return self._fns.register(arg)
+
+    def _check_for_redefinition(self, arg):
+        if arg in self._fns:
+            raise ValueError(f"function for {arg} already defined in lowering pass")
