9cc227eac39ee9f3850491e985a6199797714938
Lukas Einhaus <lukas.einhaus@uni-due.de>
2024-12-03 08:05:51 +0100

    feat(ir): make suc/predecessors sorted/deterministic

--- elasticai/creator/ir/graph_delegate.py
+++ elasticai/creator/ir/graph_delegate.py
@@ -5,6 +5,14 @@ _T = TypeVar("_T", int, str)
 
 
 class GraphDelegate(Generic[_T]):
+    """All iterators in this class are guaranteed to have a fixed
+    order. That means, the order of iteration is only allowed
+    to change when the data structure is altered. If the
+    content of two GraphDelegates is the same and they were built
+    in the same way, then their iteration order is the same as well.
+
+    """
+
     def __init__(self) -> None:
         """We keep successor and predecessor nodes just to allow for easier implementation.
         Currently, this implementation is not optimized for performance.
@@ -38,15 +46,19 @@ class GraphDelegate(Generic[_T]):
         return self
 
     def iter_nodes(self) -> Iterator[_T]:
+        """Iterator over nodes in a fixed but unspecified order."""
         yield from self.predecessors.keys()
 
     def get_edges(self) -> Iterator[tuple[_T, _T]]:
+        """Iterator over edges in a fixed but unspecified order."""
         for _from, _tos in self.successors.items():
             for _to in _tos:
                 yield _from, _to
 
     def get_successors(self, node: _T) -> Iterator[_T]:
+        """Iterator over node successors in a fixed but unspecified order."""
         yield from sorted(self.successors[node])
 
     def get_predecessors(self, node: _T) -> Iterator[_T]:
+        """Iterator over node predecessors in a fixed but unspecified order."""
         yield from sorted(self.predecessors[node])
