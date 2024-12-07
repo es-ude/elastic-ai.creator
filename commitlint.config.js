3baf4138da3a1e99704c5ecaf0e3d662ff9d6629
Lukas Einhaus <lukas.einhaus@uni-due.de>
2024-12-07 17:49:56 +0100

    chore(commitlint): add test/build/style as types

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'type-enum': [2, "always", ['feat', 'fix', 'docs', 'style', 'refactor', 'revert', 'chore', 'wip', 'perf']],
+        'type-enum': [2, "always", ['feat', 'fix', 'docs', 'style', 'refactor', 'revert', 'chore', 'wip', 'perf', 'build', 'test', 'style']],
         'header-max-length': [1, 'always', 100]
     }
 }
