2295ab462fc463919c0abe052be70a9b8e823882
Lukas Einhaus <lukas.einhaus@uni-due.de>
2024-12-19 17:48:36 +0100

    feat(vhdl-plugins): add a new skeleton
    A  version of the skeleton with support for
    
    * dynamically generated skeleton ids
    * the new axi-like layer interfaces
    
    This design also exposes the generic parameters
    that future lowering passes will set, such that
    the correct padding and parameters for sliding
    windows etc. will be used.
    
    
    Signed-off-by: Lukas Einhaus <lukas.einhaus@uni-due.de>

--- elasticai/creator_plugins/skeleton/__init__.py
+++ elasticai/creator_plugins/skeleton/__init__.py
@@ -1,1 +1,39 @@
+import importlib.resources as res
 
+from elasticai.creator.ir2vhdl import Code, Implementation, PluginLoader, PluginSymbol
+from elasticai.creator.vhdl_template import EntityTemplateDirector, Template
+
+from ._skeleton import skeleton as skeleton
+
+
+class _BufferedNetworkWrapper(PluginSymbol):
+    def __init__(self, template: Template):
+        self._template = template
+
+    def __call__(self, args: Implementation) -> Code:
+        def _iter():
+            yield self._template.render(
+                dict(entity=args.name) | args.attributes["generic_map"]
+            )
+
+        return args.name, _iter()
+
+    @classmethod
+    def load_into(cls, receiver: PluginLoader) -> None:
+        def load_file():
+            return (
+                res.files("elasticai.creator_plugins.skeleton")
+                .joinpath("vhdl/buffered_network_wrapper.vhd")
+                .read_text()
+            )
+
+        template = (
+            EntityTemplateDirector()
+            .set_prototype(load_file())
+            .add_generic("KERNEL_SIZE")
+            .add_generic("STRIDE")
+        ).build()
+        receiver.register("buffered_network_wrapper", cls(template))
+
+
+buffered_network_wrapper = _BufferedNetworkWrapper
