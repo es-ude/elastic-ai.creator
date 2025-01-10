b4a54eec17ab43811e832289fb3e838da14ce585
Lukas Einhaus <lukas.einhaus@uni-due.de>
2025-01-10 13:58:10 +0100

    build: update ruff version and uv.lock file
    New ruff version was released, and CI automatically
    switched to that. Since formatting differs between
    versions 0.8.x and 0.9.0 we raise the minimum
    version in the pyproject.toml to 0.9.0, so devs
    are using the same formatting/linting as CI.
    
    
    closes: #457

--- elasticai/creator/nn/sequential/layer_test.py
+++ elasticai/creator/nn/sequential/layer_test.py
@@ -75,9 +75,9 @@ architecture rtl of sequential is
     signal i_bufferedidentity_0_done : std_logic := '0';
     signal i_bufferedidentity_0_enable : std_logic := '0';
     signal i_bufferedidentity_0_x : std_logic_vector(3 downto 0) := (others => '0');
-    signal i_bufferedidentity_0_x_address : std_logic_vector({address_width-1} downto 0) := (others => '0');
+    signal i_bufferedidentity_0_x_address : std_logic_vector({address_width - 1} downto 0) := (others => '0');
     signal i_bufferedidentity_0_y : std_logic_vector(3 downto 0) := (others => '0');
-    signal i_bufferedidentity_0_y_address : std_logic_vector({address_width-1} downto 0) := (others => '0');
+    signal i_bufferedidentity_0_y_address : std_logic_vector({address_width - 1} downto 0) := (others => '0');
 begin
     done <= i_bufferedidentity_0_done;
     i_bufferedidentity_0_clock <= clock;
