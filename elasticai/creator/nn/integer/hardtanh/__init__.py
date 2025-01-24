5b3ef8f54bfcbd8d9c13ccdd58d9eb5fe9cd5aa6
Edwina1030 <edwina1030@hotmail.com>
2025-01-24 15:13:19 +0100

    feat: added new component of hardtanh

---
+++ elasticai/creator/nn/integer/hardtanh/__init__.py
@@ -0,0 +1,1 @@
+from .hardtanh import HardTanh
--- elasticai/creator/nn/integer/hardtanh/hardtanh.py
+++ elasticai/creator/nn/integer/hardtanh/hardtanh.py
@@ -4,15 +4,13 @@ import torch
 from torch import nn
 
 from elasticai.creator.nn.integer.design_creator_module import DesignCreatorModule
-from elasticai.creator.nn.integer.hardsigmoid.design import (
-    HardSigmoid as HardSigmoidDesign,
-)
+from elasticai.creator.nn.integer.hardtanh.design import HardTanh as HardTanhDesign
 from elasticai.creator.nn.integer.quant_utils.Observers import GlobalMinMaxObserver
 from elasticai.creator.nn.integer.quant_utils.QParams import AsymmetricSignedQParams
 from elasticai.creator.nn.integer.quant_utils.SimQuant import SimQuant
 
 
-class HardSigmoid(DesignCreatorModule, nn.Module):
+class HardTanh(DesignCreatorModule, nn.Module):
     def __init__(self, **kwargs):
         super().__init__()
 
@@ -31,33 +29,16 @@ class HardSigmoid(DesignCreatorModule, nn.Module):
             observer=GlobalMinMaxObserver(),
         ).to(self.device)
 
+        self.hardtanh = nn.Hardtanh(min_val=-1.0, max_val=1.0, inplace=False)
+
         self.precomputed = False
 
-    def create_design(self, name: str) -> HardSigmoidDesign:
+    def create_design(self, name: str) -> HardTanhDesign:
         pass
 
-    def _customized_hard_sigmoid(self, inputs: torch.FloatTensor) -> torch.FloatTensor:
-        outputs = torch.where(inputs <= -3.0, torch.zeros_like(inputs), inputs)
-        outputs = torch.where(inputs >= 3.0, torch.ones_like(outputs), outputs)
-
-        outputs = torch.where(
-            (inputs > -3.0) & (inputs < 3.0), outputs / 8.0 + 0.5, outputs
-        )
-        return outputs
-
     def precompute(self):
-        self.quantized_three = self.inputs_QParams.quantize(torch.tensor(3.0))
-        self.quantized_minus_three = self.inputs_QParams.quantize(torch.tensor(-3.0))
-
         self.quantized_one = self.inputs_QParams.quantize(torch.tensor(1.0))
-        self.quantized_zero = self.inputs_QParams.quantize(torch.tensor(0.0))
-
-        self.out_quantized_one = self.outputs_QParams.quantize(torch.tensor(1.0))
-        self.out_quantized_zero = self.outputs_QParams.quantize(torch.tensor(0.0))
-
-        self.tmp = int(
-            4 / (self.inputs_QParams.scale_factor) + 7 * self.inputs_QParams.zero_point
-        )
+        self.quantized_minus_one = self.inputs_QParams.quantize(torch.tensor(-1.0))
         self.precomputed = True
 
     def int_forward(self, q_inputs: torch.IntTensor) -> torch.IntTensor:
@@ -65,23 +46,11 @@ class HardSigmoid(DesignCreatorModule, nn.Module):
         assert self.precomputed, "precompute should be called before int_forward"
 
         q_outputs = torch.where(
-            q_inputs <= self.quantized_minus_three,
-            torch.ones_like(q_inputs) * self.quantized_zero,
-            q_inputs,
+            q_inputs > self.quantized_one, self.quantized_one, q_inputs
         )
-
         q_outputs = torch.where(
-            q_inputs >= self.quantized_three,
-            torch.ones_like(q_outputs) * self.quantized_one,
-            q_outputs,
+            q_outputs < self.quantized_minus_one, self.quantized_minus_one, q_outputs
         )
-
-        q_outputs = torch.where(
-            (q_inputs > self.quantized_minus_three) & (q_inputs < self.quantized_three),
-            (q_outputs + self.tmp) // 8,
-            q_outputs,
-        )
-
         return q_outputs
 
     def forward(
@@ -95,8 +64,7 @@ class HardSigmoid(DesignCreatorModule, nn.Module):
             else:
                 self.inputs_QParams = given_inputs_QParams
 
-        inputs = SimQuant.apply(inputs, self.inputs_QParams)
-        outputs = self._customized_hard_sigmoid(inputs)
+        outputs = self.hardtanh(inputs)
 
         if self.training:
             self.outputs_QParams.update_quant_params(outputs)
