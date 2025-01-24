c5db4be962c867460d733f56f27e64da405d7ce6
Leo Buron <leo.buron@uni-due.de>
2025-01-24 21:31:44 +0100

    feat(qgrads): added parametrization and module quantization
    In this feature the current state of the quantized grads got heavily changed.
    This is due to several reasons.
    One beeing that the data of the fixed point config was needed in function calls.
    Thus, the change of the position of the tensors was not
    possible after generating the function.
    Also, the support for other Optimizers was not possible and
    thus, the use of built in learning rate scheduler from pytorch.
    
    This commit features
    1. param_quantizations that register a parametrization for each parameter of the module.
    Also, a version for STE
    2. module_quantizations those are used to quantize the
    output of a layer with a custom autograd function.
    They are available for fixedpoint with forw only and forw+back quantizations.
    3. A quantized optimizer factory for updating parametrized
    parameters according to their quantization scheme.
    In addition, the buffers of the optimizer can be quantized too.
    
    Signed-off-by: Leo Buron <leo.buron@uni-due.de>

--- tests/unit_tests/nn/quantized_grads/fixed_point/base_modules/test_batchnorm.py
+++ tests/unit_tests/nn/quantized_grads/fixed_point/base_modules/test_batchnorm.py
@@ -1,39 +1,16 @@
-import torch
-
-from elasticai.creator.nn.quantized_grads import MathOperationsForw
 from elasticai.creator.nn.quantized_grads.base_modules import BatchNorm2d
 from elasticai.creator.nn.quantized_grads.fixed_point import (
-    FixedPointConfigV2,
-    quantize_to_fxp_stochastic,
-    quantize_to_fxp_stochastic_,
+    FixedPointConfigV2, QuantizeParamToFixedPointStochastic, QuantizeParamToFixedPointHTE,
+QuantizeForwHTE
 )
 
 
 def test_batchnorm_fxp_init():
     conf = FixedPointConfigV2(8, 3)
 
-    def quantize_weight(tensor: torch.Tensor) -> None:
-        quantize_to_fxp_stochastic_(tensor, conf)
-
-    def quantize_bias(tensor: torch.Tensor) -> None:
-        quantize_to_fxp_stochastic_(tensor, conf)
-
-    def quantize_forward(tensor: torch.Tensor) -> torch.Tensor:
-        return quantize_to_fxp_stochastic(tensor, conf)
-
-    ops = MathOperationsForw(quantize_forward)
     l = BatchNorm2d(
-        operations=ops,
-        weight_quantization=quantize_weight,
-        bias_quantization=quantize_bias,
+        math_ops=QuantizeForwHTE(forward_conf=conf),
+        weight_quantization=QuantizeParamToFixedPointHTE(conf),
+        bias_quantization=QuantizeParamToFixedPointStochastic(conf),
         num_features=3,
     )
-
-    for qparam in l.qparams:
-        assert qparam.name in ["weight", "bias"]
-    if qparam.name == "weight":
-        assert qparam.quantization == quantize_weight
-    elif qparam.name == "bias":
-        assert qparam.quantization == quantize_bias
-    else:
-        assert False
