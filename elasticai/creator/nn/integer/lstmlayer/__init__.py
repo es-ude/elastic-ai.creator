2c10acb51a67fea0b4e9cae26770ba3898112665
Edwina1030 <edwina1030@hotmail.com>
2025-01-24 16:09:01 +0100

    feat: added new component of lstm layer

---
+++ elasticai/creator/nn/integer/lstmlayer/__init__.py
@@ -0,0 +1,1 @@
+from .lstmlayer import LSTMLayer
---
+++ elasticai/creator/nn/integer/lstmlayer/lstmlayer.py
@@ -0,0 +1,129 @@
+import logging
+from pathlib import Path
+
+import torch
+import torch.nn as nn
+
+from elasticai.creator.nn.integer.quant_utils.lstmcell import LSTMCell
+from elasticai.creator.nn.integer.quant_utils.lstmlayer.design import (
+    LSTMLayer as LSTMLayerDesign,
+)
+from elasticai.creator.nn.integer.quant_utils.Observers import GlobalMinMaxObserver
+from elasticai.creator.nn.integer.quant_utils.QParams import AsymmetricSignedQParams
+
+
+class LSTMLayer(nn.Module):
+    def __init__(self, **kwargs) -> None:
+        super().__init__()
+
+        inputs_size = kwargs.get("inputs_size")
+        hidden_size = kwargs.get("hidden_size")
+        self.seq_len = kwargs.get("seq_len")
+
+        self.name = kwargs.get("name")
+        quant_bits = kwargs.get("quant_bits")
+        self.quant_data_file_dir = Path(kwargs.get("quant_data_file_dir"))
+        device = kwargs.get("device")
+        self.logger = logging.getLogger(self.__class__.__name__)
+
+        self.lstm_cell = LSTMCell(
+            name=f"{self.name}_lstm_cell",
+            inputs_size=inputs_size,
+            hidden_size=hidden_size,
+            quant_bits=quant_bits,
+            quant_data_file_dir=self.quant_data_file_dir,
+            device=device,
+        )
+
+        self.inputs_QParams = AsymmetricSignedQParams(
+            quant_bits=quant_bits, observer=GlobalMinMaxObserver()
+        ).to(device)
+        self.h_prev_QParams = AsymmetricSignedQParams(
+            quant_bits=quant_bits, observer=GlobalMinMaxObserver()
+        ).to(device)
+        self.c_prev_QParams = AsymmetricSignedQParams(
+            quant_bits=quant_bits, observer=GlobalMinMaxObserver()
+        ).to(device)
+        self.h_next_QParams = AsymmetricSignedQParams(
+            quant_bits=quant_bits, observer=GlobalMinMaxObserver()
+        ).to(device)
+        self.c_next_QParams = AsymmetricSignedQParams(
+            quant_bits=quant_bits, observer=GlobalMinMaxObserver()
+        ).to(device)
+
+        self.precomputed = False
+
+    def precompute(self):
+        self.lstm_cell.precompute()
+        self.precomputed = True
+
+    def _save_quant_data(self, tensor, file_dir: Path, file_name: str):
+        file_path = file_dir / f"{file_name}.txt"
+        tensor_str = "\n".join(map(str, tensor.flatten().tolist()))
+        file_path.write_text(tensor_str)
+
+    def int_forward(
+        self,
+        q_inputs: torch.FloatTensor,
+        q_h_prev: torch.FloatTensor,
+        q_c_prev: torch.FloatTensor,
+    ) -> torch.FloatTensor:
+        assert not self.training, "int_forward should be called in eval mode"
+        assert self.precomputed, "precompute should be called before int_forward"
+
+        self._save_quant_data(q_inputs, self.quant_data_file_dir, f"{self.name}_q_x")
+        self._save_quant_data(
+            q_h_prev, self.quant_data_file_dir, f"{self.name}_q_h_prev"
+        )
+        self._save_quant_data(
+            q_c_prev, self.quant_data_file_dir, f"{self.name}_q_c_prev"
+        )
+
+        q_hidden_states = []
+        for t in range(self.seq_len):
+            q_h_next, q_c_next = self.lstm_cell.int_forward(
+                q_inputs=q_inputs[:, t, :], q_h_prev=q_h_prev, q_c_prev=q_c_prev
+            )
+            q_hidden_states.append(q_h_next.unsqueeze(1))
+
+        q_outputs = torch.stack(q_hidden_states, dim=1)
+
+        self._save_quant_data(q_outputs, self.quant_data_file_dir, f"{self.name}_q_y")
+        self._save_quant_data(
+            q_c_next, self.quant_data_file_dir, f"{self.name}_q_c_next"
+        )
+        self._save_quant_data(
+            q_h_next, self.quant_data_file_dir, f"{self.name}_q_h_next"
+        )
+        return q_outputs, q_h_next, q_c_next
+
+    def forward(
+        self,
+        inputs: torch.FloatTensor,
+        h_prev: torch.FloatTensor,
+        c_prev: torch.FloatTensor,
+        given_inputs_QParams: torch.nn.Module = None,
+        given_h_prev_QParams: torch.nn.Module = None,
+        given_c_prev_QParams: torch.nn.Module = None,
+    ) -> torch.FloatTensor:
+        hidden_states = []
+
+        for t in range(self.seq_len):
+            h_next, c_next = self.lstm_cell.forward(
+                inputs=inputs[:, t, :],
+                h_prev=h_prev,
+                c_prev=c_prev,
+                given_c_prev_QParams=given_c_prev_QParams,
+                given_h_prev_QParams=given_h_prev_QParams,
+                given_inputs_QParams=given_inputs_QParams,
+            )
+            hidden_states.append(h_next.unsqueeze(1))
+            h_prev, c_prev = h_next, c_next
+
+            given_c_prev_QParams = self.lstm_cell.c_next_QParams
+            given_h_prev_QParams = self.lstm_cell.h_next_QParams
+            given_inputs_QParams = self.lstm_cell.output_QParams
+
+        outputs = torch.stack(hidden_states, dim=1)
+
+        return outputs, h_next, c_next
