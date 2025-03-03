07e7ee6245e4a275a7e915e4d6e66eb7214b6355
Lukas Einhaus <lukas.einhaus@uni-due.de>
2025-03-03 13:31:00 +0100

    feat(graph): find all non-overlapping subgraphs
    In most cases we want to find/replace all
    non-overlapping subgraphs specified by a
    given pattern. This extends the vf2 based
    algorithm to find all occurences of the
    pattern instead of just the first one.
    
    Signed-off-by: Lukas Einhaus <lukas.einhaus@uni-due.de>

--- elasticai/creator/graph/subgraph_matching.py
+++ elasticai/creator/graph/subgraph_matching.py
@@ -2,19 +2,24 @@ from typing import TypeVar
 
 from elasticai.creator.graph._types import NodeConstraintFn
 
-from .vf2 import match as _match
+from .vf2 import find_all_matches, match
 
 TP = TypeVar("TP")
 T = TypeVar("T")
 
 
+class SubGraphMatchError(Exception):
+    def __init__(self):
+        super().__init__("No matching subgraph found")
+
+
 def find_subgraph(
     pattern, graph, node_constraint: NodeConstraintFn[TP, T]
 ) -> dict[TP, T]:
-    return _match(pattern, graph, node_constraint)
+    return match(pattern, graph, node_constraint)
 
 
-def find_subgraphs(
+def find_all_subgraphs(
     pattern, graph, node_constraint: NodeConstraintFn[TP, T]
 ) -> list[dict[TP, T]]:
-    return [_match(pattern, graph, node_constraint)]
+    return find_all_matches(pattern, graph, node_constraint)
