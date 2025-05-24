ea51ee7ce33a17263d3c1b393b2bb2d1f75cebd7
Edwina1030 <edwina1030@hotmail.com>
2025-05-24 22:14:22 +0200

    fix: assign a default value to m_q

--- elasticai/creator/nn/integer/quant_utils/scaling_M.py
+++ elasticai/creator/nn/integer/quant_utils/scaling_M.py
@@ -6,10 +6,10 @@ def scaling_M(M: torch.FloatTensor, m_q_shift_limit=32):
     M = torch.round(M * 10**5) / 10**5
     m_q_shift = torch.tensor(1, dtype=torch.int32)
 
-    if torch.any(M == 0):
-        # "if M is approaching 0 or equal to 0, we will give up approximating it"
-        m_q = torch.tensor(0, dtype=torch.int32)
-    else:
+    # "if M is approaching 0 or equal to 0, we will give up approximating it"
+    m_q = torch.tensor(0, dtype=torch.int32)
+
+    if torch.any(M != 0):
         while m_q_shift.item() < m_q_shift_limit:
             m_q = torch.round(M * (2**m_q_shift)).type(torch.int32)
             error = (M - m_q * (2 ** (-m_q_shift.item()))) / M
