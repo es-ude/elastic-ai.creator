a74e40eff57d7ec1b52d9981866b54339dbc155e
Lukas Einhaus <lukas.einhaus@uni-due.de>
2025-06-06 17:07:30 +0200

    fix(graph): fix bug where rewriting would skip available names
    The rewriting algorithm will automatically genereate new names for
    inserted nodes, to ensure node names are kept unique. In some cases
    the algorithm would skip suffix indices, e.g., conv1d_1, conv1d_3
    instead of conv1d_1, conv1d_2.
    This should now be fixed.

--- elasticai/creator/graph/name_generation.py
+++ elasticai/creator/graph/name_generation.py
@@ -5,15 +5,18 @@ class NameRegistry:
     def __init__(self):
         self._registry = {}
 
+    def _get_name_count(self, name):
+        return self._registry.get(name, 0)
+
     def prepopulate(self, names):
         for name in names:
             match = re.match(r"(.+)_(\d+)$", name)
+            suffix = 0
             if match:
                 name = match.group(1)
                 suffix = int(match.group(2))
-            else:
-                suffix = 0
-            self._registry[name] = self._registry.get(name, suffix) + 1
+            suffix = max(suffix, self._get_name_count(name))
+            self._registry[name] = suffix
         return self
 
     def get_unique_name(self, name):
@@ -21,6 +24,6 @@ class NameRegistry:
             self._registry[name] = 0
             return name
 
-        new_name = f"{name}_{self._registry[name]}"
+        new_name = f"{name}_{self._registry[name] + 1}"
         self._registry[name] += 1
         return new_name
