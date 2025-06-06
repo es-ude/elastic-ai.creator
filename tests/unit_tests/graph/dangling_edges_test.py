4f3fea287bb2104f39a8244609cc075fd185dc9f
Lukas Einhaus <lukas.einhaus@uni-due.de>
2025-06-06 17:07:30 +0200

    fix(graph): fix bug where rewriting would skip available names
    The rewriting algorithm will automatically genereate new names for
    inserted nodes, to ensure node names are kept unique. In some cases
    the algorithm would skip suffix indices, e.g., conv1d_1, conv1d_3
    instead of conv1d_1, conv1d_2.
    This should now be fixed.

--- tests/unit_tests/graph/dangling_edges_test.py
+++ tests/unit_tests/graph/dangling_edges_test.py
@@ -99,3 +99,41 @@ def test_do_not_remove_second_match_if_overlap_only_occurs_in_interface():
             lhs.values(),
         )
     )
+
+
+def test_pattern_seq_with_2_interface_nodes():
+    g = build_graph(
+        {
+            "a": ["b"],
+            "b": ["c"],
+            "c": ["d"],
+            "d": ["e"],
+            "e": ["f"],
+            "f": ["g"],
+            "g": ["h"],
+        }
+    )
+    p = build_graph(
+        {
+            "0": ["1"],
+            "1": ["2"],
+            "2": ["3"],
+            "3": ["4"],
+        }
+    )
+    matches = gr.find_all_subgraphs(
+        pattern=p, graph=g, node_constraint=lambda _, __: True
+    )
+    lhs = {"i0": "0", "i1": "1", "i2": "3", "i3": "4"}
+    expected_leftover_matches = []
+    for sequence in [("a", "b", "c", "d", "e"), ("d", "e", "f", "g", "h")]:
+        expected_leftover_matches.append(
+            {f"{i}": node for i, node in enumerate(sequence)}
+        )
+    assert expected_leftover_matches == list(
+        gr.get_rewriteable_matches(
+            g,
+            matches,
+            lhs.values(),
+        )
+    )
