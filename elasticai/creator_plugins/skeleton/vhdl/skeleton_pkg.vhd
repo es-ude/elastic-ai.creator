42948ade11b1c0005ea1dfecd74e384d992c87c5
Lukas Einhaus <lukas.einhaus@uni-due.de>
2025-07-19 12:31:50 +0200

    fix(skeleton): reintroduce buffered network wrapper
    The previous implementation did not take into account
    that the skeleton cannot see the spi clock. Hence,
    there is no way of telling two separate reads or writes
    apart.
    This new implementation respects the random access
    addressing.
    To keep from having to compute divisions and modulo
    operations for every address, we provide a byte-wise
    ram and remove padding on read (for the input buffer)
    and add padding on write (for the output buffer)

--- elasticai/creator_plugins/skeleton/vhdl/skeleton_pkg.vhd
+++ elasticai/creator_plugins/skeleton/vhdl/skeleton_pkg.vhd
@@ -1,9 +1,22 @@
 library ieee;
 use ieee.numeric_std.all;
 use ieee.std_logic_1164.all;
+use ieee.math_real.all;
 
 
 package skeleton_pkg is
   type skeleton_id_t is array (0 to 15) of std_logic_vector(7 downto 0);
   constant SKELETON_ID : skeleton_id_t := (others => x"00");
+  function size_in_bytes(size: integer) return integer;
+
+
 end package;
+
+package body skeleton_pkg is
+  function size_in_bytes(size: integer) return integer is
+  begin
+    return integer(ceil(real(size) / 8.0));
+  end function;
+
+
+end package body;
