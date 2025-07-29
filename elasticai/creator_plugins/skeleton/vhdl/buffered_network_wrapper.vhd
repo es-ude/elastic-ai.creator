26bb63413bb33644ba05fdc891dfb5e4475ccdc2
Lukas Einhaus <lukas.einhaus@uni-due.de>
2025-07-29 19:21:59 +0200

    wip: fix input buffer addressing

--- elasticai/creator_plugins/skeleton/vhdl/buffered_network_wrapper.vhd
+++ elasticai/creator_plugins/skeleton/vhdl/buffered_network_wrapper.vhd
@@ -5,12 +5,12 @@ use work.skeleton_pkg.all;
 
 entity buffered_network_wrapper is
   generic (
-    DATA_IN_WIDTH : positive;
-    DATA_IN_DEPTH : positive;
-    DATA_OUT_WIDTH : positive;
-    DATA_OUT_DEPTH : positive;
-    STRIDE : positive;
-    KERNEL_SIZE: positive
+    DATA_IN_WIDTH : positive := 1;
+    DATA_IN_DEPTH : positive := 1;
+    DATA_OUT_WIDTH : positive := 1;
+    DATA_OUT_DEPTH : positive := 1;
+    STRIDE : positive := 1;
+    KERNEL_SIZE: positive := 1
   );
 
   port (
@@ -28,12 +28,11 @@ end entity;
 
 architecture rtl of buffered_network_wrapper is
   signal run_computation : std_logic := '0';
-  signal has_been_enabled : std_logic := '0';
-  signal internal_done : std_logic := '0';
   signal in_buffer_valid_out : std_logic;
   signal in_buffer_d_out : std_logic_vector(DATA_IN_WIDTH*KERNEL_SIZE - 1 downto 0);
   signal network_d_out : std_logic_vector(DATA_OUT_WIDTH - 1 downto 0);
   signal network_valid_out : std_logic;
+  signal out_buffer_valid_out : std_logic;
   signal address_i : integer range 0 to 2**16-1;
   signal network_rd_address : std_logic_vector(address'range);
 
@@ -41,25 +40,36 @@ architecture rtl of buffered_network_wrapper is
 
   constant OUT_DEPTH_BYTES : integer := size_in_bytes(DATA_OUT_WIDTH)*DATA_OUT_DEPTH;
   constant IN_DEPTH_BYTES : integer := size_in_bytes(DATA_IN_WIDTH)*DATA_IN_DEPTH;
-
   signal network_wr_address_i : integer range 0 to IN_DEPTH_BYTES - 1;
   signal network_rd_address_i : integer range 0 to OUT_DEPTH_BYTES - 1;
+  type network_state_t is (idle, start, running, finished);
+  signal network_state : network_state_t := idle;
+  signal internal_rst : std_logic := '1';
   begin
 
-  update_run_computation:
-  process (clk, rst) is
+  update_network_state:
+  process(rst, clk) is
   begin
     if rst = '1' then
-      has_been_enabled <= '0';
+      network_state <= idle;
     elsif rising_edge(clk) then
-      if enable = '1' then
-        has_been_enabled <= '1';
+      if network_state = idle and enable = '1' then
+        network_state <= start;
+      elsif network_state = start then
+        network_state <= running;
+      elsif network_state = running and out_buffer_valid_out = '1' then
+        network_state <= finished;
+      elsif network_state = finished and enable = '0' then
+        network_state <= idle;
       end if;
     end if;
-  end process; 
+  end process;
 
-  run_computation <= (has_been_enabled or enable) and not internal_done;
-  done <= internal_done;
+    
+
+  internal_rst <= '1' when network_state = start or rst = '1' else '0';
+  run_computation <= '1' when network_state = running else '0';
+  done <= '1' when network_state = finished else '0';
   address_i <= to_integer(unsigned(address));
 
   
@@ -82,7 +92,7 @@ architecture rtl of buffered_network_wrapper is
     address => address,
     d_in => d_in,
     clk => clk,
-    rst => rst,
+    rst => internal_rst,
     ready_in => run_computation,
     valid_out => in_buffer_valid_out,
     d_out => in_buffer_d_out
@@ -93,7 +103,7 @@ architecture rtl of buffered_network_wrapper is
     d_in => in_buffer_d_out,
     valid_in => run_computation,
     clk => clk,
-    rst => rst,
+    rst => internal_rst,
     d_out => network_d_out,
     valid_out => network_valid_out
   );
@@ -106,11 +116,11 @@ architecture rtl of buffered_network_wrapper is
   )
   port map (
     clk => clk,
-    rst => rst,
+    rst => internal_rst,
     valid_in => network_valid_out,
     address => address,
     d_in => network_d_out,
-    valid_out => internal_done,
+    valid_out => out_buffer_valid_out,
     d_out => d_out
   );
   
