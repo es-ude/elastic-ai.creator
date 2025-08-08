bc55ca870d91cb1a5aec150a2c2ceb2ce619aa34
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2025-08-08 13:23:28 +0200

    feat: adapt cocotb runner tests and refactor act func tests

--- tests/unit_tests/nn/fixed_point/hard_tanh/layer_func_test.py
+++ tests/unit_tests/nn/fixed_point/hard_tanh/layer_func_test.py
@@ -1,12 +1,16 @@
 import numpy as np
+import pytest
 from torch import Tensor
 from torch import nn as nn_torch
 
 from elasticai.creator.nn import fixed_point as nn_creator
 
 
-def hardtanh_compared_torch(
-    total_bits: int, frac_bits: int, min_val=-1.0, max_val=+1.0
+@pytest.mark.parametrize(
+    "total_bits, frac_bits", [(4, 3), (6, 4), (8, 5), (10, 6), (12, 7), (12, 8)]
+)
+def test_hard_tanh_compared_torch(
+    total_bits: int, frac_bits: int, val_range=(-1.0, +1.0)
 ) -> None:
     range = [
         -(2 ** (total_bits - frac_bits - 1)),
@@ -16,25 +20,10 @@ def hardtanh_compared_torch(
         start=range[0], stop=range[1], num=2 ** (total_bits + 1), endpoint=True
     )
 
-    act0 = nn_torch.Hardtanh(min_val=min_val, max_val=max_val)
+    act0 = nn_torch.Hardtanh(min_val=val_range[0], max_val=val_range[1])
     out0 = act0(Tensor(stimulus))
-    act1 = nn_creator.HardTanh(total_bits, frac_bits, min_val=min_val, max_val=max_val)
+    act1 = nn_creator.HardTanh(
+        total_bits, frac_bits, min_val=val_range[0], max_val=val_range[1]
+    )
     out1 = act1(Tensor(stimulus))
-
-    assert abs(float(sum(out1 - out0))) < 1e-6
-
-
-def test_hardtanh_compared_torch_6bit_4bit_pm1() -> None:
-    hardtanh_compared_torch(total_bits=6, frac_bits=4)
-
-
-def test_hardtanh_compared_torch_8bit_2bit_pm1() -> None:
-    hardtanh_compared_torch(total_bits=8, frac_bits=2)
-
-
-def test_hardtanh_compared_torch_7bit_4bit_pm1() -> None:
-    hardtanh_compared_torch(total_bits=7, frac_bits=4)
-
-
-def test_hardtanh_compared_torch_7bit_4bit_pm2() -> None:
-    hardtanh_compared_torch(total_bits=7, frac_bits=4, min_val=-2.0, max_val=+2.0)
+    assert float(sum(abs(out1 - out0))) < 1e-6
--- tests/unit_tests/testing/cocotb_runner_tb.py
+++ tests/unit_tests/testing/cocotb_runner_tb.py
@@ -1,52 +1,5 @@
-from pathlib import Path
-
 import cocotb
 from cocotb.triggers import Timer
-
-from elasticai.creator.file_generation.resource_utils import get_full_path
-from elasticai.creator.testing.cocotb_runner import run_cocotb_sim_for_src_dir
-
-_path2src = Path(
-    get_full_path("tests.unit_tests.testing", "cocotb_runner_tb.py")
-).parent
-_path2tb = "tests.unit_tests.testing.cocotb_runner_tb"
-
-cocotb_settings_verilog0 = dict(
-    src_files=["cocotb_runner_tb.v"],
-    top_module_name="COCOTB_TEST",
-    cocotb_test_module=_path2tb,
-    path2src=_path2src,
-    defines={},
-    params={"BITWIDTH": 4, "SCALE": 2},
-    en_debug_mode=True,
-)
-cocotb_settings_verilog1 = dict(
-    src_files=["cocotb_runner_tb.v"],
-    top_module_name="COCOTB_TEST",
-    cocotb_test_module=_path2tb,
-    path2src=_path2src,
-    params={"BITWIDTH": 4, "SCALE": 2, "OFFSET": 2},
-    defines={"DEFINE_TEST": True},
-    en_debug_mode=True,
-)
-cocotb_settings_vhdl0 = dict(
-    src_files=["cocotb_runner_tb.vhd"],
-    top_module_name="COCOTB_TEST",
-    cocotb_test_module=_path2tb,
-    path2src=_path2src,
-    params={"BITWIDTH": 4, "SCALE": 2},
-    defines={},
-    en_debug_mode=True,
-)
-cocotb_settings_vhdl1 = dict(
-    src_files=["cocotb_runner_tb.vhd"],
-    top_module_name="COCOTB_TEST",
-    cocotb_test_module=_path2tb,
-    path2src=_path2src,
-    params={"BITWIDTH": 4, "SCALE": 2, "OFFSET": 2},
-    defines={"DEFINE_TEST": True},
-    en_debug_mode=True,
-)
 
 
 def model(a: int, scale: int, offset: int, do_offset: bool) -> int:
@@ -61,10 +14,3 @@ async def model_test(dut):
     dut.A.value = A
     await Timer(2, units="step")
     assert dut.Q.value == model(A, dut.SCALE.value, B, "OFFSET" in dir(dut))
-
-
-if __name__ == "__main__":
-    run_cocotb_sim_for_src_dir(**cocotb_settings_verilog0)
-    run_cocotb_sim_for_src_dir(**cocotb_settings_verilog1)
-    run_cocotb_sim_for_src_dir(**cocotb_settings_vhdl0)
-    run_cocotb_sim_for_src_dir(**cocotb_settings_vhdl1)
