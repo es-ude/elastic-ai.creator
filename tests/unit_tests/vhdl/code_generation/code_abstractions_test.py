e36a59933dada2e9e326bdbd96d4352f4e23256d
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2025-08-19 21:43:16 +0200

    fix(code_abstractions): error in binary string generation
    The func to_vhdl_binary_string was able to calculate the error if number is out of range but it was only suitable for unsigned integer. In our case, we only save signed integers and the out-of-rnage detection was always false.

--- tests/unit_tests/vhdl/code_generation/code_abstractions_test.py
+++ tests/unit_tests/vhdl/code_generation/code_abstractions_test.py
@@ -5,7 +5,17 @@ from elasticai.creator.vhdl.code_generation.code_abstractions import (
 )
 
 
-def test_to_vhdl_binary_string_raises_error_if_value_not_representable() -> None:
+@pytest.mark.parametrize(
+    ("number", "number_of_bits"),
+    [
+        (2, 1),
+        (-5, 3),
+        (4, 3),
+    ],
+)
+def test_to_vhdl_binary_string_raises_error_if_value_not_representable(
+    number: int, number_of_bits: int
+) -> None:
     with pytest.raises(ValueError):
         _ = to_vhdl_binary_string(-8, 3)
 
@@ -13,12 +23,14 @@ def test_to_vhdl_binary_string_raises_error_if_value_not_representable() -> None
 @pytest.mark.parametrize(
     ("number", "number_of_bits", "expected"),
     [
-        (0, 1, '"0"'),
-        (1, 1, '"1"'),
+        (1, 2, '"01"'),
+        (0, 2, '"00"'),
         (6, 4, '"0110"'),
         (-6, 4, '"1010"'),
         (7, 5, '"00111"'),
         (-7, 5, '"11001"'),
+        (-4, 3, '"100"'),
+        (3, 3, '"011"'),
     ],
 )
 def test_to_vhdl_binary_string(number: int, number_of_bits: int, expected: str) -> None:
