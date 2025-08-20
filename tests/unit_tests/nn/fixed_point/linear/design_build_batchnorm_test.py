0d85c0a26b2710b14de495d1dd3698845a412a40
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2025-08-20 08:44:30 +0200

    fix(lineat): building design of batchnorm linear layer is now possible
    When we try to build the hardware design of the batchnorm linear layer, an error occupied due to unmatched tensor sizes in lin_weight and std. This is fixed. Also unit tests for testing if layer is trainable and could be builded. Also integrations tests are added using cocotb for checking if software layer and hardware layer are similar.

---
+++ tests/unit_tests/nn/fixed_point/linear/design_build_batchnorm_test.py
@@ -0,0 +1,36 @@
+from typing import cast
+
+import pytest
+
+from elasticai.creator.file_generation.in_memory_path import InMemoryFile, InMemoryPath
+from elasticai.creator.nn.fixed_point import BatchNormedLinear
+
+
+@pytest.fixture
+def batchnorm_linear_design() -> BatchNormedLinear:
+    return BatchNormedLinear(
+        total_bits=16,
+        frac_bits=8,
+        in_features=3,
+        out_features=2,
+    )
+
+
+def save_design(design: BatchNormedLinear) -> dict[str, str]:
+    destination = InMemoryPath("batchnorm_linear", parent=None)
+    design.create_design("batchnorm_linear").save_to(destination)
+    files = cast(list[InMemoryFile], list(destination.children.values()))
+    return {file.name: "\n".join(file.text) for file in files}
+
+
+def test_saved_design_contains_needed_files(
+    batchnorm_linear_design: BatchNormedLinear,
+) -> None:
+    saved_files = save_design(batchnorm_linear_design)
+    expected_files = {
+        "batchnorm_linear_w_rom.vhd",
+        "batchnorm_linear_b_rom.vhd",
+        "batchnorm_linear.vhd",
+    }
+    actual_files = set(saved_files.keys())
+    assert expected_files == actual_files
