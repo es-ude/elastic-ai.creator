01b2ebdebdb296c69531bc74c5940dbc09720653
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2025-08-22 15:18:42 +0200

    chore: address review comments from PR #693

--- tests/integration_tests/nn/fixed_point/silu_adaptible/layer_design_test.py
+++ tests/integration_tests/nn/fixed_point/silu_adaptible/layer_design_test.py
@@ -12,7 +12,7 @@ from tests.integration_tests.nn.fixed_point.precomputed_routine import (
 @pytest.mark.parametrize(
     "total_bits, frac_bits, num_steps", [(6, 4, 32), (8, 4, 32), (10, 9, 64)]
 )
-def test_build_test_silu_design(
+def test_build_test_silu_adapt_design(
     total_bits: int, frac_bits: int, num_steps: int
 ) -> None:
     file_name = f"TestAdaptSiLU_{total_bits}_{frac_bits}_{num_steps}"
