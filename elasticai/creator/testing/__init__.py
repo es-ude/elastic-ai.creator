e8f0d93d6d9c3c8b67c0cca4805036d84c25a040
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2025-09-16 17:01:02 +0200

    feat(cocotb): Add func to init file

--- elasticai/creator/testing/__init__.py
+++ elasticai/creator/testing/__init__.py
@@ -1,11 +1,17 @@
 from .cocotb_prepare import build_report_folder_and_testdata, read_testdata
-from .cocotb_runner import run_cocotb_sim
+from .cocotb_runner import (
+    check_cocotb_test_result,
+    run_cocotb_sim,
+    run_cocotb_sim_for_src_dir,
+)
 from .ghdl_report_parsing import parse_report
 from .ghdl_simulation import GHDLSimulator
 from .simulated_layer import SimulatedLayer, Testbench
 
 __all__ = [
     "run_cocotb_sim",
+    "run_cocotb_sim_for_src_dir",
+    "check_cocotb_test_result",
     "build_report_folder_and_testdata",
     "read_testdata",
     "GHDLSimulator",
