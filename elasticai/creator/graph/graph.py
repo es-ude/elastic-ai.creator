2c7a673f98495fb3735cd7afa470f0910865acb3
Lukas Einhaus <lukas.einhaus@uni-due.de>
2025-12-05 14:53:10 +0100

    chore(typing): make graph.Graph abstract and segregate ReadOnlyGraph

--- elasticai/creator/graph/graph.py
+++ elasticai/creator/graph/graph.py
@@ -1,23 +1,15 @@
 from abc import abstractmethod
 from collections.abc import Hashable, Iterator, Mapping, Set
-from typing import Protocol, Self, TypeVar
-
-T = TypeVar("T", bound=Hashable)
+from typing import Protocol, Self
 
 
-class Graph(Protocol[T]):
+class ReadOnlyGraph[T: Hashable](Protocol):
     @property
     @abstractmethod
     def nodes(self) -> Set[T]: ...
 
     @abstractmethod
     def iter_edges(self) -> Iterator[tuple[T, T]]: ...
-
-    @abstractmethod
-    def add_node(self, node: T) -> Self: ...
-
-    @abstractmethod
-    def add_edge(self, src: T, dst: T) -> Self: ...
 
     @property
     @abstractmethod
@@ -26,6 +18,14 @@ class Graph(Protocol[T]):
     @property
     @abstractmethod
     def successors(self) -> Mapping[T, set[T]]: ...
+
+
+class Graph[T: Hashable](ReadOnlyGraph[T], Protocol):
+    @abstractmethod
+    def add_node(self, node: T) -> Self: ...
+
+    @abstractmethod
+    def add_edge(self, src: T, dst: T) -> Self: ...
 
     @abstractmethod
     def new(self) -> "Graph[T]": ...
