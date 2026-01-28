462cd01b878b3c781b457d626a80d8aff4d1c883
Lukas Einhaus <lukas.einhaus@uni-due.de>
2026-01-28 18:26:42 +0100

    feat(hw_function_id): add optional file filter
    Use case: Storing other files, e.g. metadata
    in the build folder, that does not influence
    the resulting hw accelerator

--- elasticai/creator/hw_function_id.py
+++ elasticai/creator/hw_function_id.py
@@ -5,20 +5,49 @@ from pathlib import Path
 
 
 class HwFunctionIdUpdater:
+    """Hash all files in `build_dir` except for `target_file` and uses `replace_id_fn` to write the result to `target_file`.
+
+    The id will stay the same, if none of the files, except for the target file changed.
+
+    You can use the optional `file_filter` to exclude additional files from the id computation.
+    Example:
+
+    ```python
+    generate_my_source_files(build_dir)
+    def replace_id(code: Iterable[str], id: bytes) -> Iterator[str]:
+        for line in code:
+            if line == "my_id_placeholder":
+                yield f"my_id = {id.hex()};"
+            else:
+                yield line
+
+    def filter_non_vhd(file):
+        return str(file).endswith(".vhd")
+
+    id_updater = HwFunctionIdUpdate(build_dir, "id_package.vhd", replace_id, filter_non_vhd)
+    id_update.compute_id()
+    id_updater.write_id()
+    print(id_updater.id)
+    ```
+
+    """
+
     def __init__(
         self,
         build_dir: Path,
         target_file: str | Path,
         replace_id_fn: Callable[[Iterable[str], bytes], Iterator[str]],
+        file_filter: Callable[[Path], bool] = lambda f: True,
     ):
         self._target_file = Path(target_file)
         self._build_dir = build_dir
         self._id = bytes()
+        self._file_filter = file_filter
         self._replace_id_fn = replace_id_fn
 
     def _files_from_build_dir(self) -> Iterator[Path]:
         for f in self._build_dir.glob("**/*"):
-            if f.is_file():
+            if f.is_file() and self._file_filter(f):
                 yield f
 
     def _is_not_target_file(self, file: Path) -> bool:
