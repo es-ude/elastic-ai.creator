a12ddc91496d63348a8d02a3f3c327db8ede13ef
Lukas Einhaus <lukas.einhaus@uni-due.de>
2026-04-25 10:43:19 +0200

    feat(plugins.lutron_filters): add truth table generation

---
+++ src/elasticai/creator_plugins/lutron_filter/__init__.py
@@ -0,0 +1,7 @@
+from .nn import Binarize as Binarize
+from .precompute.truth_table_generation import (
+    generate_input_tensor_1d as generate_input_tensor_1d,
+)
+from .tensor_conversion import (
+    torch1d_input_tensor_to_grouped_strings as torch1d_input_tensor_to_grouped_strings,
+)
---
+++ src/elasticai/creator_plugins/lutron_filter/nn/__init__.py
@@ -0,0 +1,1 @@
+from .binarize import Binarize as Binarize
