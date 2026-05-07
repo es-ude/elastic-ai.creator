b0d32cb36dfbd47adce01722f4001a266c7a6757
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2026-05-07 10:58:21 +0200

    fix(nn.fxp): Fix lut table generation for precomputed module
    There was an error in building the LUT tables of the transfer function.
    It receives a float input and scale it down again.
    There was no check if float or int.

--- tests/unit_tests/nn/fixed_point/sigmoid/layer_func_test.py
+++ tests/unit_tests/nn/fixed_point/sigmoid/layer_func_test.py
@@ -42,3 +42,105 @@ def test_sigmoid_compared_torch(
         out1.max() - out1.min()
     )
     assert metric_mean < 0.6 * fxp.minimum_step_as_rational
+
+
+@pytest.mark.parametrize(
+    "total_bits, frac_bits, num_steps, q_input, q_output",
+    [
+        (
+            5,
+            3,
+            12,
+            [-16, -13, -10, -8, -5, -2, 1, 4, 7, 9, 12, 15],
+            [1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6, 7],
+        ),
+        (
+            5,
+            3,
+            32,
+            [
+                -16,
+                -15,
+                -14,
+                -13,
+                -12,
+                -11,
+                -10,
+                -9,
+                -8,
+                -7,
+                -6,
+                -5,
+                -4,
+                -3,
+                -2,
+                -1,
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10,
+                11,
+                12,
+                13,
+                14,
+                15,
+            ],
+            [
+                1,
+                1,
+                1,
+                1,
+                1,
+                2,
+                2,
+                2,
+                2,
+                2,
+                2,
+                3,
+                3,
+                3,
+                3,
+                4,
+                4,
+                4,
+                4,
+                5,
+                5,
+                5,
+                5,
+                6,
+                6,
+                6,
+                6,
+                6,
+                6,
+                7,
+                7,
+                7,
+            ],
+        ),
+    ],
+)
+def test_transfer_function_precomputed_sigmoid(
+    total_bits: int,
+    frac_bits: int,
+    num_steps: int,
+    q_input: list[int],
+    q_output: list[int],
+) -> None:
+    act = nn_creator.Sigmoid(
+        total_bits=total_bits, frac_bits=frac_bits, num_steps=num_steps
+    )
+    qin_result, qout_result = act.get_lut_integer()
+    assert len(qin_result) == num_steps
+    assert len(qout_result) == num_steps
+    assert q_input == qin_result
+    assert q_output == qout_result
--- tests/unit_tests/nn/fixed_point/silu_adaptible/layer_func_test.py
+++ tests/unit_tests/nn/fixed_point/silu_adaptible/layer_func_test.py
@@ -33,3 +33,33 @@ def test_silu_compared_torch(total_bits: int, frac_bits: int, num_steps: int) ->
         out0.max() - out0.min()
     )
     assert metric_mean < 0.6 * fxp.minimum_step_as_rational
+
+
+@pytest.mark.parametrize(
+    "total_bits, frac_bits, num_steps, q_input, q_output",
+    [
+        (3, 2, 8, [-4, -3, -2, -1, 0, 1, 2, 3], [-1, -1, -1, -1, 0, 0, 1, 2]),
+        (
+            5,
+            3,
+            16,
+            [-16, -14, -12, -10, -8, -6, -4, -2, 1, 3, 5, 7, 9, 11, 13, 15],
+            [-2, -2, -2, -2, -2, -2, -2, -1, 0, 1, 2, 4, 6, 8, 10, 12],
+        ),
+    ],
+)
+def test_transfer_function_precomputed_adapt_silu(
+    total_bits: int,
+    frac_bits: int,
+    num_steps: int,
+    q_input: list[int],
+    q_output: list[int],
+) -> None:
+    act = nn_creator.AdaptableSiLU(
+        total_bits=total_bits, frac_bits=frac_bits, num_steps=num_steps
+    )
+    qin_result, qout_result = act.get_lut_integer()
+    assert len(qin_result) == num_steps
+    assert len(qout_result) == num_steps
+    assert q_input == qin_result
+    assert q_output == qout_result
